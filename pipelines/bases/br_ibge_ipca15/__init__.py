870c4203b9e84bb0edc841a06558c4db7992d962
lucascr91 <lucas.ecomg@gmail.com>
2022-02-10 21:33:25 -0300

    add ipca15 data

---
+++ pipelines/bases/br_ibge_ipca15/__init__.py
@@ -0,0 +1,7 @@
+"""
+Prefect flows for br_ibge_ipca15 project
+"""
+###############################################################################
+# Automatically managed, please do not touch
+###############################################################################
+from .flows import *
