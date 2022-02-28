0b78c5848402f26c1fa6f4bdda178ae9b4c8ac4d
lucascr91 <lucas.ecomg@gmail.com>
2022-02-28 12:05:03 -0300

    fix __init__.py

--- pipelines/bases/__init__.py
+++ pipelines/bases/__init__.py
@@ -9,4 +9,5 @@ from pipelines.bases.br_cvm_oferta_publica_distribuicao.flows import *
 from pipelines.bases.br_ibge_inpc.flows import *
 from pipelines.bases.br_ibge_ipca.flows import *
 from pipelines.bases.br_ibge_ipca15.flows import *
+from pipelines.bases.br_ms_vacinacao_covid19.flows import *
 from pipelines.bases.test_pipeline.flows import *
