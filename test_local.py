598c9652662c004d7bf5c57164e9d21ad4202931
lucascr91 <lucas.ecomg@gmail.com>
2022-03-08 16:32:45 -0300

    [fix] update test_local to include all flows

--- test_local.py
+++ test_local.py
@@ -1,5 +1,31 @@
 from pipelines.utils import run_local
-from pipelines.datasets.br_ibge_ipca.flows import br_ibge_ipca_mes_brasil
+from functools import reduce
+from pipelines.datasets.br_ibge_ipca15 import (
+br_ibge_ipca15_mes_categoria_brasil, br_ibge_ipca15_mes_categoria_rm, br_ibge_ipca15_mes_categoria_municipio, br_ibge_ipca15_mes_brasil
+)
+
+from pipelines.datasets.br_ibge_ipca import (
+br_ibge_ipca_mes_categoria_brasil, br_ibge_ipca_mes_categoria_rm, br_ibge_ipca_mes_categoria_municipio, br_ibge_ipca_mes_brasil
+)
+
+from pipelines.datasets.br_ibge_inpc import (
+br_ibge_inpc_mes_categoria_brasil, br_ibge_inpc_mes_categoria_rm, br_ibge_inpc_mes_categoria_municipio, br_ibge_inpc_mes_brasil
+)
+
+from pipelines.datasets.br_cvm_oferta_publica_distribuicao import br_cvm_ofe_pub_dis_dia
+from pipelines.datasets.br_cvm_administradores_carteira import (br_cvm_adm_car_res, br_cvm_adm_car_pes_fis, br_cvm_adm_car_pes_jur)
+
+flows_ipca15 = [br_ibge_ipca15_mes_categoria_brasil, br_ibge_ipca15_mes_categoria_rm, br_ibge_ipca15_mes_categoria_municipio, br_ibge_ipca15_mes_brasil]
+
+flows_ipca = [br_ibge_ipca_mes_categoria_brasil, br_ibge_ipca_mes_categoria_rm, br_ibge_ipca_mes_categoria_municipio, br_ibge_ipca_mes_brasil]
+
+flows_inpc = [br_ibge_inpc_mes_categoria_brasil, br_ibge_inpc_mes_categoria_rm, br_ibge_inpc_mes_categoria_municipio, br_ibge_inpc_mes_brasil]
+
+flows_cvm = [br_cvm_ofe_pub_dis_dia, br_cvm_adm_car_res, br_cvm_adm_car_pes_fis, br_cvm_adm_car_pes_jur]
+
+flows = reduce(lambda x,y: x+y, [flows_ipca,flows_inpc,flows_ipca15, flows_cvm])
+
 
 if __name__ == "__main__":
-    run_local(br_ibge_ipca_mes_brasil)
+    for flow in flows_cvm[:1]:
+        run_local(flow)
