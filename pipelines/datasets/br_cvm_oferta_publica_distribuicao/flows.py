c0cb899adbe6af320961ccd7e718761140c9f825
lucascr91 <lucas.ecomg@gmail.com>
2022-03-14 18:55:03 -0300

    [fix] add upstream_tasks ofe_pub flow

--- pipelines/datasets/br_cvm_oferta_publica_distribuicao/flows.py
+++ pipelines/datasets/br_cvm_oferta_publica_distribuicao/flows.py
@@ -17,8 +17,8 @@ ROOT = "/tmp/data"
 URL = "http://dados.cvm.gov.br/dados/OFERTA/DISTRIB/DADOS/oferta_distribuicao.csv"
 
 with Flow("br_cvm_oferta_publica_distribuicao.dia") as br_cvm_ofe_pub_dis_dia:
-    crawl(ROOT, URL)
-    filepath = clean_table_oferta_distribuicao(ROOT)
+    wait_crawl = crawl(ROOT, URL)
+    filepath = clean_table_oferta_distribuicao(ROOT, upstream_tasks[wait_crawl])
     dataset_id = "br_cvm_oferta_publica_distribuicao"
     table_id = "dia"
 
