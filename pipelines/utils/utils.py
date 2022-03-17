2284e505f903c48bcf9152581bdee76b5373d2ab
lucascr91 <lucas.ecomg@gmail.com>
2022-03-16 22:07:23 -0300

    solve conflicts in task and utils

--- pipelines/utils/utils.py
+++ pipelines/utils/utils.py
@@ -1,17 +1,22 @@
 """
 General utilities for all pipelines.
 """
-from pathlib import Path
-from typing import Any, Dict, Union, Tuple, List
+
 import logging
+from os import getenv
+from pathlib import Path
+from typing import Any, Dict, List, Tuple, Union
 
 import basedosdados as bd
-
-# from mysqlx import Row
-import pandas as pd
+import hvac
 import numpy as np
+import pandas as pd
 import prefect
-from prefect import task
+from prefect.client import Client
+from prefect.engine.state import State
+from prefect.run_configs import KubernetesRun
+import requests
+# import telegram
 
 
 def log(msg: Any, level: str = "info") -> None:
@@ -31,11 +36,68 @@ def log(msg: Any, level: str = "info") -> None:
 
 
 @prefect.task(checkpoint=False)
-def log_task(msg: Any, level: str = "info"):
+def log_task(
+    msg: Any, level: str = "info", wait=None  # pylint: disable=unused-argument
+):
     """
     Logs a message to prefect's logger.
     """
     log(msg, level)
+
+
+def get_vault_client() -> hvac.Client:
+    """
+    Returns a Vault client.
+    """
+    return hvac.Client(
+        url=getenv("VAULT_ADDRESS").strip(),
+        token=getenv("VAULT_TOKEN").strip(),
+    )
+
+
+def get_vault_secret(secret_path: str, client: hvac.Client = None) -> dict:
+    """
+    Returns a secret from Vault.
+    """
+    vault_client = client if client else get_vault_client()
+    return vault_client.secrets.kv.read_secret_version(secret_path)["data"]
+
+
+def get_username_and_password_from_secret(
+    secret_path: str,
+    client: hvac.Client = None,
+) -> Tuple[str, str]:
+    """
+    Returns a username and password from a secret in Vault.
+    """
+    secret = get_vault_secret(secret_path, client)
+    return (
+        secret["data"]["username"],
+        secret["data"]["password"],
+    )
+
+
+def notify_discord_on_failure(
+    flow: prefect.Flow,
+    state: State,
+    secret_path: str,
+):
+    """
+    Notifies a Discord channel when a flow fails.
+    """
+    url = get_vault_secret(secret_path)["data"]["url"]
+    flow_run_id = prefect.context.get("flow_run_id")
+    message = (
+        f":man_facepalming: Flow **{flow.name}** has failed."
+        + f'\n  - State message: *"{state.message}"*'
+        + "\n  - Link to the failed flow: "
+        + f"http://prefect-ui.prefect.svc.cluster.local:8080/flow-run/{flow_run_id}"
+    )
+    send_discord_message(
+        message=message,
+        webhook_url=url,
+    )
+
 
 
 def run_local(flow: prefect.Flow, parameters: Dict[str, Any] = None):
@@ -51,6 +113,104 @@ def run_local(flow: prefect.Flow, parameters: Dict[str, Any] = None):
     if parameters:
         return flow.run(parameters=parameters)
     return flow.run()
+
+
+def run_cloud(
+    flow: prefect.Flow,
+    labels: List[str],
+    parameters: Dict[str, Any] = None,
+    run_description: str = "",
+):
+    """
+    Runs a flow on Prefect Server (must have VPN configured).
+    """
+    # Setup no schedule
+    flow.schedule = None
+
+    # Change flow name for development and register
+    flow.name = f"{flow.name} (development)"
+    flow.run_config = KubernetesRun(image="ghcr.io/prefeitura-rio/prefect-flows:latest")
+    flow_id = flow.register(project_name="main", labels=[])
+
+    # Get Prefect Client and submit flow run
+    client = Client()
+    flow_run_id = client.create_flow_run(
+        flow_id=flow_id,
+        run_name=f"TEST RUN - {run_description} - {flow.name}",
+        labels=labels,
+        parameters=parameters,
+    )
+
+    # Print flow run link so user can check it
+    print(f"Run submitted: TEST RUN - {run_description} - {flow.name}")
+    print(
+        f"Please check at: http://prefect-ui.prefect.svc.cluster.local:8080/flow-run/{flow_run_id}"
+    )
+
+
+def query_to_line(query: str) -> str:
+    """
+    Converts a query to a line.
+    """
+    return " ".join([line.strip() for line in query.split("\n")])
+
+
+def send_discord_message(
+    message: str,
+    webhook_url: str,
+) -> None:
+    """
+    Sends a message to a Discord channel.
+    """
+    requests.post(
+        webhook_url,
+        data={"content": message},
+    )
+
+
+# def send_telegram_message(
+#     message: str,
+#     token: str,
+#     chat_id: int,
+#     parse_mode: str = telegram.ParseMode.HTML,
+# ):
+#     """
+#     Sends a message to a Telegram chat.
+#     """
+#     bot = telegram.Bot(token=token)
+#     bot.send_message(
+#         chat_id=chat_id,
+#         text=message,
+#         parse_mode=parse_mode,
+#     )
+
+
+def smart_split(
+    text: str,
+    max_length: int,
+    separator: str = " ",
+) -> List[str]:
+    """
+    Splits a string into a list of strings.
+    """
+    if len(text) <= max_length:
+        return [text]
+
+    separator_index = text.rfind(separator, 0, max_length)
+    if (separator_index >= max_length) or (separator_index == -1):
+        raise ValueError(
+            f'Cannot split text "{text}" into {max_length}'
+            f'characters using separator "{separator}"'
+        )
+
+    return [
+        text[:separator_index],
+        *smart_split(
+            text[separator_index + len(separator) :],
+            max_length,
+            separator,
+        ),
+    ]
 
 
 def untuple_clocks(clocks):
@@ -76,16 +236,13 @@ def dataframe_to_csv(dataframe: pd.DataFrame, path: Union[str, Path]) -> None:
     # Create directory if it doesn't exist
     path.parent.mkdir(parents=True, exist_ok=True)
     # Write dataframe to CSV
-    log(f"Writing dataframe to CSV: {path}")
     dataframe.to_csv(path, index=False, encoding="utf-8")
-    log(f"Wrote dataframe to CSV: {path}")
 
 
 def batch_to_dataframe(batch: Tuple[Tuple], columns: List[str]) -> pd.DataFrame:
     """
     Converts a batch of rows to a dataframe.
     """
-    log(f"Converting batch of size {len(batch)} to dataframe")
     return pd.DataFrame(batch, columns=columns)
 
 
@@ -109,6 +266,9 @@ def clean_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
 
 
 def remove_columns_accents(dataframe: pd.DataFrame) -> list:
+    """
+    Remove accents from dataframe columns.
+    """
     return list(
         dataframe.columns.str.normalize("NFKD")
         .str.encode("ascii", errors="ignore")
@@ -116,29 +276,19 @@ def remove_columns_accents(dataframe: pd.DataFrame) -> list:
     )
 
 
-###############
-#
-# File
-#
-###############
-
-
-def to_partitions(data, partition_columns, savepath):
+def to_partitions(data: pd.DataFrame, partition_columns: List[str], savepath: str):
     """Save data in to hive patitions schema, given a dataframe and a list of partition columns.
     Args:
         data (pandas.core.frame.DataFrame): Dataframe to be partitioned.
         partition_columns (list): List of columns to be used as partitions.
         savepath (str, pathlib.PosixPath): folder path to save the partitions
-
     Exemple:
-
         data = {
             "ano": [2020, 2021, 2020, 2021, 2020, 2021, 2021,2025],
             "mes": [1, 2, 3, 4, 5, 6, 6,9],
             "sigla_uf": ["SP", "SP", "RJ", "RJ", "PR", "PR", "PR","PR"],
             "dado": ["a", "b", "c", "d", "e", "f", "g",'h'],
         }
-
         to_partitions(
             data=pd.DataFrame(data),
             partition_columns=['ano','mes','sigla_uf'],
@@ -150,6 +300,7 @@ def to_partitions(data, partition_columns, savepath):
 
         savepath = Path(savepath)
 
+        # create unique combinations between partition columns
         unique_combinations = (
             data[partition_columns]
             .drop_duplicates(subset=partition_columns)
@@ -161,9 +312,8 @@ def to_partitions(data, partition_columns, savepath):
                 f"{partition}={value}"
                 for partition, value in filter_combination.items()
             ]
-            filter_save_path = Path(savepath / "/".join(patitions_values))
-            filter_save_path.mkdir(parents=True, exist_ok=True)
 
+            # get filtered data
             df_filter = data.loc[
                 data[filter_combination.keys()]
                 .isin(filter_combination.values())
@@ -172,7 +322,55 @@ def to_partitions(data, partition_columns, savepath):
             ]
             df_filter = df_filter.drop(columns=partition_columns)
 
-            df_filter.to_csv(filter_save_path / "data.csv", index=False)
+            # create folder tree
+            filter_save_path = Path(savepath / "/".join(patitions_values))
+            filter_save_path.mkdir(parents=True, exist_ok=True)
+            file_filter_save_path = Path(filter_save_path) / "data.csv"
 
+            # append data to csv
+            df_filter.to_csv(
+                file_filter_save_path,
+                index=False,
+                mode="a",
+                header=not file_filter_save_path.exists(),
+            )
     else:
         raise BaseException("Data need to be a pandas DataFrame")
+
+
+###############
+#
+# Storage utils
+#
+###############
+
+
+def get_storage_blobs(dataset_id: str, table_id: str) -> list:
+    """
+    Get all blobs from a table in a dataset.
+    """
+
+    storage = bd.Storage(dataset_id=dataset_id, table_id=table_id)
+    return list(
+        storage.client["storage_staging"]
+        .bucket(storage.bucket_name)
+        .list_blobs(prefix=f"staging/{storage.dataset_id}/{storage.table_id}/")
+    )
+
+
+def parser_blobs_to_partition_dict(blobs: list) -> dict:
+    """
+    Extracts the partition information from the blobs.
+    """
+
+    partitions_dict = {}
+    for blob in blobs:
+        for folder in blob.name.split("/"):
+            if "=" in folder:
+                key = folder.split("=")[0]
+                value = folder.split("=")[1]
+                try:
+                    partitions_dict[key].append(value)
+                except KeyError:
+                    partitions_dict[key] = [value]
+    return partitions_dict
