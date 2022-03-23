baf5c315c278919253ee99c1c6b4b3b314a0ed19
lucascr91 <lucas.ecomg@gmail.com>
2022-03-23 12:09:14 -0300

    [infra] add update_metadata to oferta_distribuicao flow

--- pipelines/datasets/br_cvm_oferta_publica_distribuicao/flows.py
+++ pipelines/datasets/br_cvm_oferta_publica_distribuicao/flows.py
@@ -15,9 +15,12 @@ from pipelines.utils.tasks import (
     upload_to_gcs,
     create_bd_table,
     dump_header_to_csv,
+    get_temporal_coverage,
+    update_metadata,
     publish_table,
 )
 from pipelines.datasets.br_cvm_oferta_publica_distribuicao.schedules import every_day
+from datetime import datetime
 
 ROOT = "/tmp/data"
 URL = "http://dados.cvm.gov.br/dados/OFERTA/DISTRIB/DADOS/oferta_distribuicao.csv"
@@ -47,12 +50,32 @@ with Flow("br_cvm_oferta_publica_distribuicao.dia") as br_cvm_ofe_pub_dis_dia:
         wait=wait_create_bd_table,
     )
 
+    ##update_metadata
+    now = datetime.now().strftime("%Y/%m/%d")
+    temporal_coverage = get_temporal_coverage(
+        filepath=filepath,
+        date_col="data_abertura_processo",
+        time_unit="day",
+        interval="1",
+        upstream_tasks=[wait_upload_table],
+    )
+
+    wait_update_metadata = update_metadata(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        fields_to_update=[
+            {"last_updated": {"metadata": now}},
+            {"temporal_coverage": [temporal_coverage]},
+        ],
+        upstream_tasks=[temporal_coverage],
+    )
+
     publish_table(
         path=filepath,
         dataset_id=dataset_id,
         table_id=table_id,
         if_exists="replace",
-        wait=wait_upload_table,
+        wait=wait_update_metadata,
     )
 
 br_cvm_ofe_pub_dis_dia.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
