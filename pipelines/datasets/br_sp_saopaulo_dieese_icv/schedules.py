dd769cf0e687c42009623a738c68a843f07d3a4c
crislanealves <crislanealves@protonmail.com>
2022-04-11 19:53:24 -0300

    sobe br_sp_dieese

---
+++ pipelines/datasets/br_sp_saopaulo_dieese_icv/schedules.py
@@ -0,0 +1,22 @@
+"""
+Schedules for br_sp_saopaulo_dieese_icv
+"""
+
+from datetime import timedelta, datetime
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+from pipelines.constants import constants
+
+every_month = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days = 30),
+            start_date=datetime(2021, 1, 1),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ]
+        )
+    ]
+)
+
+
---
+++ pipelines/datasets/br_sp_saopaulo_dieese_icv/utils.py
@@ -0,0 +1,30 @@
+"""
+General purpose functions for the br_sp_saopaulo_dieese_icv project
+"""
+
+###############################################################################
+#
+# Esse é um arquivo onde podem ser declaratas funções que serão usadas
+# pelo projeto br_sp_saopaulo_dieese_icv.
+#
+# Por ser um arquivo opcional, pode ser removido sem prejuízo ao funcionamento
+# do projeto, caos não esteja em uso.
+#
+# Para declarar funções, basta fazer em código Python comum, como abaixo:
+#
+# ```
+# def foo():
+#     """
+#     Function foo
+#     """
+#     print("foo")
+# ```
+#
+# Para usá-las, basta fazer conforme o exemplo abaixo:
+#
+# ```py
+# from pipelines.datasets.br_sp_saopaulo_dieese_icv.utils import foo
+# foo()
+# ```
+#
+###############################################################################
