bc14a582791c8eb5dab2453792e582d39c1f388b
Lucas Cavalcanti Rodrigues <lucas.ecomg@gmail.com>
2022-04-20 16:06:07 -0300

    fix imports to match pipelines in prod
    Adicionei as importações de pipelines que foram incluídas após o início desse PR

--- pipelines/datasets/__init__.py
+++ pipelines/datasets/__init__.py
@@ -9,6 +9,7 @@ from pipelines.datasets.br_cvm_oferta_publica_distribuicao.flows import *
 from pipelines.datasets.br_ibge_inpc.flows import *
 from pipelines.datasets.br_ibge_ipca.flows import *
 from pipelines.datasets.br_ibge_ipca15.flows import *
-from pipelines.datasets.br_cgu_terceirizados.flows import *
 from pipelines.datasets.br_bd_indicadores.flows import *
 from pipelines.datasets.br_poder360_pesquisas.flows import *
+from pipelines.datasets.botdosdados.flows import *
+from pipelines.datasets.br_cgu_terceirizados.flows import *
