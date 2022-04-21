8ba5107c68aee9f80986897e7360d1af91330726
Lucas Cavalcanti Rodrigues <lucas.ecomg@gmail.com>
2022-04-21 14:13:41 -0300

    [infra] cgu to prod

--- pipelines/datasets/br_cgu_pessoal_executivo_federal/schedules.py
+++ pipelines/datasets/br_cgu_pessoal_executivo_federal/schedules.py
@@ -15,7 +15,7 @@ every_four_months = Schedule(
             rrule(freq=MONTHLY, count=4),
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value
                 ]
             )
         ]
