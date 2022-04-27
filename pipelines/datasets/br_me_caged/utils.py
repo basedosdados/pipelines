61ed19acd1c2dd080c7387c687364ce280731049
lucascr91 <lucas.ecomg@gmail.com>
2022-04-27 16:18:04 -0300

    [infra] add basic structure

---
+++ pipelines/datasets/br_me_caged/utils.py
@@ -0,0 +1,30 @@
+"""
+General purpose functions for the br_me_caged project
+"""
+
+###############################################################################
+#
+# Esse é um arquivo onde podem ser declaratas funções que serão usadas
+# pelo projeto br_me_caged.
+#
+# Por ser um arquivo opcional, pode ser removido sem prejuízo ao funcionamento
+# do projeto, caos não esteja em uso.
+#
+# Para declarar funções, basta fazer em código Python comum, como abaixo:
+#
+# ```
+# def foo():
+#     """
+#     Function foo
+#     """
+#     print("foo")
+# ```
+#
+# Para usá-las, basta fazer conforme o exemplo abaixo:
+#
+# ```py
+# from pipelines.datasets.br_me_caged.utils import foo
+# foo()
+# ```
+#
+###############################################################################
