cb2725a32fe26c90e1662f669b7cffed57a69fbf
lucascr91 <lucas.ecomg@gmail.com>
2022-04-28 19:32:28 -0300

    [infra] add cagedmov flow

---
+++ pipelines/datasets/br_me_novo_caged/flows.py
@@ -0,0 +1,38 @@
+"""
+Flows for br_me_novo_caged
+"""
+from prefect import Flow
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefect.tasks.shell import ShellTask
+
+from pipelines.constants import constants
+from pipelines.datasets.br_me_novo_caged.tasks import build_partitions
+from pipelines.utils.tasks import create_table_and_upload_to_gcs
+from pipelines.datasets.br_me_novo_caged.schedules import every_month
+
+download = ShellTask(
+    command="bash bash_scripts/download.sh cagedmov", stream_output=True
+)
+
+# pylint: disable=C0103
+with Flow("br_me_novo_caged.microdados_mov") as cagedmov:
+    dataset_id = "br_me_novo_caged"
+    table_id = "microdados_mov"
+
+    download()
+
+    # pylint: disable=E1123
+    filepath = build_partitions(group="cagedmov", upstream_tasks=[download])
+
+    wait_upload_table = create_table_and_upload_to_gcs(
+        data_path=filepath,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_type="append",
+        wait=filepath,
+    )
+
+cagedmov.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+cagedmov.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
+cagedmov.schedule = every_month
---
+++ pipelines/datasets/br_me_novo_caged/schedules.py
@@ -0,0 +1,19 @@
+"""
+Schedules for br_me_novo_caged
+"""
+from datetime import timedelta, datetime
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import IntervalClock
+from pipelines.constants import constants
+
+every_month = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=30),
+            start_date=datetime(2021, 1, 1),
+            labels=[
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
+            ],
+        )
+    ]
+)
