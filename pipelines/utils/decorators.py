a13866991edff0da823f3c02f42e1d6bf9e6b851
lucascr91 <lucas.ecomg@gmail.com>
2022-04-30 21:38:55 -0300

    [infra] replace custom Flow to default Flow

--- pipelines/utils/decorators.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Custom decorators for pipelines.
-"""
-
-from pipelines.utils.custom import CustomFlow
-
-Flow = CustomFlow
--- pipelines/utils/execute_dbt_model/flows.py
+++ pipelines/utils/execute_dbt_model/flows.py
@@ -7,23 +7,17 @@ from prefect import Parameter, case
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+from prefect import Flow
 
 from pipelines.constants import constants
 from pipelines.utils.constants import constants as utils_constants
-from pipelines.utils.decorators import Flow
 from pipelines.utils.execute_dbt_model.tasks import (
     get_k8s_dbt_client,
     run_dbt_model,
 )
 from pipelines.utils.tasks import rename_current_flow_run_dataset_table
 
-with Flow(
-    name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
-    code_owners=[
-        "@pimbel#2426",
-        "@Gabriel Gazola Milan#8183",
-    ],
-) as run_dbt_model_flow:
+with Flow(name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value) as run_dbt_model_flow:
 
     # Parameters
     dataset_id = Parameter("dataset_id")
