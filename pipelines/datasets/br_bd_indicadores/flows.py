5660cf67541a6e370e4ed2d8c05fad84d45bff74
lucascr91 <lucas.ecomg@gmail.com>
2022-05-05 16:34:23 -0300

    [infra] remove old flow

--- pipelines/datasets/br_bd_indicadores/flows.py
+++ pipelines/datasets/br_bd_indicadores/flows.py
@@ -1,7 +1,6 @@
 """
 Flows for br_bd_indicadores
 """
-from copy import deepcopy
 
 from prefect import Flow, case
 from prefect.run_configs import KubernetesRun
@@ -17,7 +16,6 @@ from pipelines.datasets.br_bd_indicadores.tasks import (
     get_credentials,
 )
 
-from pipelines.utils.execute_dbt_model.flows import run_dbt_model_flow
 from pipelines.utils.tasks import create_table_and_upload_to_gcs
 from pipelines.datasets.br_bd_indicadores.schedules import every_day, every_week
 
@@ -102,11 +100,3 @@ with Flow("br_bd_indicadores.metricas_tweets_agg") as bd_twt_metricas_agg:
 bd_twt_metricas_agg.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 bd_twt_metricas_agg.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 bd_twt_metricas_agg.schedule = every_week
-
-run_dbt_br_bd_indicadores_flow = deepcopy(run_dbt_model_flow)
-run_dbt_br_bd_indicadores_flow.name = "br_bd_indicadores - Materializar tabelas"
-run_dbt_br_bd_indicadores_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-run_dbt_br_bd_indicadores_flow.run_config = KubernetesRun(
-    image=constants.DOCKER_IMAGE.value
-)
-run_dbt_br_bd_indicadores_flow.schedule = every_day
