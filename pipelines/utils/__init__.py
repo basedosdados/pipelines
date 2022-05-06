250eebb42a090069c63ed0f2864ee2a6e4724a77
d116626 <d116626@gmail.com>
2022-05-06 12:27:07 -0300

    feat: create one-click-download pipeline

--- pipelines/utils/__init__.py
+++ pipelines/utils/__init__.py
@@ -6,3 +6,5 @@ Prefect flows for basedosdados project
 # Automatically managed, please do not touch
 ###############################################################################
 from pipelines.utils.crawler_ibge_inflacao.flows import *
+from pipelines.utils.dump_to_gcs.flows import *
+from pipelines.utils.execute_dbt_model.flows import *
--- pipelines/utils/constants.py
+++ pipelines/utils/constants.py
@@ -11,6 +11,7 @@ class constants(Enum):  # pylint: disable=c0103
     Constants for utils.
     """
 
-    FLOW_EXECUTE_DBT_MODEL_NAME = "EMD: template - Executa DBT model"
-    FLOW_DUMP_DB_NAME = "EMD: template - Ingerir tabela de banco SQL"
-    FLOW_DUMP_BASEDOSDADOS_NAME = "EMD: template - Ingerir tabela do basedosdados"
+    FLOW_EXECUTE_DBT_MODEL_NAME = "DB: template - Executa DBT model"
+    FLOW_DUMP_DB_NAME = "DB: template - Ingerir tabela de banco SQL"
+    FLOW_DUMP_BASEDOSDADOS_NAME = "DB: template - Ingerir tabela do basedosdados"
+    FLOW_DUMP_TO_GCS_NAME = "BD: template - Ingerir tabela zipada para GCS"
