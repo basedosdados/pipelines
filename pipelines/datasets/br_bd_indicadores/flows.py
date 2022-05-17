fdcce8d6915688d6691ac0d11bf98d7991f0afd6
lucascr91 <lucas.ecomg@gmail.com>
2022-05-17 20:36:16 -0300

    [infra] add flow for twitter aggregate metrics

--- pipelines/datasets/br_bd_indicadores/flows.py
+++ pipelines/datasets/br_bd_indicadores/flows.py
@@ -15,7 +15,6 @@ from pipelines.utils.decorators import Flow
 from pipelines.utils.execute_dbt_model.constants import constants as dump_db_constants
 from pipelines.datasets.br_bd_indicadores.tasks import (
     crawler_metricas,
-    crawler_metricas_agg,
     has_new_tweets,
     echo,
     get_credentials,
@@ -113,21 +112,34 @@ bd_twt_metricas.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 bd_twt_metricas.schedule = every_day
 
 
-with Flow("br_bd_indicadores.metricas_tweets_agg") as bd_twt_metricas_agg:
+with Flow("br_bd_indicadores.twitter_metrics_agg") as bd_twt_metricas_agg:
     dataset_id = Parameter("dataset_id", default="br_bd_indicadores", required=True)
     table_id = Parameter("table_id", default="twitter_metrics_agg", required=True)
-    table_to_agg = Parameter("table_to_agg", default="twitter_metrics", required=True)
 
-    # pylint: disable=C0103
-    filepath = crawler_metricas_agg(table_to_agg=table_to_agg)
+    current_flow_labels = get_current_flow_labels()
+    materialization_flow = create_flow_run(
+        flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+        project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+        parameters={
+            "dataset_id": dataset_id,
+            "table_id": table_id,
+            "mode": materialization_mode,
+        },
+        labels=current_flow_labels,
+        run_name=f"Materialize {dataset_id}.{table_id}",
+    )
 
-    # pylint: disable=C0103
-    wait_upload_table = create_table_and_upload_to_gcs(
-        data_path=filepath,
-        dataset_id=dataset_id,
-        table_id=table_id,
-        dump_type="overwrite",
-        wait=filepath,
+    wait_for_materialization = wait_for_flow_run(
+        materialization_flow,
+        stream_states=True,
+        stream_logs=True,
+        raise_final_state=True,
+    )
+    wait_for_materialization.max_retries = (
+        dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+    )
+    wait_for_materialization.retry_delay = timedelta(
+        seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
     )
 
 bd_twt_metricas_agg.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
