0f5f6e8bfca082c0bcf2a1b6b1495b8e7710229c
lucascr91 <lucas.ecomg@gmail.com>
2022-05-31 11:22:04 -0300

    [infra] add values dbt_alias

--- pipelines/datasets/br_sp_saopaulo_dieese_icv/flows.py
+++ pipelines/datasets/br_sp_saopaulo_dieese_icv/flows.py
@@ -75,6 +75,7 @@ with Flow(
                 "dataset_id": dataset_id,
                 "table_id": table_id,
                 "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
             },
             labels=current_flow_labels,
             run_name=f"Materialize {dataset_id}.{table_id}",
