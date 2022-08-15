2746e8f556d1e519d22a531bbc790bade3009eee
lucascr91 <lucas.ecomg@gmail.com>
2022-08-14 22:42:01 -0300

    [infra] add materialization for tse tables

--- pipelines/datasets/br_tse_eleicoes/flows.py
+++ pipelines/datasets/br_tse_eleicoes/flows.py
@@ -3,12 +3,16 @@
 Flows for br_tse_eleicoes
 """
 # pylint: disable=invalid-name,line-too-long
+from datetime import datetime, timedelta
 
 from prefect import Parameter, case
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
 from pipelines.constants import constants
+from pipelines.utils.execute_dbt_model.constants import constants as dump_db_constants
+from pipelines.utils.constants import constants as utils_constants
 from pipelines.utils.decorators import Flow
 from pipelines.datasets.br_tse_eleicoes.tasks import (
     download_before22,
@@ -28,6 +32,8 @@ from pipelines.datasets.br_tse_eleicoes.schedules import (
 from pipelines.utils.tasks import (
     create_table_and_upload_to_gcs,
     rename_current_flow_run_dataset_table,
+    update_metadata,
+    get_current_flow_labels,
 )
 
 from pipelines.datasets.br_tse_eleicoes.constants import constants as tse_constants
@@ -40,6 +46,15 @@ with Flow(
     table_id = Parameter("table_id", default="candidatos", required=True)
     start = Parameter("start", default=2018, required=True)
     id_candidato_bd = Parameter("id_candidato_bd", default=False, required=True)
+
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
+
+    materialize_after_dump = Parameter(
+        "materialize after dump", default=True, required=False
+    )
+    dbt_alias = Parameter("dbt_alias", default=False, required=False)
 
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
@@ -100,6 +115,44 @@ with Flow(
             wait=filepath,
         )
 
+    wait_update_metadata = update_metadata(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        fields_to_update=[
+            {"last_updated": {"data": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}}
+        ],
+        upstream_tasks=[wait_upload_table],
+    )
+
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
 br_tse_candidatos.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_tse_candidatos.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 br_tse_candidatos.schedule = schedule_candidatos
@@ -113,6 +166,15 @@ with Flow(
     table_id = Parameter("table_id", default="bens_candidato", required=True)
     start = Parameter("start", default=2018, required=True)
     id_candidato_bd = Parameter("id_candidato_bd", default=False, required=True)
+
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
+
+    materialize_after_dump = Parameter(
+        "materialize after dump", default=True, required=False
+    )
+    dbt_alias = Parameter("dbt_alias", default=False, required=False)
 
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
@@ -171,6 +233,44 @@ with Flow(
             wait=filepath,
         )
 
+    wait_update_metadata = update_metadata(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        fields_to_update=[
+            {"last_updated": {"data": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}}
+        ],
+        upstream_tasks=[wait_upload_table],
+    )
+
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
 br_tse_bens.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_tse_bens.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 br_tse_bens.schedule = schedule_bens
@@ -183,6 +283,15 @@ with Flow(
     dataset_id = Parameter("dataset_id", default="br_tse_eleicoes", required=True)
     table_id = Parameter("table_id", default="despesas_candidato", required=True)
     id_candidato_bd = Parameter("id_candidato_bd", default=False, required=True)
+
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
+
+    materialize_after_dump = Parameter(
+        "materialize after dump", default=True, required=False
+    )
+    dbt_alias = Parameter("dbt_alias", default=False, required=False)
 
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
@@ -206,6 +315,44 @@ with Flow(
             wait=filepath,
         )
 
+    wait_update_metadata = update_metadata(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        fields_to_update=[
+            {"last_updated": {"data": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}}
+        ],
+        upstream_tasks=[wait_upload_table],
+    )
+
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
 br_tse_despesa.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_tse_despesa.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
 br_tse_despesa.schedule = schedule_despesa
@@ -218,6 +365,15 @@ with Flow(
     dataset_id = Parameter("dataset_id", default="br_tse_eleicoes", required=True)
     table_id = Parameter("table_id", default="receitas_candidato", required=True)
     id_candidato_bd = Parameter("id_candidato_bd", default=False, required=True)
+
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
+
+    materialize_after_dump = Parameter(
+        "materialize after dump", default=True, required=False
+    )
+    dbt_alias = Parameter("dbt_alias", default=False, required=False)
 
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
@@ -239,6 +395,44 @@ with Flow(
             table_id=table_id,
             dump_mode="append",
             wait=filepath,
+        )
+
+    wait_update_metadata = update_metadata(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        fields_to_update=[
+            {"last_updated": {"data": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}}
+        ],
+        upstream_tasks=[wait_upload_table],
+    )
+
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
         )
 
 br_tse_receita.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
