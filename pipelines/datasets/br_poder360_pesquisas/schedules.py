8dfca7c3e2adacc37f6faf8d0c4b80f396014951
lucascr91 <lucas.ecomg@gmail.com>
2022-08-18 09:53:39 -0300

    [infra] move flows to prod

--- pipelines/datasets/br_poder360_pesquisas/schedules.py
+++ pipelines/datasets/br_poder360_pesquisas/schedules.py
@@ -17,11 +17,11 @@ every_monday_thursday = Schedule(
             cron="0 21 * * 1,4",  # 0 21 * * 1,4 means At 21:00 on Monday and Thursday.
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_poder360_pesquisas",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize after dump": True,
                 "table_id": "microdados",
                 "dbt_alias": False,
