e9e059f27c90bde878f49ef0fdab4b64b62c148a
lucascr91 <lucas.ecomg@gmail.com>
2022-08-18 10:23:13 -0300

    [infra] fix import poder360

--- pipelines/datasets/br_poder360_pesquisas/schedules.py
+++ pipelines/datasets/br_poder360_pesquisas/schedules.py
@@ -3,7 +3,7 @@
 Schedules for br_poder360_pesquisas
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import IntervalClock
