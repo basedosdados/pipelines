b118b20a239907e684308247536912d45fcc1b2f
lucascr91 <lucas.ecomg@gmail.com>
2022-08-23 13:42:43 -0300

    [infra] add fllow for reprting GA metrics

--- pipelines/datasets/br_bd_indicadores/flows.py
+++ pipelines/datasets/br_bd_indicadores/flows.py
@@ -21,6 +21,7 @@ from pipelines.datasets.br_bd_indicadores.tasks import (
     echo,
     get_twitter_credentials,
     get_ga_credentials,
+    crawler_report_ga
 )
 from pipelines.utils.tasks import (
     create_table_and_upload_to_gcs,
@@ -31,6 +32,7 @@ from pipelines.utils.tasks import (
 from pipelines.datasets.br_bd_indicadores.schedules import (
     every_day,
     every_week,
+    schedule_users,
 )
 
 with Flow(
@@ -179,7 +181,7 @@ with Flow(
         prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
     )
 
-    property_id = get_ga_credentials(secret_path="ga_credentials", wait=None)
+    property_id = get_ga_credentials(secret_path="ga_credentials", key='property_id', wait=None)
 
     filepath = crawler_real_time(
         lst_dimension=["country", "city", "unifiedScreenName"],
@@ -205,16 +207,20 @@ with Flow(
     code_owners=[
         "lucas_cr",
     ],
-) as bd_pageviews:
+) as bd_ga_users:
     dataset_id = Parameter("dataset_id", default="br_bd_indicadores", required=True)
     table_id = Parameter("table_id", default="analytics_users", required=True)
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
     )
 
-    property_id = get_ga_credentials(secret_path="ga_credentials", wait=None)
+    view_id = get_ga_credentials(secret_path="ga_credentials", key="view_id", wait=None)
 
-    filepath = crawler_report_ga()
+    filepath = crawler_report_ga(
+        view_id=view_id,
+        metrics=["1dayUsers", "7dayUsers", "14dayUsers", "28dayUsers", "newUsers"],
+        upstream_tasks=[view_id],
+    )
 
     wait_upload_table = create_table_and_upload_to_gcs(
         data_path=filepath,
@@ -227,4 +233,4 @@ with Flow(
 
 bd_ga_users.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 bd_ga_users.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
-bd_ga_users.schedule = schedule_pageviews
+bd_ga_users.schedule = schedule_users
