9cc603a82ade94a0803c63b88fb8a769cf9072a6
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-09-04 18:45:51 -0300

    creating object to format IGP data from Ipeadata according do FGV

--- pipelines/datasets/br_fgv_igp/utils.py
+++ pipelines/datasets/br_fgv_igp/utils.py
@@ -29,3 +29,141 @@ General purpose functions for the br_fgv_igp project
 # ```
 #
 ###############################################################################
+import ipeadatapy as idpy
+import numpy as np
+import pandas as pd
+
+from pipelines.datasets.br_fgv_igp.constants import constants as fgv_constants
+
+FGV_INDEX = fgv_constants.FGV_INDEX.value
+ROOT = fgv_constants.ROOT.value
+
+
+class IGPData:
+    def __init__(self, ipeacode, period):
+        self.fgv_indexes = FGV_INDEX.get(ipeacode)
+        self.period = period
+        self.main_index = self.fgv_indexes[0]
+        self.first_dec = self.fgv_indexes[1]
+        self.second_dec = self.fgv_indexes[2]
+        self.decendios = self.fgv_indexes[1:3]
+        self.index_name = ipeacode
+        self.df = self._create_dataframe()
+
+    @property
+    def filepath(self) -> str:
+        """
+        The path of the file that will be saved to be uploaded to GCS
+        Returns:
+            str: full path of csv file
+        """
+        return f"{ROOT}/{self.index_name}_{self.period}.csv"
+
+    def _get_ipea_data(self) -> pd.DataFrame:
+        """
+        Method to get the DataFrame from Ipeadata
+        Returns:
+            pd.DataFrame: DataFrame from Ipeadata with FGV data
+        """
+        return idpy.timeseries(self.main_index)
+
+    def _create_dataframe(self) -> pd.DataFrame:
+        """
+        Creates basic DataFrame, according to period
+        Returns:
+            pd.DataFrame
+        """
+        df = self._get_ipea_data()
+        df.rename({df.columns[-1]: "indice"}, axis=1, inplace=True)
+        df["indice"] = df["indice"].astype(float, errors="ignore")
+        df.rename(columns={"YEAR": "ano", "MONTH": "mes"}, inplace=True)
+        df.drop(columns=["DAY", "CODE", "RAW DATE"], inplace=True)
+
+        if self.period == "mes":
+            df = self.month_dataframe(df)
+        if self.period == "ano":
+            df = self.year_dataframe(df)
+
+        return df
+
+    def month_dataframe(self, dfm: pd.DataFrame) -> pd.DataFrame:
+        """
+        Complement Ipea data with FGV information
+        Args:
+            dfm (pd.DataFrame: DataFrame to be modelled
+
+        Returns:
+            pd.DataFrame: DataFrame with specific columns
+        """
+        dfm["var_mensal"] = dfm["indice"].pct_change(periods=1) * 100
+        dfm["var_12_meses"] = dfm["indice"].pct_change(periods=12) * 100
+        if self.first_dec and self.second_dec:
+            decendios = self._get_decendios()
+            dfm = pd.merge(dfm, decendios, how="outer", on="DATE")
+        # noinspection PyUnresolvedReferences
+        dfm["acum_ano"] = (
+            dfm[["var_mensal"]]
+            .groupby(dfm.index.year)
+            .apply(self._calculate_year_accum)
+        )
+        dfm["next_month"] = dfm["indice"].shift(-1)
+        dfm["indice_fechamento_mensal"] = round(
+            np.sqrt(dfm["indice"] * dfm["next_month"]), ndigits=6
+        )
+        dfm.drop(columns=["next_month"], inplace=True)
+        return dfm
+
+    def year_dataframe(self, dff: pd.DataFrame) -> pd.DataFrame:
+        """
+        Complement Ipea data with FGV information
+        Args:
+            dff (pd.DataFrame: DataFrame to be modelled
+
+        Returns:
+            pd.DataFrame: DataFrame with specific columns
+
+        """
+        # noinspection PyUnresolvedReferences
+        grouped_df = dff.groupby(dff.index.year).mean()
+        grouped_df.rename(columns={"indice": "indice_medio"}, inplace=True)
+        grouped_df.index.name = "YEAR"
+        grouped_df.drop(columns=["mes"], inplace=True)
+        last_days = dff.loc[dff["mes"] == 12]
+        last_days.set_index("ano", inplace=True)
+        last_days.index.name = "YEAR"
+        last_days.drop(columns=["mes"], inplace=True)
+        dfy = pd.merge(grouped_df, last_days, how="outer", on="YEAR")
+        dfy.drop(dfy.tail(1).index, inplace=True)
+        dfy["var_anual"] = dfy["indice"].pct_change(periods=1) * 100
+        dfy["next_month"] = dfy["indice"].shift(-1)
+        dfy["indice_fechamento_anual"] = round(
+            np.sqrt(dfy["indice"] * dfy["next_month"]), ndigits=6
+        )
+        dfy.drop(columns=["next_month"], inplace=True)
+        return dfy
+
+    def _get_decendios(self) -> pd.DataFrame:
+        """
+        Get the 2 tenths of IGP-M, as calculated by FGV
+        Returns:
+            pd.DataFrame: DataFrame with 1st and 2nd tenths
+        """
+        dec1 = idpy.timeseries(self.decendios[0])
+        dec1.rename(
+            {dec1.columns[-1]: "indice_primeiro_decendio"}, axis=1, inplace=True
+        )
+        dec1 = dec1[["indice_primeiro_decendio"]]
+        dec2 = idpy.timeseries(self.decendios[1])
+        dec2.rename({dec2.columns[-1]: "indice_segundo_decendio"}, axis=1, inplace=True)
+        dec2 = dec2[["indice_segundo_decendio"]]
+        return pd.merge(dec1, dec2, how="outer", on="DATE")
+
+    @staticmethod
+    def _calculate_year_accum(row: pd.Series) -> pd.Series:
+        """
+        Calculates accumulated index in the year
+        Returns:
+            pd.Series: The colum with the accumulated index by year
+        """
+        idx = (row / 100) + 1
+        return (np.cumprod(idx) - 1) * 100
--- tests/test_igp.py
+++ tests/test_igp.py
@@ -5,11 +5,106 @@ from unittest.mock import patch
 import pandas as pd
 import pytest
 
+from pipelines.datasets.br_fgv_igp.constants import constants as fgv_igp_constants
 from pipelines.datasets.br_fgv_igp.flows import ROOT
-from pipelines.datasets.br_fgv_igp.tasks import clean_fgv_df
+from pipelines.datasets.br_fgv_igp.tasks import crawler_fgv, clean_fgv_df
+from pipelines.datasets.br_fgv_igp.utils import IGPData
 
 
-def test_crawler(igpdi_mensal: pd.DataFrame) -> None:
+def test_igp_di_object_mes(igpdi_mes_mock):
+    with patch(
+        "pipelines.datasets.br_fgv_igp.utils.IGPData._get_ipea_data",
+        return_value=igpdi_mes_mock,
+    ):
+        igpdi_mensal = IGPData("IGPDI", "mes")
+    acum_2021 = igpdi_mensal.df.loc["2021-12-01", "acum_ano"]
+    assert round(acum_2021, ndigits=2) == 17.74
+
+
+def test_igp_di_object_ano(igpdi_ano_mock):
+    with patch(
+        "pipelines.datasets.br_fgv_igp.utils.IGPData._get_ipea_data",
+        return_value=igpdi_ano_mock,
+    ):
+        igpdi_anual = IGPData("IGPDI", "ano")
+    var_2021 = igpdi_anual.df.loc[2021, "var_anual"]
+    assert round(var_2021, ndigits=6) == 17.737619
+
+
+def test_igp_m_object_mes(igpm_mes_mock, igpm_decendios_mock):
+    with patch(
+        "pipelines.datasets.br_fgv_igp.utils.IGPData._get_ipea_data",
+        return_value=igpm_mes_mock,
+    ):
+        with patch(
+            "pipelines.datasets.br_fgv_igp.utils.IGPData._get_decendios",
+            return_value=igpm_decendios_mock,
+        ):
+            igpm_mensal = IGPData("IGPM", "mes")
+
+    df = igpm_mensal.df
+    igpm_2021_12 = df.loc[(df.ano == 2021) & (df.mes == 12)].iloc[0]
+    assert round(igpm_2021_12.indice, ndigits=3) == 1100.988
+    assert round(igpm_2021_12.var_mensal, ndigits=2) == 0.87
+    assert round(igpm_2021_12.indice_primeiro_decendio, ndigits=2) == -0.22
+    assert round(igpm_2021_12.indice_segundo_decendio, ndigits=2) == 0.43
+
+
+def test_igp_m_object_ano(igpm_ano_mock):
+    with patch(
+        "pipelines.datasets.br_fgv_igp.utils.IGPData._get_ipea_data",
+        return_value=igpm_ano_mock,
+    ):
+        igpm_anual = IGPData("IGPM", "ano")
+
+    df = igpm_anual.df
+    igpm_2021 = df.loc[df.ano == 2021].iloc[0]
+    assert round(igpm_2021.var_anual, ndigits=6) == 17.783212
+
+
+def test_igp_og_object_mes(igpog_mes_mock):
+    with patch(
+        "pipelines.datasets.br_fgv_igp.utils.IGPData._get_ipea_data",
+        return_value=igpog_mes_mock,
+    ):
+        igpog_mensal = IGPData("IGPOG", "mes")
+    df = igpog_mensal.df
+    igpog_2022_06 = df.loc[(df.ano == 2022) & (df.mes == 6)].iloc[0]
+    assert round(igpog_2022_06.indice, ndigits=3) == 1158.568
+
+
+def test_igp_og_object_ano(igpog_ano_mock):
+    with patch(
+        "pipelines.datasets.br_fgv_igp.utils.IGPData._get_ipea_data",
+        return_value=igpog_ano_mock,
+    ):
+        igpog_anual = IGPData("IGPOG", "ano")
+    df = igpog_anual.df
+    igpog_2021 = df.loc[df.ano == 2021].iloc[0]
+    assert round(igpog_2021.indice_medio, ndigits=5) == 1029.02317
+
+
+def test_igp_10_object_mes(igp10_mes_mock):
+    with patch(
+        "pipelines.datasets.br_fgv_igp.utils.IGPData._get_ipea_data",
+        return_value=igp10_mes_mock,
+    ):
+        igp10_mensal = IGPData("IGP10", "mes")
+    df = igp10_mensal.df
+    igp10_2022_07 = df.loc[(df.ano == 2022) & (df.mes == 7)].iloc[0]
+    assert round(igp10_2022_07.indice, ndigits=3) == 1220.204
+
+
+@pytest.mark.skip("Will be converted to POO")
+def test_crawler():
+    INDICE = "IGPDI"
+    PERIODO = "mes"
+    df = crawler_fgv.run(fgv_igp_constants.FGV_INDEX.value.get(INDICE), PERIODO)
+    assert isinstance(df, pd.DataFrame)
+
+
+@pytest.mark.skip("Will be converted to POO")
+def test_clean_df_month(igpdi_mensal: pd.DataFrame) -> None:
     """
     Check indice of July 2022
     Args:
@@ -19,52 +114,172 @@ def test_crawler(igpdi_mensal: pd.DataFrame) -> None:
         None
 
     """
+    INDICE = "IGPDI"
+    PERIODO = "mes"
     with patch(
         "pipelines.datasets.br_fgv_igp.tasks.crawler_fgv.run",
         return_value=igpdi_mensal,
     ) as mocked:
-        df = mocked("IGP12_IGPDI12", period="mes")
+        df = mocked(INDICE, period=PERIODO)
 
-    df_cleaned = clean_fgv_df.run(df, root=ROOT)
+    df_cleaned = clean_fgv_df.run(df, root=ROOT, period=PERIODO)
 
     assert df_cleaned == Path("tmp/data/igpdi_mes.csv")
 
 
-@pytest.fixture
-def igpdi_mensal() -> pd.DataFrame:
+@pytest.mark.skip("Will be converted to POO")
+def test_clean_df_year(igpdi_anual: pd.DataFrame) -> None:
     """
-    Fixture of Monthly IGP-DI DataFrame (from Oct/2021 to Jul/2022)
+    Check indice of July 2022
+    Args:
+        igpdi_anual (pd.DataFrame): fixture of Monthly IGP-DI
+
+    Returns:
+        None
+
+    """
+    INDICE = "IGPDI"
+    PERIODO = "ano"
+    with patch(
+        "pipelines.datasets.br_fgv_igp.tasks.crawler_fgv.run",
+        return_value=igpdi_anual,
+    ) as mocked:
+        df = mocked(INDICE, period=PERIODO)
+
+    df_cleaned = clean_fgv_df.run(df, root=ROOT, period=PERIODO)
+
+    assert df_cleaned == Path("tmp/data/igpdi_ano.csv")
+
+
+@pytest.fixture
+def igpdi_mes_mock() -> pd.DataFrame:
+    """
+    Fixture of Monthly IGP-DI DataFrame
 
     Returns:
         pd.DataFrame: the fixture of the DataFrame
 
     """
-    df = pd.DataFrame(
-        [
-            [2021, 1, 10, "IGP12_IGPDI12", "2021-10-01T00:00:00-03:00", 1081.301],
-            [2021, 1, 11, "IGP12_IGPDI12", "2021-11-01T00:00:00-03:00", 1075.022],
-            [2021, 1, 12, "IGP12_IGPDI12", "2021-12-01T00:00:00-03:00", 1088.489],
-            [2022, 1, 1, "IGP12_IGPDI12", "2022-01-01T00:00:00-03:00", 1110.398],
-            [2022, 1, 2, "IGP12_IGPDI12", "2022-02-01T00:00:00-03:00", 1127.077],
-            [2022, 1, 3, "IGP12_IGPDI12", "2022-03-01T00:00:00-03:00", 1153.777],
-            [2022, 1, 4, "IGP12_IGPDI12", "2022-04-01T00:00:00-03:00", 1158.546],
-            [2022, 1, 5, "IGP12_IGPDI12", "2022-05-01T00:00:00-03:00", 1166.542],
-            [2022, 1, 6, "IGP12_IGPDI12", "2022-06-01T00:00:00-03:00", 1173.831],
-            [2022, 1, 7, "IGP12_IGPDI12", "2022-07-01T00:00:00-03:00", 1169.426],
-        ],
-        index=[
-            "2021-10-01",
-            "2021-11-01",
-            "2021-12-01",
-            "2022-01-01",
-            "2022-02-01",
-            "2022-03-01",
-            "2022-04-01",
-            "2022-05-01",
-            "2022-06-01",
-            "2022-07-01",
-        ],
-        columns=["YEAR", "DAY", "MONTH", "CODE", "RAW DATE", "indice"],
-    )
-    df.index.names = ["DATE"]
+    df = pd.read_csv("fixtures/igpdi_mes_ipea.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpdi_ano_mock() -> pd.DataFrame:
+    """
+    Fixture of Annual IGP-DI DataFrame
+
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+
+    """
+    df = pd.read_csv("fixtures/igpdi_mes_ipea.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpm_mes_mock() -> pd.DataFrame:
+    """
+    Fixture of monthly IGP-M Dataframe
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+    """
+    df = pd.read_csv("fixtures/igpm_mes_ipea.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpm_mes_1dec_mock() -> pd.DataFrame:
+    """
+    Fixture of first tenth of the month
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+    """
+    df = pd.read_csv("fixtures/igpm_mes_ipea_1dec.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpm_mes_2dec_mock() -> pd.DataFrame:
+    """
+    Fixture of first tenth of the month
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+    """
+    df = pd.read_csv("fixtures/igpm_mes_ipea_2dec.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpm_decendios_mock() -> pd.DataFrame:
+    """
+    Fixture for both tenths
+    Returns:
+        pd.DataFrame
+    """
+    df = pd.read_csv("fixtures/igpm_mes_decendios.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpm_ano_mock() -> pd.DataFrame:
+    """
+    Fixture of first tenth of the month
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+    """
+    df = pd.read_csv("fixtures/igpm_mes_ipea.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpog_mes_mock() -> pd.DataFrame:
+    """
+    Fixture of IGPOG month
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+    """
+    df = pd.read_csv("fixtures/igpog_mes_ipea.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igpog_ano_mock() -> pd.DataFrame:
+    """
+    Fixture of IGPOG month
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+    """
+    df = pd.read_csv("fixtures/igpog_mes_ipea.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+@pytest.fixture
+def igp10_mes_mock() -> pd.DataFrame:
+    """
+    Fixture of IGP10 month
+    Returns:
+        pd.DataFrame: the fixture of the DataFrame
+    """
+    df = pd.read_csv("fixtures/igp10_mes_ipea.csv")
+    df.set_index("DATE", drop=True, inplace=True)
+    df.index = pd.to_datetime(df.index)
     return df
