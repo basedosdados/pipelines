87b6622fb4546943955f1b4239fb9b4d94e2f43e
Ricardo Dahis <rdahis@gmail.com>
2022-09-16 18:10:24 -0400

    feat: move to br_bd_metadados

---
+++ pipelines/datasets/br_bd_metadados/schedules.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Schedules for bd_tweet_data
+"""
+
+from datetime import timedelta, datetime
+from prefect.schedules import Schedule, filters, adjustments
+from prefect.schedules.clocks import IntervalClock
+from pipelines.constants import constants
+
+every_day_organizations = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=1),
+            start_date=datetime(2022, 9, 15, 10, 00),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "br_bd_metadados",
+                "table_id": "organizations",
+                "materialization_mode": "dev",
+                "materialize after dump": True,
+                "dbt_alias": False,
+            },
+        ),
+    ],
+)
+
+every_day_datasets = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=1),
+            start_date=datetime(2022, 9, 15, 10, 00),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "br_bd_metadados",
+                "table_id": "datasets",
+                "materialization_mode": "dev",
+                "materialize after dump": True,
+                "dbt_alias": False,
+            },
+        ),
+    ],
+)
+
+every_day_resources = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=1),
+            start_date=datetime(2022, 9, 15, 10, 00),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "br_bd_metadados",
+                "table_id": "resources",
+                "materialization_mode": "dev",
+                "materialize after dump": True,
+                "dbt_alias": False,
+            },
+        ),
+    ],
+)
+
+every_day_external_links = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=1),
+            start_date=datetime(2022, 9, 15, 10, 00),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "br_bd_metadados",
+                "table_id": "external_links",
+                "materialization_mode": "dev",
+                "materialize after dump": True,
+                "dbt_alias": False,
+            },
+        ),
+    ],
+)
+
+every_day_information_requests = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=1),
+            start_date=datetime(2022, 9, 15, 10, 00),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "br_bd_metadados",
+                "table_id": "information_requests",
+                "materialization_mode": "dev",
+                "materialize after dump": True,
+                "dbt_alias": False,
+            },
+        ),
+    ],
+)
+
+every_day_tables = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=1),
+            start_date=datetime(2022, 9, 15, 10, 00),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "br_bd_metadados",
+                "table_id": "tables",
+                "materialization_mode": "dev",
+                "materialize after dump": True,
+                "dbt_alias": False,
+            },
+        ),
+    ],
+)
+
+every_day_columns = Schedule(
+    clocks=[
+        IntervalClock(
+            interval=timedelta(days=1),
+            start_date=datetime(2022, 9, 15, 10, 00),
+            labels=[
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+                "dataset_id": "br_bd_metadados",
+                "table_id": "columns",
+                "materialization_mode": "dev",
+                "materialize after dump": True,
+                "dbt_alias": False,
+            },
+        ),
+    ],
+)
