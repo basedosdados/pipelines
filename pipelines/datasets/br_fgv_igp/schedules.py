cf27f17772c62291c19379dbe8c47f6703647435
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-09-26 23:24:16 -0300

    changing schedule to CronClock, agent and materialization to prod

--- pipelines/datasets/br_fgv_igp/schedules.py
+++ pipelines/datasets/br_fgv_igp/schedules.py
@@ -3,27 +3,27 @@
 Schedules for br_fgv_igp
 """
 
-from datetime import timedelta, datetime
+from datetime import datetime
 
 from prefect.schedules import Schedule
-from prefect.schedules.clocks import IntervalClock
+from prefect.schedules.clocks import CronClock
 
 from pipelines.constants import constants
 
 igp_di_mes = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),
+        CronClock(
+            cron="00 03 08 * *",  # At 03:00 on day-of-month 8
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_fgv_igp",
                 "table_id": "igp_di_mes",
                 "indice": "IGPDI",
                 "periodo": "mes",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize_after_dump": True,
                 "dbt_alias": True,
             },
@@ -33,18 +33,18 @@ igp_di_mes = Schedule(
 
 igp_di_ano = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),
+        CronClock(
+            cron="00 03 08 01 *",  # At 03:00 on day-of-month 8 in January
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_fgv_igp",
                 "table_id": "igp_di_ano",
                 "indice": "IGPDI",
                 "periodo": "ano",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize_after_dump": True,
                 "dbt_alias": True,
             },
@@ -54,18 +54,18 @@ igp_di_ano = Schedule(
 
 igp_m_mes = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),
+        CronClock(
+            cron="00 03 01 * *",  # At 03:00 on day-of-month 1
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_fgv_igp",
                 "table_id": "igp_m_mes",
                 "indice": "IGPM",
                 "periodo": "mes",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize_after_dump": True,
                 "dbt_alias": True,
             },
@@ -75,18 +75,18 @@ igp_m_mes = Schedule(
 
 igp_m_ano = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),
+        CronClock(
+            cron="00 03 01 01 *",  # At 03:00 on day-of-month 1 in January
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_fgv_igp",
                 "table_id": "igp_m_ano",
                 "indice": "IGPM",
                 "periodo": "ano",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize_after_dump": True,
                 "dbt_alias": True,
             },
@@ -96,18 +96,18 @@ igp_m_ano = Schedule(
 
 igp_og_mes = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),
+        CronClock(
+            cron="00 03 08 * *",  # At 03:00 on day-of-month 8
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_fgv_igp",
                 "table_id": "igp_og_mes",
                 "indice": "IGPOG",
                 "periodo": "mes",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize_after_dump": True,
                 "dbt_alias": True,
             },
@@ -117,18 +117,18 @@ igp_og_mes = Schedule(
 
 igp_og_ano = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),
+        CronClock(
+            cron="00 03 08 01 *",  # At 03:00 on day-of-month 8 in January
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_fgv_igp",
                 "table_id": "igp_og_ano",
                 "indice": "IGPOG",
                 "periodo": "ano",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize_after_dump": True,
                 "dbt_alias": True,
             },
@@ -138,18 +138,18 @@ igp_og_ano = Schedule(
 
 igp_10_mes = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),
+        CronClock(
+            cron="00 03 18 * *",  # At 03:00 on day-of-month 18
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_fgv_igp",
                 "table_id": "igp_10_mes",
                 "indice": "IGP10",
                 "periodo": "mes",
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "materialize_after_dump": True,
                 "dbt_alias": True,
             },
