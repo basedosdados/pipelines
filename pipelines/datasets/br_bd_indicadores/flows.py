213448010db5d7e0f2ca125983e36b992171cd04
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2022-10-31 16:37:37 -0300

    adding underscores to variable materialize_after_dump

--- pipelines/datasets/br_bd_indicadores/flows.py
+++ pipelines/datasets/br_bd_indicadores/flows.py
@@ -315,7 +315,7 @@ with Flow(
     )
 
     materialize_after_dump = Parameter(
-        "materialize after dump", default=True, required=False
+        "materialize_after_dump", default=True, required=False
     )
     dbt_alias = Parameter("dbt_alias", default=False, required=False)
 
