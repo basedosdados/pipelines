c10bd930dd5e606bf452512fe3928a103ff55ae7
Tamir <tamtamireinhorn@gmail.com>
2023-03-28 17:54:43 -0300

    Installable package + test

---
+++ br_denatran_frota/code/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='download_frota',
+    version='1.0',
+    py_modules=['download_frota'],
+    install_requires=[
+        'beautifulsoup4',
+        'rarfile',
+    ],
+)
---
+++ br_denatran_frota/code/test/download_frota_test.py
@@ -0,0 +1,39 @@
+import os
+import tempfile
+import unittest
+from unittest.mock import patch
+from urllib.error import HTTPError
+from download_frota import *
+
+
+class TestDownloadFrota(unittest.TestCase):
+
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        os.rmdir(self.test_dir)
+
+    def test_download_frota_with_valid_args(self):
+        with patch('urllib.request.urlretrieve') as mock_urlretrieve:
+            download_frota(month=2, year=2022, dir=self.test_dir)
+            mock_urlretrieve.assert_called_once()
+
+    def test_download_frota_with_invalid_year(self):
+        with self.assertRaises(ValueError):
+            download_frota(month=1, year=2010)
+
+    def test_download_frota_with_invalid_month(self):
+        with self.assertRaises(HTTPError):
+            download_frota(month=13, year=2022, dir=self.test_dir)
+
+    def test_download_frota_with_missing_directory(self):
+        with self.assertRaises(FileNotFoundError):
+            download_frota(month=1, year=2022, dir='invalid_directory')
+
+    def test_download_frota_with_missing_temp_directory(self):
+        with self.assertRaises(FileNotFoundError):
+            download_frota(month=1, year=2022, tempdir='invalid_directory')
+
+if __name__ == '__main__':
+    unittest.main()
