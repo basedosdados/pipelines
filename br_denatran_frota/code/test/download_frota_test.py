b520f2dc0793fe2a0dabd789122bc3d3aa96305a
Tamir <tamtamireinhorn@gmail.com>
2023-03-28 21:41:06 -0300

    Working (badly) tests

--- br_denatran_frota/code/test/download_frota_test.py
+++ br_denatran_frota/code/test/download_frota_test.py
@@ -14,9 +14,17 @@ class TestDownloadFrota(unittest.TestCase):
         os.rmdir(self.test_dir)
 
     def test_download_frota_with_valid_args(self):
-        with patch("urllib.request.urlretrieve") as mock_urlretrieve:
-            download_frota(month=2, year=2022)
-            mock_urlretrieve.assert_called_once()
+        # TODO: Since there is no teardown, this test doesn't have the same effect running twice.
+        # It could always download the file twice in download_frota OR we implement cleanup?
+        # Or we generalize with tempd irs. This might just be overkill anyway.
+        download_frota(month=2, year=2022)
+        expected_file_path = os.path.join(
+            os.getcwd(),
+            "br_denatran_frota",
+            "files",
+            "frota_por_município_e_tipo_2-2022.xls",
+        )
+        self.assertTrue(os.path.isfile(expected_file_path))
 
     def test_download_frota_with_invalid_year(self):
         with self.assertRaises(ValueError):
