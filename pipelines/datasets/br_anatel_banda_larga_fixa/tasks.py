814ef881fe4802298f99f8d89adc0fe89288b71a
tricktx <patrick.tx@hotmail.com.br>
2023-04-01 11:54:46 -0300

    update comments br_anatel_banda_larga_fixa

--- pipelines/datasets/br_anatel_banda_larga_fixa/tasks.py
+++ pipelines/datasets/br_anatel_banda_larga_fixa/tasks.py
@@ -32,30 +32,33 @@ from prefect import task
     retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
 )
 def to_partitions(df: pd.DataFrame, partition_columns: list[str], savepath: str):
-    """Save data in to hive patitions schema, given a dataframe and a list of partition columns.
-    Args:
-        data (pandas.core.frame.DataFrame): Dataframe to be partitioned.
-        partition_columns (list): List of columns to be used as partitions.
-        savepath (str, pathlib.PosixPath): folder path to save the partitions
-    Exemple:
-        data = {
-            "ano": [2020, 2021, 2020, 2021, 2020, 2021, 2021,2025],
-            "mes": [1, 2, 3, 4, 5, 6, 6,9],
-            "sigla_uf": ["SP", "SP", "RJ", "RJ", "PR", "PR", "PR","PR"],
-            "dado": ["a", "b", "c", "d", "e", "f", "g",'h'],
-        }
-        to_partitions(
-            data=pd.DataFrame(data),
-            partition_columns=['ano','mes','sigla_uf'],
-            savepath='partitions/'
-        )
+    """ # ! Salve os dados no esquema de partições do hive, dado um dataframe e uma lista de colunas de partição.
+
+    # * Argumentos:
+
+        # ! data (pandas.core.frame.DataFrame): Dataframe a ser particionado.
+        # ! partition_columns (list): Lista de colunas a serem usadas como partições.
+        # ! savepath (str, pathlib.PosixPath): caminho da pasta para salvar as partições.
+
+    # * Exemple:
+
+        # ! data = {
+        # !    "ano": [2020, 2021, 2020, 2021, 2020, 2021, 2021,2025],
+        # !    "mes": [1, 2, 3, 4, 5, 6, 6,9],
+        # !    "sigla_uf": ["SP", "SP", "RJ", "RJ", "PR", "PR", "PR","PR"],
+        # !    "dado": ["a", "b", "c", "d", "e", "f", "g",'h']}
+
+        # ! to_partitions(
+        # !    data=pd.DataFrame(data),
+        # !    partition_columns=['ano','mes','sigla_uf'],
+        # !    savepath='partitions/')
     """
 
     if isinstance(df, (pd.core.frame.DataFrame)):
 
         savepath = Path(savepath)
 
-        # create unique combinations between partition columns
+        # ! criar combinações únicas entre colunas de partição
         unique_combinations = (
             df[partition_columns]
             .drop_duplicates(subset=partition_columns)
@@ -77,12 +80,12 @@ def to_partitions(df: pd.DataFrame, partition_columns: list[str], savepath: str)
             ]
             df_filter = df_filter.drop(columns=partition_columns)
 
-            # create folder tree
+            # ! criar árvore de pastas
             filter_save_path = Path(savepath / "/".join(patitions_values))
             filter_save_path.mkdir(parents=True, exist_ok=True)
             file_filter_save_path = Path(filter_save_path) / "microdados.csv"
 
-            # append data to csv
+            # ! anexar dados ao csv
             df_filter.to_csv(
                 file_filter_save_path,
                 index=False,
@@ -92,7 +95,7 @@ def to_partitions(df: pd.DataFrame, partition_columns: list[str], savepath: str)
             )
 
     else:
-        raise BaseException("Data need to be a pandas DataFrame")
+        raise BaseException("Os dados precisam ser um DataFrame do pandas")
 
 
 @task(
@@ -101,16 +104,17 @@ def to_partitions(df: pd.DataFrame, partition_columns: list[str], savepath: str)
 )
 def check_and_create_column(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
     """
-    Verifique se existe uma coluna em um Pandas DataFrame. Caso contrário, crie uma nova coluna com o nome fornecido
-    e preenchê-lo com valores NaN. Se existir, não faça nada.
+    # ! Verifique se existe uma coluna em um Pandas DataFrame. Caso contrário, crie uma nova coluna com o nome fornecido
+    # ! e preenchê-lo com valores NaN. Se existir, não faça nada.
 
-    Parâmetros:
-    df (Pandas DataFrame): O DataFrame a ser verificado.
-    col_name (str): O nome da coluna a ser verificada ou criada.
+    # * Parâmetros:
+    # ! df (Pandas DataFrame): O DataFrame a ser verificado.
+    # ! col_name (str): O nome da coluna a ser verificada ou criada.
 
-    Retorna:
-    Pandas DataFrame: O DataFrame modificado.
+    # * Retorna:
+    # ! Pandas DataFrame: O DataFrame modificado.
     """
+
     if col_name not in df.columns:
         df[col_name] = ""
     return df
@@ -121,31 +125,45 @@ def check_and_create_column(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
     retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
 )
 def treatment():
-    os.makedirs("/tmp/data/input", exist_ok=True)
+
+    # ! Crio os diretórios de entrada 
+    os.makedirs("/tmp/data/input", exist_ok=True) 
+
+    # ! url do arquivo zip e baixo os arquivos
     url = "https://www.anatel.gov.br/dadosabertos/paineis_de_dados/acessos/acessos_banda_larga_fixa.zip"
     response = requests.get(url)
 
+    # ! Abrindo o arquivo zip
     with open("/tmp/data/input/acessos_banda_larga_fixa.zip", "wb") as zip_file:
         zip_file.write(response.content)
 
+    # ! Descompactando o arquivo zip
     with zipfile.ZipFile(
-        "/tmp/data/input/acessos_banda_larga_fixa.zip", "r"
-    ) as zip_ref:
+        "/tmp/data/input/acessos_banda_larga_fixa.zip", "r") as zip_ref:
         zip_ref.extractall("/tmp/data/input")
 
+    # ! Criando o caminho para o arquivo zipado
     pasta = "/tmp/data/input"
     banda_larga = os.path.join(pasta, "acessos_banda_larga_fixa.zip")
 
-    # anos = ['2007-2010', '2011-2012', '2013-2014', '2015-2016', '2017-2018', '2019-2020', '2021', '2022', '2023']
+
+    # anos = ['2007-2010', '2011-2012', '2013-2014', '2015-2016', '2017-2018', '2019-2020', '2021', '2022', '2023'] # ! Lista de anos a serem processados
     anos = ["2007-2010"]
-    with ZipFile(banda_larga) as z:
-        for ano in anos:
+    # ! Abrindo o arquivo zipado
+    with ZipFile(banda_larga) as z: 
 
-            with z.open(f"Acessos_Banda_Larga_Fixa_{ano}.csv") as f:
+        # ! Iterando sobre a lista de anos
+        for ano in anos: 
+
+            # ! Abrindo o arquivo csv dentro do zip pelo ano
+            with z.open(f"Acessos_Banda_Larga_Fixa_{ano}.csv") as f: 
+
+                # ! Lendo o arquivo csv
                 df = pd.read_csv(f, sep=";", encoding="utf-8")
-                df = check_and_create_column.run(df, "Tipo de Produto")
+                # ! Fazendo referencia a função criada anteriormente para verificar colunas
+                df = check_and_create_column.run(df, "Tipo de Produto") 
 
-                # ! A partir do ano de 2021, há uma nova coluna chamada 'Tipo de Produto'
+                # ! Renomeando as colunas
                 df.rename(
                     columns={
                         "Ano": "ano",
@@ -167,9 +185,10 @@ def treatment():
                     inplace=True,
                 )
 
+                # ! Removendo colunas "grupo_economico" e "municipio"
                 # organização das variáveis
                 df.drop(["grupo_economico", "municipio"], axis=1, inplace=True)
-
+                # ! Reordenando as colunas
                 df = df[
                     [
                         "ano",
@@ -186,7 +205,7 @@ def treatment():
                         "acessos",
                     ]
                 ]
-
+                # ! Classificação do DataFrame em ordem crescente
                 df.sort_values(
                     [
                         "ano",
@@ -202,28 +221,21 @@ def treatment():
                     ],
                     inplace=True,
                 )
-
+                # ! substituindo valores nulos por vazio
                 df.replace(np.nan, "", inplace=True)
-                df["transmissao"] = df["transmissao"].apply(
-                    lambda x: x.replace("Cabo Metálico", "Cabo Metalico")
-                )
-                df["transmissao"] = df["transmissao"].apply(
-                    lambda x: x.replace("Satélite", "Satelite")
-                )
-                df["transmissao"] = df["transmissao"].apply(
-                    lambda x: x.replace("Híbrido", "Hibrido")
-                )
-                df["transmissao"] = df["transmissao"].apply(
-                    lambda x: x.replace("Fibra Óptica", "Fibra Optica")
-                )
-                df["transmissao"] = df["transmissao"].apply(
-                    lambda x: x.replace("Rádio", "Radio")
-                )
-
+                # ! Retirando os acentos da coluna "transmissao"
+                df['transmissao'] = df['transmissao'].apply(lambda x: x.replace('Cabo Metálico', 'Cabo Metalico')
+                                                            .replace('Satélite', 'Satelite')
+                                                            .replace('Híbrido', 'Hibrido')
+                                                            .replace('Fibra Óptica', 'Fibra Optica')
+                                                            .replace('Rádio', 'Radio'))
+                
+                # ! Fazendo referencia a função criada anteriormente para particionar o arquivo o arquivo
                 to_partitions.run(
                     df=df,
                     partition_columns=["ano", "mes", "sigla_uf"],
                     savepath="/tmp/data/microdados.csv",
                 )
-
+                
+    # ! retornando o caminho do path
     return "/tmp/data/microdados.csv"
