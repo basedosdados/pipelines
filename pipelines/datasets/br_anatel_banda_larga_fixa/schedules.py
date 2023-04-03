f476c14f1b159c62eb6a9a1c443cbb25cdcfa407
tricktx <patrick.tx@hotmail.com.br>
2023-04-03 15:43:57 -0300

    fix: update to cron schedule #2

--- pipelines/datasets/br_anatel_banda_larga_fixa/schedules.py
+++ pipelines/datasets/br_anatel_banda_larga_fixa/schedules.py
@@ -11,8 +11,8 @@ from pipelines.constants import constants
 
 every_month_anatel = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=30),  # ! Intervalo de tempo entre as execuções
+        CronClock(
+            cron='00 05 28 * *',  # ! Intervalo de tempo entre as execuções
             start_date=datetime(2023, 4, 3, 7, 0, 0),  # ! Data de início da execução
             labels=[
                 constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
