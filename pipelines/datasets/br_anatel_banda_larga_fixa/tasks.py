40b3e352152feaaa4797298aa27f8c135c2da318
tricktx <patrick.tx@hotmail.com.br>
2023-04-04 14:32:54 -0300

    fix: path

--- pipelines/datasets/br_anatel_banda_larga_fixa/tasks.py
+++ pipelines/datasets/br_anatel_banda_larga_fixa/tasks.py
@@ -243,8 +243,8 @@ def treatment():
                 to_partitions.run(
                     df=df,
                     partition_columns=["ano", "mes", "sigla_uf"],
-                    savepath="/tmp/data/output/",
+                    savepath="/tmp/data/microdados.csv",
                 )
 
     # ! retornando o caminho do path
-    return "/tmp/data/output/"
+    return "/tmp/data/microdados.csv"
