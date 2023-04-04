97ee8b22789feeb85a785132df68f2d4fa79dac8
folhesgabriel <pisagabriel09@gmail.com>
2023-04-04 10:22:08 -0300

    fix: change constants file paths

--- pipelines/datasets/br_bcb_estban/constants.py
+++ pipelines/datasets/br_bcb_estban/constants.py
@@ -17,7 +17,7 @@ class constants(Enum):  # pylint: disable=c0103
     ESTBAN_URL = "https://www4.bcb.gov.br/fis/cosif/estban.asp?frame=1"
     AGENCIA_XPATH = '//*[@id="ESTBAN_MUNICIPIO"]'
     MUNICIPIO_XPATH = '//*[@id="ESTBAN_AGENCIA"]'
-    DOWNLOAD_PATH_MUNICIPIO = "/tmp/input/municipio"
-    DOWNLOAD_PATH_AGENCIA = "/tmp/input/agencia"
-    CLEANED_FILES_PATH_MUNICIPIO = "/tmp/data/output/municipio"
-    CLEANED_FILES_PATH_AGENCIA = "/tmp/data/output/agencia"
+    DOWNLOAD_PATH_MUNICIPIO = "/tmp/input/municipio/"
+    DOWNLOAD_PATH_AGENCIA = "/tmp/input/agencia/"
+    CLEANED_FILES_PATH_MUNICIPIO = "/tmp/data/output/municipio/"
+    CLEANED_FILES_PATH_AGENCIA = "/tmp/data/output/agencia/"
