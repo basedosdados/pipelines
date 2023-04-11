4ee136f90ba6c95628c8d5073859fa9035d861a5
tricktx <patrick.tx@hotmail.com.br>
2023-04-11 18:08:03 -0300

    fix: init

--- pipelines/datasets/__init__.py
+++ pipelines/datasets/__init__.py
@@ -6,6 +6,7 @@ Prefect flows for basedosdados project
 # Automatically managed, please do not touch
 ###############################################################################
 from pipelines.datasets.br_anatel_banda_larga_fixa.flows import * 
+from pipelines.datasets.br_bcb_estban.flows import *
 from pipelines.datasets.br_cvm_administradores_carteira.flows import *
 from pipelines.datasets.br_cvm_oferta_publica_distribuicao.flows import *
 from pipelines.datasets.br_ibge_inpc.flows import *
@@ -14,7 +15,6 @@ from pipelines.datasets.br_ibge_ipca15.flows import *
 from pipelines.datasets.br_sp_saopaulo_dieese_icv.flows import *
 from pipelines.datasets.br_bd_indicadores.flows import *
 from pipelines.datasets.br_bd_metadados.flows import *
-from pipelines.datasets.br_bcb_estban.flows import *
 from pipelines.datasets.br_poder360_pesquisas.flows import *
 from pipelines.datasets.botdosdados.flows import *
 from pipelines.datasets.br_cgu_pessoal_executivo_federal.flows import *
