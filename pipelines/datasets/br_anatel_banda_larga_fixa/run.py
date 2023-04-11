74acb52c8cb4d5819e50d5f96e41240895d1c681
tricktx <patrick.tx@hotmail.com.br>
2023-04-11 18:42:20 -0300

    fix: test

--- pipelines/datasets/br_anatel_banda_larga_fixa/run.py
+++ pipelines/datasets/br_anatel_banda_larga_fixa/run.py
@@ -7,19 +7,8 @@ from pipelines.datasets.br_anatel_banda_larga_fixa.flows import (
     br_anatel_densidade_municipio,
 )
 
-"""run_cloud(
-    br_anatel,  # ! O flow que você deseja executar
-    labels=[
-        "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
-    ],
-    parameters={  # ! True como bool e não como string!
-        "materialize_after_dump": False,  # ! Parâmetros que serão passados para a pipeline (opcional)
-    },
-)"""
-
-
 run_cloud(
-    br_anatel_densidade_br,  # ! O flow que você deseja executar
+    br_anatel,  # ! O flow que você deseja executar
     labels=[
         "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
     ],
@@ -30,6 +19,17 @@ run_cloud(
 
 
 """run_cloud(
+    br_anatel_densidade_br,  # ! O flow que você deseja executar
+    labels=[
+        "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
+    ],
+    parameters={  # ! True como bool e não como string!
+        "materialize_after_dump": False,  # ! Parâmetros que serão passados para a pipeline (opcional)
+    },
+)
+
+
+run_cloud(
     br_anatel_densidade_uf,  # ! O flow que você deseja executar
     labels=[
         "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
