0f895c9db1742bbe23774cf883ec68a7e9c2f927
tricktx <patrick.tx@hotmail.com.br>
2023-04-12 23:18:19 -0300

    fix: ajustes na materialização.

--- pipelines/datasets/br_anatel_banda_larga_fixa/run.py
+++ pipelines/datasets/br_anatel_banda_larga_fixa/run.py
@@ -13,7 +13,7 @@ run_cloud(
         "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
     ],
     parameters={  # ! True como bool e não como string!
-        "materialize_after_dump": False,  # ! Parâmetros que serão passados para a pipeline (opcional)
+        "materialize_after_dump": True,  # ! Parâmetros que serão passados para a pipeline (opcional)
     },
 )
 
@@ -24,7 +24,7 @@ run_cloud(
         "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
     ],
     parameters={  # ! True como bool e não como string!
-        "materialize_after_dump": False,  # ! Parâmetros que serão passados para a pipeline (opcional)
+        "materialize_after_dump": True,  # ! Parâmetros que serão passados para a pipeline (opcional)
     },
 )
 
@@ -35,7 +35,7 @@ run_cloud(
         "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
     ],
     parameters={  # ! True como bool e não como string!
-        "materialize_after_dump": False,  # ! Parâmetros que serão passados para a pipeline (opcional)
+        "materialize_after_dump": True,  # ! Parâmetros que serão passados para a pipeline (opcional)
     },
 )
 
@@ -45,6 +45,6 @@ run_cloud(
         "basedosdados-dev",  # ! Label para identificar o agente que irá executar a pipeline (ex: basedosdados-dev)
     ],
     parameters={  # ! True como bool e não como string!
-        "materialize_after_dump": False,  # ! Parâmetros que serão passados para a pipeline (opcional)
+        "materialize_after_dump": True,  # ! Parâmetros que serão passados para a pipeline (opcional)
     },
 )
