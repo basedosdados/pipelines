42b07d6257f6e027623827ea96f3ea9b0167ae82
Tamir <tamtamireinhorn@gmail.com>
2023-04-20 04:41:30 -0300

    Better handling of directory changes

--- br_denatran_frota/code/download_frota.py
+++ br_denatran_frota/code/download_frota.py
@@ -75,8 +75,6 @@ def make_filename(i: dict, ext: bool = True) -> str:
         filename += f".{filetype}"
     return filename
 
-
-def handle_compact(i):
     print(2)
 
 
@@ -143,24 +141,29 @@ def download_frota(month: int, year: int, temp_dir: str = ""):
     Raises:
         ValueError: Errors if the month is not a valid one.
     """
-
     if month not in MONTHS.values():
         raise ValueError("Mês inválido.")
 
     dir_list = glob.glob(f"**/{DATASET}", recursive=True)
+    # Get the directory where this Python file is located
+    initial_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the path to the "files" directory relative to this directory
+    files_dir = os.path.join(initial_dir, "..", "files")
     if dir_list:
-        # I always want to be in the actual folder for this dataset:
+        # I always want to be in the actual folder for this dataset, because I might start in the pipelines full repo:
         os.chdir(dir_list[0])
     if temp_dir:
         os.chdir(temp_dir)
     # I always need a files directory inside my dataset folder.
-    make_dir_when_not_exists("files")
+    make_dir_when_not_exists(files_dir)
+    # I should always switch to the files dir now and save stuff inside it.
+    os.chdir(files_dir)
     year_dir_name = f"{year}"
-    desired_dir_name = os.path.join(os.getcwd(), temp_dir, "files", year_dir_name)
 
     # Create dir for that specific year should it be necessary.
-    make_dir_when_not_exists(desired_dir_name)
-    os.chdir(desired_dir_name)
+    make_dir_when_not_exists(year_dir_name)
+    os.chdir(year_dir_name)
     if year > 2012:
         download_post_2012(month, year)
     else:
@@ -170,3 +173,10 @@ def download_frota(month: int, year: int, temp_dir: str = ""):
         urlretrieve(url, generic_zip_filename)
         with ZipFile(generic_zip_filename) as zip_file:
             zip_file.extractall()
+    os.chdir(initial_dir)
+
+
+year = 2022
+if __name__ == "__main__":
+    for month in range(1, 6):
+        download_frota(month, year)
