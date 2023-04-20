427ae9700de3e132da2fb5b2440cd8056105cde9
Tamir <tamtamireinhorn@gmail.com>
2023-04-20 07:32:41 -0300

    Revert "Revert bugged discovery"
    This reverts commit 5aab93c98c120fe23531f1f1d0dd4be886cc232e.

--- br_denatran_frota/code/test/download_frota_unit_tests.py
+++ br_denatran_frota/code/test/download_frota_unit_tests.py
@@ -67,34 +67,9 @@ class TestMakeDirWhenNotExists(unittest.TestCase):
 
 
 class TestDownloadFrota(unittest.TestCase):
-    def setUp(self):
-        file_dir = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(file_dir)
-        self.temp_dir = tempfile.TemporaryDirectory(dir=file_dir)
-
-    def tearDown(self):
-        print("Deleting temporary directory")
-        shutil.rmtree(self.temp_dir.name)
-
     def test_download_frota_with_invalid_month(self):
         with self.assertRaises(ValueError):
             download_frota(13, 2013)
-
-    @parameterized.expand(
-        [(month, year) for year in range(2013, 2014) for month in range(1, 4)],
-        name_func=custom_name_func,
-    )
-    def test_download_post_2012(self, month, year):
-        download_frota(month, year, self.temp_dir.name)
-        expected_files = {
-            f"frota_por_uf_e_tipo_de_veículo_{month}-{year}",
-            f"frota_por_município_e_tipo_{month}-{year}",
-        }
-        list_of_files = os.listdir(
-            os.path.join(DATASET, self.temp_dir.name, "files", f"{year}")
-        )
-        files = set(os.path.splitext(file)[0] for file in list_of_files)
-        self.assertEqual(files, expected_files)
 
 
 if __name__ == "__main__":
