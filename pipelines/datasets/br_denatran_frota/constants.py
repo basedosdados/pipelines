635c61f6d37eddc882e0c3e6f1684f4e80f10cac
Tamir <tamtamireinhorn@gmail.com>
2023-04-23 05:26:55 -0300

    More constants

--- pipelines/datasets/br_denatran_frota/constants.py
+++ pipelines/datasets/br_denatran_frota/constants.py
@@ -72,3 +72,20 @@ class constants(Enum):  # pylint: disable=c0103
     REGRAS = {
         ("RN", "ASSU"): "açu",
     }
+
+    MONTHS = {
+        "janeiro": 1,
+        "fevereiro": 2,
+        "marco": 3,
+        "abril": 4,
+        "maio": 5,
+        "junho": 6,
+        "julho": 7,
+        "agosto": 8,
+        "setembro": 9,
+        "outubro": 10,
+        "novembro": 11,
+        "dezembro": 12,
+    }
+
+    DATASET = "br_denatran_frota"
