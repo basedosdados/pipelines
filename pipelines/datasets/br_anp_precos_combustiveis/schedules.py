f8d1090b7973d8d09a91f2b980e2738cde4b714f
Mauricio Fagundes <mauricio.fagundes@gmail.com>
2023-05-08 15:30:59 -0300

    minor fixes

--- pipelines/datasets/br_anp_precos_combustiveis/schedules.py
+++ pipelines/datasets/br_anp_precos_combustiveis/schedules.py
@@ -4,17 +4,19 @@ Schedules for br_anp_precos_combustiveis
 """
 
 from datetime import timedelta, datetime
+
 from prefect.schedules import Schedule
 from prefect.schedules.clocks import IntervalClock
+
 from pipelines.constants import constants
 
-every_two_weeks = Schedule(
+every_week_anp_microdados = Schedule(
     clocks=[
         IntervalClock(
-            interval=timedelta(weeks=2),
+            interval=timedelta(weeks=1),
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.BR_ANP_PRECOS_COMBUSTIVEIS_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
         ),
     ]
