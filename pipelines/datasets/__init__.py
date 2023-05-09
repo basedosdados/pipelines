2864d46ae195f7a451b0eb473e0c52f7917a7073
tricktx <patrick.tx@hotmail.com.br>
2023-05-09 09:57:19 -0300

    fix: __init__.py

--- pipelines/datasets/__init__.py
+++ pipelines/datasets/__init__.py
@@ -29,3 +29,4 @@ from pipelines.datasets.br_ibge_pnadc.flows import *
 from pipelines.datasets.cross_update.flows import *
 from pipelines.datasets.br_rj_isp_estatisticas_seguranca.flows import *
 from pipelines.datasets.br_anatel_banda_larga_fixa.flows import *
+from pipelines.datasets.br_rj_isp_estatisticas_seguranca.flows import *
