62a8086fe74421ef02fe2da2f1a028558f7b77d4
folhesgabriel <pisagabriel09@gmail.com>
2023-05-11 14:13:34 -0300

    fix: flow

--- pipelines/datasets/br_me_comex_stat/flows.py
+++ pipelines/datasets/br_me_comex_stat/flows.py
@@ -25,12 +25,13 @@ from pipelines.utils.tasks import (
     rename_current_flow_run_dataset_table,
     get_current_flow_labels,
 )
-from pipelines.datasets.br_me_comex_stat.schedules import (
-    schedule_municipio_exportacao,
-    schedule_municipio_importacao,
-    schedule_ncm_exportacao,
-    schedule_ncm_importacao,
-)
+
+# from pipelines.datasets.br_me_comex_stat.schedules import (
+#    schedule_municipio_exportacao,
+#    schedule_municipio_importacao,
+#    schedule_ncm_exportacao,
+#    schedule_ncm_importacao,
+# )
 
 
 with Flow(
@@ -112,7 +113,7 @@ br_comex_municipio_exportacao.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_comex_municipio_exportacao.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
-br_comex_municipio_exportacao.schedule = schedule_municipio_exportacao
+# br_comex_municipio_exportacao.schedule = schedule_municipio_exportacao
 
 
 with Flow(
@@ -194,7 +195,7 @@ br_comex_municipio_importacao.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_comex_municipio_importacao.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
-br_comex_municipio_importacao.schedule = schedule_municipio_importacao
+# br_comex_municipio_importacao.schedule = schedule_municipio_importacao
 
 
 with Flow(
@@ -274,7 +275,7 @@ with Flow(
 
 br_comex_ncm_exportacao.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_comex_ncm_exportacao.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
-br_comex_ncm_exportacao.schedule = schedule_ncm_exportacao
+# br_comex_ncm_exportacao.schedule = schedule_ncm_exportacao
 
 
 with Flow(
@@ -354,4 +355,4 @@ with Flow(
 
 br_comex_ncm_importacao.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_comex_ncm_importacao.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
-br_comex_ncm_importacao.schedule = schedule_ncm_importacao
+# br_comex_ncm_importacao.schedule = schedule_ncm_importacao
