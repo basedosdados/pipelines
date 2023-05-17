048e5e5e607b017486953eec6bd63651f53d83e9
Tamir <tamtamireinhorn@gmail.com>
2023-05-16 22:38:38 -0300

    Create first flow

--- pipelines/datasets/br_denatran_frota/flows.py
+++ pipelines/datasets/br_denatran_frota/flows.py
@@ -60,7 +60,7 @@ Flows for br_denatran_frota
 from datetime import datetime, timedelta
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
-from prefect import Parameter, case, Flow
+from prefect import Parameter, case
 from prefect.tasks.prefect import (
     create_flow_run,
     wait_for_flow_run,
@@ -69,49 +69,95 @@ from prefect.tasks.prefect import (
 from pipelines.constants import constants as pipelines_constants
 from pipelines.utils.constants import constants as utils_constants
 
-# from pipelines.utils.decorators import Flow
+from pipelines.utils.decorators import Flow
 from pipelines.utils.execute_dbt_model.constants import constants as dump_db_constants
 from pipelines.utils.tasks import (
     create_table_and_upload_to_gcs,
     rename_current_flow_run_dataset_table,
     get_current_flow_labels,
 )
-from pipelines.datasets.br_denatran_frota.tasks import crawl_task, treat_uf_tipo_task
+from pipelines.datasets.br_denatran_frota.tasks import (
+    crawl_task,
+    treat_uf_tipo_task,
+    output_file_to_csv_task,
+    get_desired_file_task,
+)
 from pipelines.datasets.br_denatran_frota.constants import constants
 
-download_path = constants.DOWNLOAD_PATH.value
+
 # from pipelines.datasets.br_denatran_frota.schedules import every_two_weeks
 
 with Flow(
     name="br_denatran_frota.uf_tipo",
-    # code_owners=[
-    #     "Tamir",
-    # ],
+    code_owners=[
+        "Tamir",
+    ],
 ) as br_denatran_frota_uf_tipo:
-    # dataset_id = Parameter("dataset_id", default="br_denatran_frota")
-    # table_id = Parameter("table_id", default="uf_tipo")
+    dataset_id = Parameter("dataset_id", default="br_denatran_frota")
+    table_id = Parameter("table_id", default="uf_tipo")
 
-    # # Materialization mode
-    # materialization_mode = Parameter(
-    #     "materialization_mode", default="dev", required=False
-    # )
+    # Materialization mode
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
 
-    # materialize_after_dump = Parameter(
-    #     "materialize after dump", default=False, required=False
-    # )
+    materialize_after_dump = Parameter(
+        "materialize after dump", default=False, required=False
+    )
 
-    # dbt_alias = Parameter("dbt_alias", default=True, required=False)
+    dbt_alias = Parameter("dbt_alias", default=True, required=False)
 
-    # rename_flow_run = rename_current_flow_run_dataset_table(
-    #     prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
-    # )
-    crawled = crawl_task(
-        month=2, year=2021, temp_dir=download_path
-    )  # Download the desired files.
-    uf_tipo_file = "/home/tamir/basedosdados/pipelines/pipelines/datasets/br_denatran_frota/tmp/input/files/2021/frota_por_uf_e_tipo_de_veículo_2-2021.xls"
-    df = treat_uf_tipo_task(file=uf_tipo_file, upstream_tasks=[crawled])
+    rename_flow_run = rename_current_flow_run_dataset_table(
+        prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
+    )
+    # Download the file:
+    crawled = crawl_task(month=2, year=2021, temp_dir=constants.DOWNLOAD_PATH.value)
+    # Now get the downloaded file:
+    uf_tipo_file = get_desired_file_task(
+        year=2021,
+        download_directory=constants.DOWNLOAD_PATH.value,
+        filetype=constants.UF_TIPO_BASIC_FILENAME.value,
+        upstream_tasks=[crawled],
+    )
+    df = treat_uf_tipo_task(file=uf_tipo_file, upstream_tasks=[uf_tipo_file])
+    csv_output = output_file_to_csv_task(df, upstream_tasks=[df])
+    wait_upload_table = create_table_and_upload_to_gcs(
+        data_path=csv_output,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode="append",
+        wait=csv_output,
+    )
 
-    print("foi?")
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=pipelines_constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
 
 br_denatran_frota_uf_tipo.storage = GCS(pipelines_constants.GCS_FLOWS_BUCKET.value)
 br_denatran_frota_uf_tipo.run_config = KubernetesRun(
