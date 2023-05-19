94cde79f9f48554e109d7dac932f47c6814c4d30
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2023-05-19 16:10:18 -0300

    fix: constants_ontem_1

--- pipelines/datasets/br_b3_cotacoes/constants.py
+++ pipelines/datasets/br_b3_cotacoes/constants.py
@@ -9,9 +9,10 @@ class constants(Enum):  # pylint: disable=c0103
     """
 
     data = datetime.now().strftime("%d-%m-%Y")
-    ontem = datetime.now() - timedelta(days=1).strftime("%d-%m-%Y")
+    ontem = datetime.now() - timedelta(days=1)
+    ontem_dia = ontem.strftime("%d-%m-%Y")
     ontem_url = ontem.strftime("%Y-%m-%d")
     B3_URL = f"https://arquivos.b3.com.br/apinegocios/tickercsv/{ontem_url}"
     B3_PATH_INPUT = "/tmp/input/br_b3_cotacoes"
-    B3_PATH_OUTPUT_DF = f"/tmp/input/br_b3_cotacoes/{ontem}_NEGOCIOSAVISTA.txt"
+    B3_PATH_OUTPUT_DF = f"/tmp/input/br_b3_cotacoes/{ontem_dia}_NEGOCIOSAVISTA.txt"
     B3_PATH_OUTPUT = "/tmp/output/br_b3_cotacoes"
