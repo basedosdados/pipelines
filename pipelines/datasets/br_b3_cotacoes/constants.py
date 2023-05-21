3f242ddb46d91700c306a79393bccd1290071a93
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2023-05-21 15:06:42 -0300

    fix: constants

--- pipelines/datasets/br_b3_cotacoes/constants.py
+++ pipelines/datasets/br_b3_cotacoes/constants.py
@@ -12,7 +12,7 @@ class constants(Enum):  # pylint: disable=c0103
     # ontem = datetime.now() - timedelta(days=1)
     # ontem_dia = ontem.strftime("%d-%m-%Y")
     # ontem_url = ontem.strftime("%Y-%m-%d")
-    B3_URL = f"https://arquivos.b3.com.br/apinegocios/tickercsv/2023-05-19"
+    B3_URL = "https://arquivos.b3.com.br/apinegocios/tickercsv/2023-05-19"
     B3_PATH_INPUT = "/tmp/input/br_b3_cotacoes"
-    B3_PATH_OUTPUT_DF = f"/tmp/input/br_b3_cotacoes/19-05-2023_NEGOCIOSAVISTA.txt"
+    B3_PATH_OUTPUT_DF = "/tmp/input/br_b3_cotacoes/19-05-2023_NEGOCIOSAVISTA.txt"
     B3_PATH_OUTPUT = "/tmp/output/br_b3_cotacoes"
