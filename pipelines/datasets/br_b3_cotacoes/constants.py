1df201d3b960765a5b91428b0fe65af57f26ca45
tricktx <patrick.tx@hotmail.com.br>
2023-05-29 17:44:11 -0300

    fix: constants

--- pipelines/datasets/br_b3_cotacoes/constants.py
+++ pipelines/datasets/br_b3_cotacoes/constants.py
@@ -8,9 +8,9 @@ class constants(Enum):  # pylint: disable=c0103
     Constant values for the br_b3_cotacoes project
     """
 
-    # data = datetime.now().strftime("%d-%m-%Y")
-    # data_nova = datetime.now().strf("%Y-%m-%d")
-    B3_URL = "https://arquivos.b3.com.br/apinegocios/tickercsv/2023-05-26"
+    data = datetime.now().strftime("%d-%m-%Y")
+    data_nova = datetime.now().strf("%Y-%m-%d")
+    B3_URL = f"https://arquivos.b3.com.br/apinegocios/tickercsv/{data_nova}"
     B3_PATH_INPUT = "/tmp/input/br_b3_cotacoes"
-    B3_PATH_OUTPUT_DF = "/tmp/input/br_b3_cotacoes/26-05-2023_NEGOCIOSAVISTA.txt"
+    B3_PATH_OUTPUT_DF = f"/tmp/input/br_b3_cotacoes/{data}_NEGOCIOSAVISTA.txt"
     B3_PATH_OUTPUT = "/tmp/output/br_b3_cotacoes"
