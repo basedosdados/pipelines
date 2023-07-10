e3af371863912edfcee76247e1586a44469fa5a0
tricktx <patrick.tx@hotmail.com.br>
2023-07-10 12:32:57 -0300

    test flows uf, brasil and municipio

--- pipelines/datasets/br_anatel_telefonia_movel/flows.py
+++ pipelines/datasets/br_anatel_telefonia_movel/flows.py
@@ -15,7 +15,12 @@ from pipelines.constants import constants
 from pipelines.datasets.br_anatel_telefonia_movel.constants import (
     constants as anatel_constants,
 )
-from pipelines.datasets.br_anatel_telefonia_movel.tasks import clean_csvs
+from pipelines.datasets.br_anatel_telefonia_movel.tasks import (
+    #clean_csvs,
+    clean_csv_brasil,
+    clean_csv_uf,
+    clean_csv_municipio
+)
 from pipelines.utils.decorators import Flow
 from pipelines.utils.tasks import (
     create_table_and_upload_to_gcs,
@@ -24,10 +29,13 @@ from pipelines.utils.tasks import (
 )
 
 from pipelines.datasets.br_anatel_telefonia_movel.schedules import (
-    every_month_anatel,
+    #every_month_anatel,
+    every_month_anatel_municipio,
+    every_month_anatel_uf,
+    every_month_anatel_brasil
 )
 
-with Flow(name="br_anatel_telefonia_movel", code_owners=["tricktx"]) as br_anatel:
+'''with Flow(name="br_anatel_telefonia_movel", code_owners=["tricktx"]) as br_anatel:
     # Parameters
     dataset_id = Parameter(
         "dataset_id", default="br_anatel_telefonia_movel", required=True
@@ -97,4 +105,193 @@ with Flow(name="br_anatel_telefonia_movel", code_owners=["tricktx"]) as br_anate
 
 br_anatel.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_anatel.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
-br_anatel.schedule = every_month_anatel
+br_anatel.schedule = every_month_anatel'''
+
+with Flow(
+    name="br_anatel_telefonia_movel", code_owners=["tricktx"]
+) as br_anatel_brasil:
+    # Parameters
+    dataset_id = Parameter(
+        "dataset_id", default="br_anatel_telefonia_movel", required=True
+    )
+    table_id = Parameter("table_id", default="densidade_brasil", required=True)
+
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
+    materialize_after_dump = Parameter(
+        "materialize_after_dump", default=True, required=False
+    )
+    dbt_alias = Parameter("dbt_alias", default=True, required=False)
+
+    rename_flow_run = rename_current_flow_run_dataset_table(
+        prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
+    )
+
+    filepath = clean_csv_brasil()
+
+    wait_upload_table = create_table_and_upload_to_gcs(
+        data_path=filepath,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode="overwrite",
+        wait=filepath,
+    )
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
+br_anatel_brasil.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+br_anatel_brasil.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
+br_anatel_brasil.schedule = every_month_anatel_brasil
+
+
+with Flow(name="br_anatel_telefonia_movel", code_owners=["tricktx"]) as br_anatel_uf:
+    # Parameters
+    dataset_id = Parameter(
+        "dataset_id", default="br_anatel_telefonia_movel", required=True
+    )
+    table_id = Parameter("table_id", default="densidade_uf", required=True)
+
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
+    materialize_after_dump = Parameter(
+        "materialize_after_dump", default=True, required=False
+    )
+    dbt_alias = Parameter("dbt_alias", default=True, required=False)
+
+    rename_flow_run = rename_current_flow_run_dataset_table(
+        prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
+    )
+
+    filepath = clean_csv_uf()
+
+    wait_upload_table = create_table_and_upload_to_gcs(
+        data_path=filepath,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode="overwrite",
+        wait=filepath,
+    )
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
+br_anatel_uf.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+br_anatel_uf.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
+br_anatel_uf.schedule = every_month_anatel_uf
+
+
+with Flow(
+    name="br_anatel_telefonia_movel", code_owners=["tricktx"]
+) as br_anatel_municipio:
+    # Parameters
+    dataset_id = Parameter(
+        "dataset_id", default="br_anatel_telefonia_movel", required=True
+    )
+    table_id = Parameter("table_id", default="densidade_municipio", required=True)
+
+    materialization_mode = Parameter(
+        "materialization_mode", default="dev", required=False
+    )
+    materialize_after_dump = Parameter(
+        "materialize_after_dump", default=True, required=False
+    )
+    dbt_alias = Parameter("dbt_alias", default=True, required=False)
+
+    rename_flow_run = rename_current_flow_run_dataset_table(
+        prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
+    )
+
+    filepath = clean_csv_municipio()
+
+    wait_upload_table = create_table_and_upload_to_gcs(
+        data_path=filepath,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode="append",
+        wait=filepath,
+    )
+    with case(materialize_after_dump, True):
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
+br_anatel_municipio.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+br_anatel_municipio.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
+br_anatel_municipio.schedule = every_month_anatel_municipio
