2a1d945ac5aff859eda25fa0d0b8ae29156631a0
Gagabrielle-carv <gabrielle.carvalho1@outlook.com>
2023-07-13 16:00:42 -0300

    decorador

--- pipelines/datasets/mundo_transfermarkt_competicoes/utils.py
+++ pipelines/datasets/mundo_transfermarkt_competicoes/utils.py
@@ -34,6 +34,37 @@ from bs4 import BeautifulSoup
 import requests
 import numpy as np
 import pandas as pd
+import asyncio
+
+
+def retry(content_function):
+    async def wrapper(url, attempts=2, wait_time=2, **kwargs):
+        content = None
+        count = 0
+
+        while content is None and count < attempts:
+            # print(f'Attempt {count + 1} of {attempts}')
+            headers = {"User-Agent": "Chrome/39.0.2171.95"}
+            try:
+                response = await asyncio.to_thread(
+                    requests.get, url, headers=headers, timeout=100
+                )
+            except Exception:
+                return None
+            await asyncio.sleep(wait_time)
+            soup = BeautifulSoup(response.text, "html.parser")
+            try:
+                content = content_function(soup, **kwargs)
+            except Exception:
+                if count == (attempts - 1):
+                    # Could not get content
+                    content = None
+            count += 1
+            # await asyncio.sleep(10)
+
+        return content
+
+    return wrapper
 
 
 def process_basico(df, content):
