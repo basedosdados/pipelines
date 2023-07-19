ad8e82ffd5c994bc6b569433324e09f3e7d95462
tricktx <patrick.tx@hotmail.com.br>
2023-07-19 10:30:29 -0300

    fix: get_today_date

--- pipelines/datasets/br_anatel_telefonia_movel/tasks.py
+++ pipelines/datasets/br_anatel_telefonia_movel/tasks.py
@@ -300,9 +300,10 @@ def clean_csv_municipio():
     # Retorna o caminho de saída dos dados de densidade por município
     return anatel_constants.OUTPUT_PATH_MUNICIPIO.value
 
-
+# task para retornar o ano e mes paara a atualização dos metadados.
 @task
 def get_today_date():
-    d = datetime.now()
+    
+    d = (datetime.now() - timedelta(days=60))
 
     return d.strftime("%Y-%m")
