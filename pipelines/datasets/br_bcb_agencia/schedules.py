2f0809c15c07a4664e9439d6fd2a63a0a55098d2
folhesgabriel <pisagabriel09@gmail.com>
2023-07-25 10:20:37 -0300

    fix br_bcb_agencia.agencia flow and add update_django_metadata task

--- pipelines/datasets/br_bcb_agencia/schedules.py
+++ pipelines/datasets/br_bcb_agencia/schedules.py
@@ -4,25 +4,27 @@ Schedules for br_bcb_agencia
 """
 
 from datetime import datetime
-from prefect.schedules import Schedule
+from prefect.schedules import Schedule, filters, adjustments
 from prefect.schedules.clocks import CronClock
 from pipelines.constants import constants
 
 every_month_agencia = Schedule(
     clocks=[
         CronClock(
-            cron="00 15 5 * *",
-            start_date=datetime(2007, 9, 1),
+            cron="@monthly",
+            start_date=datetime(2023, 8, 5, 0, 0),
             labels=[
-                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_DEV_AGENT_LABEL.value,
             ],
             parameter_defaults={
                 "dataset_id": "br_bcb_agencia",
                 "table_id": "agencia",
                 "materialization_mode": "prod",
-                "materialize_after_dump": True,
-                "dbt_alias": True,
+                "materialize after dump": True,
+                "dbt_alias": False,
             },
         )
-    ]
+    ],
+    filters=[filters.is_weekday],
+    adjustments=[adjustments.next_weekday],
 )
