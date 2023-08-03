24c3b1cc6cf3fd25f4271b2a529750656a632c74
tricktx <patrick.tx@hotmail.com.br>
2023-08-03 16:06:39 -0300

    register pipeline anp

---
+++ pipelines/datasets/br_anp_precos_combustiveis/constants.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Constant values for the datasets projects
+"""
+
+from enum import Enum
+
+
+class constants(Enum):  # pylint: disable=c0103
+    
+    rename = {
+        "Revenda": "nome_estabelecimento",
+        "CNPJ da Revenda": "cnpj_revenda",
+        "Bairro": "bairro_revenda",
+        "Cep": "cep_revenda",
+        "Produto": "produto",
+        "Valor de Venda": "preco_venda",
+        "Valor de Compra": "preco_compra",
+        "Unidade de Medida": "unidade_medida",
+        "Bandeira": "bandeira_revenda",
+        "Estado - Sigla": "sigla_uf",
+        "Municipio": "nome",
+        "Data da Coleta": "data_coleta",
+        "Nome da Rua": "nome_rua",
+        "Numero Rua": "numero_rua",
+        "Complemento": "complemento",
+    }
+
+    ordem = [
+        "ano",
+        "sigla_uf",
+        "id_municipio",
+        "bairro_revenda",
+        "cep_revenda",
+        "endereco_revenda",
+        "cnpj_revenda",
+        "nome_estabelecimento",
+        "bandeira_revenda",
+        "data_coleta",
+        "produto",
+        "unidade_medida",
+        "preco_compra",
+        "preco_venda",
+    ]
---
+++ pipelines/datasets/br_anp_precos_combustiveis/flows.py
@@ -0,0 +1,81 @@
+"""
+Flows for br_anp_precos_combustiveis
+"""
+
+from datetime import timedelta
+from prefect import Parameter, case
+from prefect.run_configs import KubernetesRun
+from prefect.storage import GCS
+from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
+from pipelines.constants import constants
+from pipelines.utils.constants import constants as utils_constants
+from pipelines.utils.decorators import Flow
+from pipelines.utils.execute_dbt_model.constants import constants as dump_db_constants
+from pipelines.datasets.br_anp_precos_combustiveis.tasks import (tratamento)
+from pipelines.datasets.br_anp_precos_combustiveis.schedules import (every_week_anp_microdados,)
+
+from pipelines.utils.tasks import (
+    create_table_and_upload_to_gcs,
+    rename_current_flow_run_dataset_table,
+    get_current_flow_labels,
+)
+
+with Flow(name="br_anp_precos_combustiveis.microdados", code_owners=["trick"]) as anp_microdados:
+    # Parameters
+    dataset_id = Parameter("dataset_id", default="br_anp_precos_combustiveis", required=True)
+    table_id = Parameter("table_id", default="microdados", required=True)
+
+    materialization_mode = Parameter("materialization_mode", default="dev", required=False)
+
+    materialize_after_dump = Parameter("materialize_after_dump", default=True, required=False)
+
+    dbt_alias = Parameter("dbt_alias", default=True, required=False)
+    
+    rename_flow_run = rename_current_flow_run_dataset_table(prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id)
+    update_metadata = Parameter("update_metadata", default=True, required=False)
+
+
+    filepath = tratamento()
+
+    # pylint: disable=C0103
+    wait_upload_table = create_table_and_upload_to_gcs(
+        data_path=filepath,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode="append",
+        wait=filepath,
+    )
+
+    with case(materialize_after_dump, True):
+
+        # Trigger DBT flow run
+        current_flow_labels = get_current_flow_labels()
+        materialization_flow = create_flow_run(
+            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+            parameters={
+                "dataset_id": dataset_id,
+                "table_id": table_id,
+                "mode": materialization_mode,
+                "dbt_alias": dbt_alias,
+            },
+            labels=current_flow_labels,
+            run_name=f"Materialize {dataset_id}.{table_id}",
+        )
+
+        wait_for_materialization = wait_for_flow_run(
+            materialization_flow,
+            stream_states=True,
+            stream_logs=True,
+            raise_final_state=True,
+        )
+        wait_for_materialization.max_retries = (
+            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+        )
+        wait_for_materialization.retry_delay = timedelta(
+            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+        )
+
+anp_microdados.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+anp_microdados.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
+anp_microdados.schedule = every_week_anp_microdados
