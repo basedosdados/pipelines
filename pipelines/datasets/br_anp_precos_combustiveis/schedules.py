44d5129fb980d49286b706c9cc470e8756ddbd7b
tricktx <patrick.tx@hotmail.com.br>
2023-08-11 14:58:34 -0300

    alteration labels and timedelta days 1

--- pipelines/datasets/br_anp_precos_combustiveis/schedules.py
+++ pipelines/datasets/br_anp_precos_combustiveis/schedules.py
@@ -18,14 +18,14 @@ from pipelines.constants import constants
 every_week_anp_microdados = Schedule(
     clocks=[
         IntervalClock(
-            interval=timedelta(weeks=1),
+            interval=timedelta(days=1),
             start_date=datetime(2021, 1, 1),
-            labels=[constants.BASEDOSDADOS_DEV_AGENT_LABEL.value],
+            labels=[constants.BASEDOSDADOS_PROD_AGENT_LABEL.value],
             parameter_defaults={
                 "update_metadata": True,
                 "dbt_alias": True,
                 "materialize_after_dump": True,
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "table_id": "microdados",
                 "dataset_id": "br_anp_precos_combustiveis",
             },
