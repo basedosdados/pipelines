dd1e32adaa586b142d37a83408972eb5e254f51e
Pedro Castro <aspeddro@gmail.com>
2023-08-11 14:56:05 -0300

    update cron

--- pipelines/datasets/br_mp_pep_cargos_funcoes/schedules.py
+++ pipelines/datasets/br_mp_pep_cargos_funcoes/schedules.py
@@ -11,7 +11,7 @@ from pipelines.constants import constants
 every_month = Schedule(
     clocks=[
         CronClock(
-            cron="0 0 */10 * *",  # A cada 10 dias a meia-noite
+            cron="0 14 * * 3",  # At 14:00 on Wednesday.
             start_date=datetime(2023, 8, 2, 10, 30),
             labels=[
                 constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
