f940d885aa4b97b8923ba0922093d507018de589
Tamir <tamtamireinhorn@gmail.com>
2023-08-17 02:08:21 -0300

    Adiciona particionamento

--- pipelines/datasets/br_denatran_frota/handlers.py
+++ pipelines/datasets/br_denatran_frota/handlers.py
@@ -126,7 +126,8 @@ def treat_uf_tipo(file: str) -> pl.DataFrame:
 
 def output_file_to_csv(df: pl.DataFrame, filename: str) -> None:
     make_dir_when_not_exists(OUTPUT_PATH)
-    df.write_csv(file=f"{OUTPUT_PATH}/{filename}.csv", has_header=True)
+    pd_df = df.to_pandas()
+    to_partitions(pd_df, partition_columns=["ano", "mes", "sigla_uf"], savepath=OUTPUT_PATH)
     return OUTPUT_PATH
 
 
