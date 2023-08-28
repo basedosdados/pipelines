c65882a3dcc4cdbd399a160c5482aa6d441b85c3
Gagabrielle-carv <gabrielle.carvalho1@outlook.com>
2023-08-28 11:46:16 -0300

    br_bd_diretorios_brasil

--- pipelines/datasets/br_me_cnpj/flows.py
+++ pipelines/datasets/br_me_cnpj/flows.py
@@ -282,6 +282,45 @@ with Flow(
                 time_delta=defasagem,
                 time_unit="months",
             )
+        with case(materialize_after_dump, True):
+            # Trigger DBT flow run
+            current_flow_labels = get_current_flow_labels()
+            materialization_flow = create_flow_run(
+                flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+                project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+                parameters={
+                    "dataset_id": "br_bd_diretorios_brasil",
+                    "table_id": "empresa",
+                    "mode": materialization_mode,
+                    "dbt_alias": dbt_alias,
+                },
+                labels=current_flow_labels,
+                run_name=f"Materialize {dataset_id}.{table_id}",
+            )
+
+            wait_for_materialization = wait_for_flow_run(
+                materialization_flow,
+                stream_states=True,
+                stream_logs=True,
+                raise_final_state=True,
+            )
+            wait_for_materialization.max_retries = (
+                dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+            )
+            wait_for_materialization.retry_delay = timedelta(
+                seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+            )
+
+            update_django_metadata(
+                dataset_id,
+                table_id,
+                metadata_type="DateTimeRange",
+                bq_last_update=True,
+                api_mode="prod",
+                date_format="yy-mm-dd",
+                is_bd_pro=True,
+                is_free=False,
+            )
 
 
 br_me_cnpj_estabelecimentos.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
