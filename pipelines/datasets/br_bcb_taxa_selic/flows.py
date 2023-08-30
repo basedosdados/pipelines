284172de6c9adde33ffec905020681abf7152597
Laura Amaral <laura.amaral@basedosdados.org>
2023-08-30 18:26:15 -0300

    exclude duplicated task

--- pipelines/datasets/br_bcb_taxa_selic/flows.py
+++ pipelines/datasets/br_bcb_taxa_selic/flows.py
@@ -66,19 +66,6 @@ with Flow(
         wait=file_info,
     )
 
-    update_django_metadata(
-        upstream_tasks=[wait_upload_table],
-        dataset_id=dataset_id,
-        table_id=table_id,
-        metadata_type="DateTimeRange",
-        _last_date=file_info["max_date"],
-        bq_table_last_year_month=False,
-        bq_last_update=False,
-        is_bd_pro=True,
-        is_free=False,
-        date_format="yy-mm-dd",
-        api_mode="prod",
-    )
 
     with case(materialize_after_dump, True):
         # Trigger DBT flow run
