6d255bc81b02a8dfe0b9c379f6b10044d9ecb20b
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2023-09-03 19:08:27 -0300

    Resolve problema 2

--- pipelines/datasets/br_denatran_frota/handlers.py
+++ pipelines/datasets/br_denatran_frota/handlers.py
@@ -151,7 +151,7 @@ def get_latest_data(table_name: str):
     denatran_data: pd.DataFrame = get_data_from_prod(
         table_id=table_name, dataset_id="br_denatran_frota"
     )
-    if not denatran_data:
+    if not isinstance(denatran_data, pd.DataFrame):
         return 2003, 1
     if not denatran_data.empty:
         year = denatran_data["ano"].max()
