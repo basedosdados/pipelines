baafc713d7d3160004c9a203514872f8e21f287d
tricktx <patrick.tx@hotmail.com.br>
2023-09-15 11:57:27 -0300

    ajustando schedules para todos os dias 10 da manhã

--- pipelines/datasets/br_anp_precos_combustiveis/schedules.py
+++ pipelines/datasets/br_anp_precos_combustiveis/schedules.py
@@ -5,20 +5,20 @@ Schedules for br_anp_precos_combustiveis
 
 from datetime import timedelta, datetime
 from prefect.schedules import Schedule
-from prefect.schedules.clocks import IntervalClock
+from prefect.schedules.clocks import CronClock
 from pipelines.constants import constants
 
 every_week_anp_microdados = Schedule(
     clocks=[
-        IntervalClock(
-            interval=timedelta(days=1),
+        CronClock(
+            cron="0 10 * * *",
             start_date=datetime(2021, 1, 1),
             labels=[constants.BASEDOSDADOS_PROD_AGENT_LABEL.value],
             parameter_defaults={
                 "update_metadata": True,
                 "dbt_alias": True,
                 "materialize_after_dump": True,
-                "materialization_mode": "dev",
+                "materialization_mode": "prod",
                 "table_id": "microdados",
                 "dataset_id": "br_anp_precos_combustiveis",
             },
