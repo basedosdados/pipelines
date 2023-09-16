d3e2e50a38db0f18a14288c4f9c4b469d6659e6a
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2023-09-16 05:52:21 -0300

    schedule fix

--- pipelines/datasets/br_denatran_frota/schedules.py
+++ pipelines/datasets/br_denatran_frota/schedules.py
@@ -15,7 +15,7 @@ every_two_weeks = Schedule(
             interval=timedelta(weeks=2),
             start_date=datetime(2021, 1, 1),
             labels=[
-                constants.DATASETS_AGENT_LABEL.value,
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
             ],
         ),
     ]
