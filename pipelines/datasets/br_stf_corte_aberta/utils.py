b1a3dcebec9bbb94ea2c130922db75ce660c9f59
tricktx <patrick.tx@hotmail.com.br>
2023-09-21 16:06:40 -0300

    increase time selenium

--- pipelines/datasets/br_stf_corte_aberta/utils.py
+++ pipelines/datasets/br_stf_corte_aberta/utils.py
@@ -30,15 +30,13 @@ def web_scrapping():
 
     options.add_argument("--headless=new")
     driver = webdriver.Chrome(options=options)
-    time.sleep(10)
-    driver.get(stf_constants.STF_LINK.value)
-    time.sleep(10)
+    time.sleep(50)
+    driver.get("https://transparencia.stf.jus.br/extensions/decisoes/decisoes.html")
+    time.sleep(50)
     driver.maximize_window()
-    time.sleep(10)
-    driver.find_element("xpath", '//*[@id="aba-4"]').click()
-    time.sleep(10)
+    time.sleep(50)
     driver.find_element("xpath", '//*[@id="EXPORT-BUTTON-2"]/button').click()
-    time.sleep(10)
+    time.sleep(50)
 
 
 def read_csv():
