a8b64ba309b2fe15ed9e230a559440f09337bfc6
tricktx <patrick.tx@hotmail.com.br>
2023-09-21 20:51:37 -0300

    add_argument selenium

--- pipelines/datasets/br_stf_corte_aberta/utils.py
+++ pipelines/datasets/br_stf_corte_aberta/utils.py
@@ -29,6 +29,12 @@ def web_scrapping():
     )
 
     options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--crash-dumps-dir=/tmp")
+    options.add_argument("--remote-debugging-port=9222")
+
     driver = webdriver.Chrome(options=options)
     time.sleep(50)
     driver.get("https://transparencia.stf.jus.br/extensions/decisoes/decisoes.html")
