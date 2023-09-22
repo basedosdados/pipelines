0cfa89a6bab2f2680b21211b3f61d66cb666211b
Tamir Einhorn Salem <tamtamireinhorn@gmail.com>
2023-09-22 02:59:33 -0300

    Tem algo rodando automático e eu cansei

--- pipelines/datasets/br_denatran_frota/backfill.py
+++ pipelines/datasets/br_denatran_frota/backfill.py
@@ -2,9 +2,14 @@
 from pipelines.datasets.br_denatran_frota.handlers import crawl, get_desired_file
 from pipelines.datasets.br_denatran_frota.constants import constants
 
+# Fill for UF TIPO
 months = range(1, 13)
 year = 2003
 for month in months:
     print(month)
     crawl(month=month, year=year, temp_dir="DENATRAN_FILES")
-    get_desired_file(year=year, download_directory=constants.DOWNLOAD_PATH.value)
+    file = get_desired_file(
+        year=year,
+        download_directory=constants.DOWNLOAD_PATH.value,
+        filetype=constants.UF_TIPO_BASIC_FILENAME.value,
+    )
