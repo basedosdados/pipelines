7714a72daa33ef0e90532d774946ec29f4651326
tricktx <patrick.tx@hotmail.com.br>
2023-09-26 11:02:36 -0300

    add run

--- pipelines/datasets/br_stf_corte_aberta/flows.py
+++ pipelines/datasets/br_stf_corte_aberta/flows.py
@@ -100,7 +100,7 @@ with Flow(name="br_stf_corte_aberta.decisoes", code_owners=["trick"]) as br_stf:
             wait_for_materialization.retry_delay = timedelta(
                 seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
             )
-            get_max_date = check_for_data(upstream_tasks=[wait_upload_table])
+            get_max_date = check_for_data.run()
             with case(update_metadata, True):
                 update_django_metadata(
                     dataset_id,
