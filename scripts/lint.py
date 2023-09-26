f03c60ff5e9565c733cd62ebe689d08b1cde94c1
Vinicius <vncsna@gmail.com>
2023-09-26 20:35:29 -0300

    feat: update lint python workflow

--- scripts/lint.py
+++ scripts/lint.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
-from pylint.lint import Run
+from subprocess import run as _run
+
+
+def run(*args):
+    process = _run(*args)
+    return process.returncode
 
 
 def main():
-    Run(["pipelines/"])
-
-
-if __name__ == "__main__":
-    main()
+    """Lint all python files in the project"""
+    code = 0
+    code |= run(["isort", "--check-only", "."])
+    code |= run(["black", "--check", "."])
+    code |= run(["autoflake", "--check", "--recursive", "--quiet", "."])
+    code |= run(["flake8", "."])
+    exit(code)
