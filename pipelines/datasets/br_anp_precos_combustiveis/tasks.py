a00d43f852a6d8ebb03e3dc908818b5e7a910560
tricktx <patrick.tx@hotmail.com.br>
2023-10-02 13:02:00 -0300

    date anp

--- pipelines/datasets/br_anp_precos_combustiveis/tasks.py
+++ pipelines/datasets/br_anp_precos_combustiveis/tasks.py
@@ -40,8 +40,15 @@ def check_for_updates(dataset_id, table_id):
     # Obtém a data mais recente do site
     download_files(anatel_constants.URLS_DATA.value, anatel_constants.PATH_INPUT.value)
     df = pd.read_csv(anatel_constants.URL_GLP.value, sep=";", encoding="utf-8")
-    data_obj = pd.to_datetime(df["Data da Coleta"]).max()
-    data_obj = data_obj.date()
+    data_obj = (
+        df["Data da Coleta"].str[6:10]
+        + "-"
+        + df["Data da Coleta"].str[3:5]
+        + "-"
+        + df["Data da Coleta"].str[0:2]
+    )
+
+    data_obj = pd.to_datetime(data_obj, format="%Y-%m-%d").max()
 
     # Obtém a última data no site BD
     data_bq_obj = extract_last_date(
