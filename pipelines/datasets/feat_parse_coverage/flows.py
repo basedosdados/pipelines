7184aa7eec019e6cb6c1dc7b808044f32960c1a8
folhesgabriel <pisagabriel09@gmail.com>
2023-10-30 09:15:43 -0300

    tests na cloud

--- pipelines/datasets/feat_parse_coverage/flows.py
+++ pipelines/datasets/feat_parse_coverage/flows.py
@@ -5,6 +5,7 @@ from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 
+from pipelines.constants import constants
 from pipelines.utils.decorators import Flow
 from pipelines.utils.metadata.tasks import parse_coverage
 
@@ -12,7 +13,11 @@ with Flow(name="feat_parse_coverage", code_owners=["Gabriel Pisa"]) as feat:
     # Parameters
     dataset_id = Parameter("dataset_id", default="br_bcb_sicor", required=True)
     table_id = Parameter("table_id", default="microdados_operacao", required=True)
+    date_format = Parameter("table_id", default="microdados_operacao", required=True)
 
     dados = parse_coverage(
-        dataset_id=dataset_id, table_id=table_id, date_format="yy-mm"
+        dataset_id=dataset_id, table_id=table_id, date_format=date_format
     )
+
+feat.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+feat.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
