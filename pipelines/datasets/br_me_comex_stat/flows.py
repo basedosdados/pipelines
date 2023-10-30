1b0e1efa6fa47940ea2ad2bfb75183a16ba02b03
folhesgabriel <pisagabriel09@gmail.com>
2023-10-30 10:18:22 -0300

    deleta flow de teste local commitado por engano

--- pipelines/datasets/br_me_comex_stat/flows.py
+++ pipelines/datasets/br_me_comex_stat/flows.py
@@ -25,7 +25,7 @@ from pipelines.datasets.br_me_comex_stat.tasks import (
 from pipelines.utils.constants import constants as utils_constants
 from pipelines.utils.decorators import Flow
 from pipelines.utils.execute_dbt_model.constants import constants as dump_db_constants
-from pipelines.utils.metadata.tasks import parse_coverage, update_django_metadata
+from pipelines.utils.metadata.tasks import update_django_metadata
 from pipelines.utils.tasks import (
     create_table_and_upload_to_gcs,
     get_current_flow_labels,
