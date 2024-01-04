01fb057d777db1eb756bb10711ef29652dfdf5f0
Laura Amaral <laura.amaral@basedosdados.org>
2024-01-04 15:36:41 -0300

    fix: change get_headers func file

--- pipelines/datasets/cross_update/utils.py
+++ pipelines/datasets/cross_update/utils.py
@@ -8,7 +8,7 @@ import pandas as pd
 from pandas import json_normalize
 
 from pipelines.utils.utils import get_credentials_from_secret, log
-
+from pipelines.utils.metadata import get_headers
 
 def save_file(df: pd.DataFrame, table_id: str) -> str:
     """
@@ -151,21 +151,3 @@ def modify_table_metadata(table, backend):
         log(table)
 
 
-def get_headers(backend):
-    credentials = get_credentials_from_secret(secret_path="api_user_prod")
-
-    mutation = """
-        mutation ($email: String!, $password: String!) {
-            tokenAuth(email: $email, password: $password) {
-                token
-            }
-        }
-    """
-    variables = {"email": credentials["email"], "password": credentials["password"]}
-
-    response = backend._execute_query(query=mutation, variables=variables)
-    token = response["tokenAuth"]["token"]
-
-    header_for_mutation_query = {"Authorization": f"Bearer {token}"}
-
-    return header_for_mutation_query
