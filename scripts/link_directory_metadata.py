ef2a83f4a234dc8a988c98e9b77ef0241485c1be
Laura Amaral <laura.amaral@basedosdados.org>
2024-01-04 15:37:03 -0300

    add: upload_columns script

---
+++ scripts/link_directory_metadata.py
@@ -0,0 +1,197 @@
+from pipelines.utils.metadata.utils import get_headers
+import basedosdados as bd 
+import pandas as pd
+
+def get_columns(column_name: str, backend) -> pd.DataFrame:
+    """
+        Get the id from all columns that have a certain name
+    """
+
+    query = """ query($column_name: String)
+        {allColumn(name_Icontains: $column_name) {
+                edges {
+                node {
+                    name
+                    _id
+                    table {
+                    dataset {
+                        fullSlug
+                    }
+                    slug
+                    }
+                    directoryPrimaryKey {
+                    table {
+                        dataset {
+                        fullSlug
+                        }
+                        slug
+                    }
+                    name
+                    }
+                }
+                }
+            }
+            }
+    """
+
+    variables = {"column_name":column_name}
+   
+    response  = backend._execute_query(query=query, variables = variables)
+    response = backend._simplify_graphql_response(response)["allColumn"]
+
+    df = pd.json_normalize(response)
+
+    starts_with = df['name'].str.startswith(column_name+'_')
+    ends_with = df['name'].str.endswith('_'+column_name)
+    contains = df['name'].str.contains('_'+column_name+"_")
+    is_exactly = df['name'].str == column_name
+
+    selected  = starts_with | ends_with | contains | is_exactly
+
+    df = df[selected]
+
+    return df
+
+def get_directory_column_id(directory_column_name, directory_table_name,backend) -> str:
+    """
+        Get the directory id from that column
+    """
+    query = """ query($column_name: String)
+        {allColumn(name: $column_name) {
+                edges {
+                node {
+                    name
+                    _id
+                    table {
+                    dataset {
+                        fullSlug
+                    }
+                    slug
+                    }
+                }
+                }
+            }
+            }
+    """
+
+    variables = {"column_name":directory_column_name}
+    response  = backend._execute_query(query=query, variables = variables)
+    response = backend._simplify_graphql_response(response)["allColumn"]
+    df = pd.json_normalize(response)
+
+    colunas_de_diretorio = df["table.dataset.fullSlug"].str.contains("diretorios")
+    for index, coluna in df[colunas_de_diretorio].iterrows():
+        if (coluna['table.slug'] == directory_table_name):
+            print(f"\nConnecting to the directory column: \n\t{coluna['table.dataset.fullSlug']}.{coluna['table.slug']}:{coluna['name']} ")
+            return coluna['_id']
+
+    print(f"\nWARNING - Unable to find the directory column with the following information: \n\tcolumn_name: {directory_column_name}  \n\ttable: {directory_table_name}")
+
+def modify_directory_metadata(column_id:str, directory_id:str,backend) -> None:
+    """
+        modify directory metadata
+    """
+    mutation = """
+                    mutation($input: CreateUpdateColumnInput!) {
+                        CreateUpdateColumn(input: $input) {
+                            errors {
+                                field,
+                                messages
+                            },
+                            clientMutationId,
+                            column {
+                                id,
+                            }
+                        }
+                    }
+        """
+    variables = {"id":column_id,"directoryPrimaryKey":directory_id}
+
+    headers = get_headers(backend)
+
+    backend._execute_query(query = mutation, variables = {"input":variables}, headers = headers)
+
+    return None
+
+
+def select_columns(df,not_column_name, remove_already_done = False):
+    descricao = df['name'].str.contains('descricao')
+    diretorio = df['table.dataset.fullSlug'].str.contains('diretorios')
+
+    print(f"\n Removed {descricao.sum()} description columns ")
+    print(f" Removed {diretorio.sum()} directory columns ")    
+
+    df = df[~(descricao|diretorio)]
+
+    for wrong_column_name in not_column_name:
+        contains_wrong_column_name = df['name'].str.contains(wrong_column_name)   
+        print(f" Removed {contains_wrong_column_name.sum()} columns with {wrong_column_name}")
+        df = df[~contains_wrong_column_name]     
+
+    if remove_already_done and ('directoryPrimaryKey.name' in df.columns):
+        conected = df['directoryPrimaryKey.name'] != None
+        print(f" Removed {conected.sum()} columns that are already conected to a directory")
+        df = df[~conected]
+
+    print("-------------------")
+    print(f" Remains {len(df)} columns to conect to the directory:")
+
+    df["column_identifier"] = df['table.dataset.fullSlug'].astype(str)+"-"+df['table.slug'].astype(str)+"."+df['name'].astype(str)
+
+    valores_ordenados = df['column_identifier'].sort_values()
+
+    for coluna in valores_ordenados:
+        print(f"\t- {coluna}")
+
+    return df["_id"].values.tolist()
+    
+
+
+def link_directory_metadata(
+    matching_column_pattern: str,
+    directory_column_name: str, 
+    directory_table_name: str,
+    not_matching_pattern: list,
+    ignore_previously_linked_columns: bool,
+    ready_to_change_metadata: bool
+    
+):
+    """
+    This function searches for columns within the metadata that match the specified `matching_column_pattern`. 
+    It performs filtering based on defined criteria and presents a list of selected columns.
+    If `ready_to_change_metadata` is True, modifies the metadata
+
+    Filters out columns based on the following criteria:
+        - Represents directories
+        - Serves as description columns
+        - Contains strings listed in `not_matching_pattern`
+
+    Usage Recommendations:
+    - Run the function with `ready_to_fix_metadata` set to False to:
+        - Review columns for potential connection to the directory.
+        - Validate the correctness of the directory column.
+
+    Note:
+    - The `directory_column_name` and `directory_table_name` default to `matching_column_pattern` if not provided.
+
+    """
+    backend = bd.Backend(graphql_url = "https://api.basedosdados.org/api/v1/graphql")
+
+    if directory_column_name is None:
+        directory_column_name = matching_column_pattern
+    
+    if directory_table_name is None:
+        directory_table_name = matching_column_pattern
+
+    directory_column_id = get_directory_column_id(directory_column_name, directory_table_name,backend)
+
+    df = get_columns(matching_column_pattern, backend) 
+
+    print(f"\nFound {len(df)} columns that include '{matching_column_pattern}'")
+
+    columns_list = select_columns(df,not_matching_pattern, ignore_previously_linked_columns)
+
+    if ready_to_change_metadata:
+        for column in columns_list:
+            modify_directory_metadata(column,directory_column_id,backend)
+        print(f"\n{len(columns_list)} columns were connected to directory")
---
+++ scripts/upload_columns.py
@@ -0,0 +1,237 @@
+import json
+import numpy as np
+import pandas as pd
+import requests
+from basedosdados import backend as b
+from typing import Any, Dict
+from io import StringIO
+from pipelines.utils.metadata.utils import get_headers
+
+from link_directory_metadata import get_directory_column_id
+
+def read_architecture_table(
+    url_architecture: str
+):
+    """URL contendo a tabela de arquitetura no formato da base dos dados
+
+    Args:
+        url_architecture (str): url de tabela de arquitera no padrão da base dos dados
+
+    Returns:
+        df: um df com a tabela de arquitetura
+    """
+    # Converte a URL de edição para um link de exportação em formato csv
+    url = url_architecture.replace("edit#gid=", "export?format=csv&gid=")
+
+    # Coloca a arquitetura em um dataframe
+    df_architecture = pd.read_csv(StringIO(requests.get(url, timeout=10).content.decode("utf-8")))
+
+    return df_architecture.replace(np.nan,'',regex=True)
+
+def create_column(
+        backend,
+        mutation_parameters: Dict[str, str] = None,
+    ):
+        
+    ## tinha que ser create or replace, por enquanto ele duplica os dados se rodar duas vezes por isso atenção na hora de rodar!
+
+        # GraphQL mutation to create or update a column
+        mutation = """
+                    mutation($input: CreateUpdateColumnInput!) {
+                        CreateUpdateColumn(input: $input) {
+                            errors {
+                                field,
+                                messages
+                            },
+                            clientMutationId,
+                            column {
+                                id,
+                            }
+                        }
+                    }
+        """
+        
+        # Set headers for the GraphQL request, including the token for authentication
+        headers = get_headers(backend)
+
+        # Print the mutation parameters for debugging purposes
+        pretty_json = json.dumps(mutation_parameters, indent=4)
+        print(pretty_json)
+
+        # Execute the GraphQL query with the provided mutation parameters and headers
+        response = backend._execute_query(query=mutation, variables={"input": mutation_parameters}, headers=headers)
+        
+        # Print the response for debugging purposes
+        if response["CreateUpdateColumn"]["errors"] != []:
+            pretty_json = json.dumps(response, indent=4)
+            print(pretty_json)
+
+def get_column_id(table_id, column_name,url_api):
+
+    backend = b.Backend(graphql_url=url_api)
+
+    query = f"""{{
+        allColumn(table_Id:"{table_id}", name:"{column_name}"){{
+        edges{{
+            node{{
+            _id
+            }}
+        }}
+        }}
+    }}"""
+
+    data = backend._execute_query(query=query)
+    data = backend._simplify_graphql_response(response=data)['allColumn']
+    if data:
+        return data[0]['_id']
+    else:
+        print("column does not exists")
+
+def get_n_columns(table_id,url_api):
+    backend = b.Backend(graphql_url=url_api)
+
+    query = f"""query get_n_columns{{
+        allTable(id:"{table_id}"){{
+            edges{{
+            node{{
+                columns{{
+                edgeCount
+                }}
+            }}
+            }}
+        }}
+        }}"""
+    
+    data = backend._execute_query(query=query)
+    data = backend._simplify_graphql_response(response=data)['allTable']
+
+    return (data[0]["columns"]["edgeCount"])
+        
+def get_bqtype_dict(url_api):
+  # Initialize the backend object to interact with the GraphQL API
+  backend = b.Backend(graphql_url=url_api)
+
+  # GraphQL query to fetch all BigQuery types
+  query = """{
+    allBigquerytype{
+      edges{
+        node{
+          name
+          _id
+        }
+      }
+    }
+  }"""
+
+  # Execute the GraphQL query to retrieve the data
+  data = backend._execute_query(query=query)
+
+  # Simplify the GraphQL response to extract the relevant data
+  data = backend._simplify_graphql_response(response=data)['allBigquerytype']
+
+  # Create a dictionary where the 'name' part is the key and the '_id' is the value
+  bqtype_dict = {item['name']: item['_id'] for item in data}
+
+  # Return the resulting dictionary
+  return bqtype_dict
+
+def check_metadata_columns(
+        dataset_id,
+        table_slug,
+        url_api:str,
+        url_architecture:str
+):
+
+    # Create a backend object with the GraphQL URL
+    # This will help us interact with the api
+    backend = b.Backend(graphql_url=url_api)
+
+    # Get the table ID using the dataset ID and table ID
+    table_id = backend._get_table_id_from_name(gcp_dataset_id = dataset_id, gcp_table_id = table_slug)
+
+    # Read the architecture table
+    architecture = read_architecture_table(
+        url_architecture = url_architecture)
+    
+    n_columns_metadata = get_n_columns(table_id=table_id, url_api = url_api)
+    n_columns_architecture = architecture.shape[0]
+    
+    print(f"\nn_columns_metadata: {n_columns_metadata}")
+    print(f"n_columns_architecture: {n_columns_architecture}")
+
+def upload_columns_from_architecture(
+        dataset_id: str,
+        table_slug: str,
+        url_architecture: str,
+        if_exists: str = 'pass'
+):
+    """
+    Uploads columns from an architecture table to the specified dataset and table in the Base dos Dados platform.
+
+    Notes:
+    - This function assumes a specific structure/format for the architecture table.
+    - It interacts with the Base dos Dados GraphQL API to create or update columns.
+    - Columns from the architecture table are processed and uploaded to the specified dataset and table.
+    - It prints information about the existing columns and performs metadata checks after uploading columns.
+    """
+    accepted_if_exists_values = ['pass','replace']
+
+    if if_exists not in accepted_if_exists_values:
+        raise ValueError(f"`if_exists` only accepts {accepted_if_exists_values}"
+            )
+
+    url_api = "https://api.basedosdados.org/api/v1/graphql"
+    # Create a backend object with the GraphQL URL
+    # This will help us interact with the api
+    backend = b.Backend(graphql_url=url_api)
+
+    # Get the table ID using the dataset ID and table ID
+    table_id = backend._get_table_id_from_name(gcp_dataset_id = dataset_id, gcp_table_id = table_slug)
+    print(f"table_id: {table_id}\n")
+
+    # Read the architecture table
+    architecture = read_architecture_table(
+        url_architecture = url_architecture)
+    
+    # Get the id of BigQueryTypes in a dict
+    bqtype_dict = get_bqtype_dict(url_api)
+
+    # Iterate over each row in the 'architecture' DataFrame
+    for index, row in architecture.iterrows():
+        # Define the mutation parameters for creating a new column
+        
+        column_id = get_column_id(table_id=  table_id, column_name=row["name"],url_api=url_api)
+        print(f"\nColumn: {row['name']}")
+        if column_id and if_exists == 'pass':
+            print("row already exists")
+            continue
+
+        directory_column_id = None
+        if row["directory_column"]:
+            directory_table_slug= row["directory_column"].split(":")[0].split(".")[1]
+            directory_column_name = row["directory_column"].split(":")[1]
+            directory_column_id = get_directory_column_id(directory_column_name, directory_table_slug,backend)
+
+        mutation_parameters = {
+            "table": table_id,
+            "bigqueryType": bqtype_dict[row['bigquery_type'].upper()],
+            "name": row['name'],
+            "description": row["description"],
+            "coveredByDictionary": row["covered_by_dictionary"] == "yes",
+            "measurementUnit": row["measurement_unit"],
+            "containsSensitiveData": row["has_sensitive_data"] == 'yes',
+            "observations": row["observations"],
+            "directoryPrimaryKey": directory_column_id
+        }
+        
+        if column_id:
+            mutation_parameters['id'] = column_id
+
+        
+        create_column(backend, mutation_parameters=mutation_parameters)
+        
+    check_metadata_columns(
+        dataset_id = dataset_id,
+        table_slug = table_slug,
+        url_api = url_api,
+        url_architecture =url_architecture,)
