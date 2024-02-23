ea4938a36ff8d57449dccf203f45fc868f067880
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-02-23 11:00:40 -0300

    fix: add rename_flow_run task

--- pipelines/datasets/br_me_cnpj/flows.py
+++ pipelines/datasets/br_me_cnpj/flows.py
@@ -231,9 +231,9 @@ with Flow(
     )
     dbt_alias = Parameter("dbt_alias", default=True, required=False)
 
-   # rename_flow_run = rename_current_flow_run_dataset_table(
-   #     prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
-   # )
+    rename_flow_run = rename_current_flow_run_dataset_table(
+        prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
+    )
     tabelas = constants_cnpj.TABELAS.value[2:3]
 
     data_source_max_date = get_data_source_max_date()
