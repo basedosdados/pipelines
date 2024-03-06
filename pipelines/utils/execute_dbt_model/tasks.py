903947a9ae2675f662f3468daedb9f60f80c496e
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-03-06 15:09:14 -0300

    fix: fix unsupported type

--- pipelines/utils/execute_dbt_model/tasks.py
+++ pipelines/utils/execute_dbt_model/tasks.py
@@ -96,7 +96,7 @@ def run_dbt_model(
         else:
             vars_str = f"'{_vars}'"
             run_command = None if dbt_command in ["test"] else run_command
-            run_command += f" --vars {vars_str}" if dbt_command in ["run", "run and test", "run/test"] else None
+            run_command += f" --vars {vars_str}" if dbt_command in ["run", "run and test", "run/test"] else ""
 
     if flags:
         run_command += f" {flags}"
