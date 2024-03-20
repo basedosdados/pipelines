b15295c6945af6d5651ad943c923ea0690103acd
Arthur Gusmão <arthurgusmao@id.uff.br>
2024-03-20 17:58:22 -0300

    feat: add every day run schedule

---
+++ pipelines/utils/metadata/schedules.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Schedules for metadata flows
+"""
+
+
+from datetime import datetime
+from prefect.schedules import Schedule
+from prefect.schedules.clocks import CronClock
+from pipelines.constants import constants
+
+
+every_day_quality_checks = Schedule(
+    clocks=[
+        CronClock(
+            cron="0 21 * * *",  # At 23:00 on every day-of-week from Monday through Friday.
+            start_date=datetime(2024, 3, 20, 12, 26),
+            labels=[
+                constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
+            ],
+            parameter_defaults={
+
+            },
+        ),
+    ],
+)
