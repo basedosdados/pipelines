790e39c73ff17f55854ccee29c415b70a4aa4dde
Laura Amaral <laura.amaral@basedosdados.org>
2024-05-09 14:57:22 -0300

    fix: api_url constants

--- pipelines/utils/constants.py
+++ pipelines/utils/constants.py
@@ -17,6 +17,6 @@ class constants(Enum):  # pylint: disable=c0103
     GOOGLE_SHEETS_URL = "https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 
     API_URL = {
-        "staging": "https://staging.api.basedosdados.org/api/v1/graphql",
-        "prod":"https://api.basedosdados.org/api/v1/graphql"
+        "staging": "https://staging.backend.basedosdados.org/api/v1/graphql",
+        "prod":"https://backend.basedosdados.org/api/v1/graphql"
     }
