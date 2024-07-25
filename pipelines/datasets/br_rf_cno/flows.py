d7872622f8404f08aacbf54649bcaebf821b7bcf
uiro-bi <pisagabriel09@gmail.com>
2024-07-25 09:27:45 -0300

    re register flow

--- pipelines/datasets/br_rf_cno/flows.py
+++ pipelines/datasets/br_rf_cno/flows.py
@@ -33,7 +33,7 @@ from pipelines.utils.tasks import (
 
 with Flow(
     name="br_rf_cno.tables", code_owners=["Gabriel Pisa"]
-) as br_rf_cno:
+) as br_rf_cno_tables:
     # Parameters
     dataset_id = Parameter("dataset_id", default="br_rf_cno", required=True)
     table_id = Parameter("table_id", default="tables", required=False)
@@ -65,54 +65,54 @@ with Flow(
         dataset_id=unmapped(dataset_id),
         table_id=table_ids,
         dump_mode=unmapped("append"),
-        source_format='parquet',
-        wait=files
+        source_format=unmapped('parquet'),
+        wait=unmapped(files),
     )
 
-    with case(materialize_after_dump, True):
-            # Trigger DBT flow run
-            current_flow_labels = get_current_flow_labels()
-            materialization_flow = create_flow_run(
-                flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
-                project_name=constants.PREFECT_DEFAULT_PROJECT.value,
-                parameters={
-                    "dataset_id": dataset_id,
-                    "table_id": table_id,
-                    "mode": materialization_mode,
-                    "dbt_alias": dbt_alias,
-                },
-                labels=current_flow_labels,
-                run_name=f"Materialize {dataset_id}.{table_id}",
-            )
+    # with case(materialize_after_dump, True):
+    #         # Trigger DBT flow run
+    #         current_flow_labels = get_current_flow_labels()
+    #         materialization_flow = create_flow_run(
+    #             flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+    #             project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+    #             parameters={
+    #                 "dataset_id": dataset_id,
+    #                 "table_id": table_id,
+    #                 "mode": materialization_mode,
+    #                 "dbt_alias": dbt_alias,
+    #             },
+    #             labels=current_flow_labels,
+    #             run_name=f"Materialize {dataset_id}.{table_id}",
+    #         )
 
-            wait_for_materialization = wait_for_flow_run(
-                materialization_flow,
-                stream_states=True,
-                stream_logs=True,
-                raise_final_state=True,
-            )
-            wait_for_materialization.max_retries = (
-                dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
-            )
-            wait_for_materialization.retry_delay = timedelta(
-                seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
-            )
+    #         wait_for_materialization = wait_for_flow_run(
+    #             materialization_flow,
+    #             stream_states=True,
+    #             stream_logs=True,
+    #             raise_final_state=True,
+    #         )
+    #         wait_for_materialization.max_retries = (
+    #             dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+    #         )
+    #         wait_for_materialization.retry_delay = timedelta(
+    #             seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+    #         )
 
-            with case(update_metadata, True):
-                update_django_metadata(
-                    dataset_id=dataset_id,
-                    table_id=table_id,
-                    date_column_name={"date": "date"},
-                    date_format="%Y-%m-%d",
-                    coverage_type="part_bdpro",
-                    time_delta={"months": 6},
-                    prefect_mode=materialization_mode,
-                    bq_project="basedosdados",
-                    upstream_tasks=[wait_for_materialization],
-                )
+    #         with case(update_metadata, True):
+    #             update_django_metadata(
+    #                 dataset_id=dataset_id,
+    #                 table_id=table_id,
+    #                 date_column_name={"date": "data_atualizacao"},
+    #                 date_format="%Y-%m-%d",
+    #                 coverage_type="part_bdpro",
+    #                 time_delta={"months": 6},
+    #                 prefect_mode=materialization_mode,
+    #                 bq_project="basedosdados",
+    #                 upstream_tasks=[wait_for_materialization],
+    #             )
 
 
-br_rf_cno.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
-br_rf_cno.run_config = KubernetesRun(
+br_rf_cno_tables.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
+br_rf_cno_tables.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
