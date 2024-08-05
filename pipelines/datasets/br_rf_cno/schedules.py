a5068b30dc8b2e1f84dd11720f56756905a8ae5a
uiro-bi <pisagabriel09@gmail.com>
2024-08-05 13:47:19 -0300

    set daily flowrun

--- pipelines/datasets/br_rf_cno/schedules.py
+++ pipelines/datasets/br_rf_cno/schedules.py
@@ -13,7 +13,7 @@ from pipelines.constants import constants
 schedule_br_rf_cno = Schedule(
     clocks=[
         CronClock(
-            cron="5 4 * * 2,5", #At 04:05 on Tuesday and Friday
+            cron="5 4 * * *", #everyday at 04:05
             start_date=datetime(2023, 9, 1, 0, 0),
             labels=[
                 constants.BASEDOSDADOS_PROD_AGENT_LABEL.value,
