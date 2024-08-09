6bfd1b1c1395944fce80e1edd29363381cc0715a
tricktx <patrick.tx@hotmail.com.br>
2024-08-08 23:28:57 -0300

    [fix] redoing modified files

--- pipelines/utils/dump_to_gcs/constants.py
+++ pipelines/utils/dump_to_gcs/constants.py
@@ -4,7 +4,7 @@ Constants for Dump to GCS pipeline.
 """
 
 from enum import Enum
-from pipelines.utils.utils import get_credentials_from_secret
+
 
 class constants(Enum):  # pylint: disable=c0103
     """
@@ -13,9 +13,6 @@ class constants(Enum):  # pylint: disable=c0103
 
     MAX_BYTES_PROCESSED_PER_TABLE = 5 * 1024 * 1024 * 1024  # 5GB
 
-    url_path = get_credentials_from_secret('url_download_data')
-    secret_path_url_free = url_path['URL_DOWNLOAD_OPEN']
-    secret_path_url_closed = url_path['URL_DOWNLOAD_CLOSED']
 
 
 
