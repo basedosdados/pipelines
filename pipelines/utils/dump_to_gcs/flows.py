bc5d7e8aba28a07809651c17b0e2b5b87927ae14
tricktx <patrick.tx@hotmail.com.br>
2024-08-20 10:44:10 -0300

    [fix] remove trigger in dump_to_gcs

--- pipelines/utils/dump_to_gcs/flows.py
+++ pipelines/utils/dump_to_gcs/flows.py
@@ -27,33 +27,13 @@ with Flow(
     project_id = Parameter("project_id", required=False) # basedosdados
     dataset_id = Parameter("dataset_id", required=False)  # dataset_id or dataset_id_staging
     table_id = Parameter("table_id", required=False)
-    #year = Parameter("year", required=False)
     query = Parameter("query", required=False)
+    #year = Parameter("year", required=False)
     #mode = Parameter("mode", required=False)
-    bd_project_mode = Parameter(
-        "bd_project_mode", required=False, default="prod"
-    )  # prod or staging
+    bd_project_mode = Parameter("bd_project_mode", required=False, default="prod")  # prod or staging
     billing_project_id = Parameter("billing_project_id", required=False)
-
-    desired_crontab = Parameter(
-        "desired_crontab",
-        required=False,
-        default="0 0 * * *",
-    )
-
-    rename_flow_run = rename_current_flow_run_dataset_table(
-        prefix="Dump to GCS: ", dataset_id=dataset_id, table_id=table_id
-    )
-
+    rename_flow_run = rename_current_flow_run_dataset_table(prefix="Dump to GCS: ", dataset_id=dataset_id, table_id=table_id)
     project_id = get_project_id(project_id=project_id, bd_project_mode=bd_project_mode)
-
-    trigger_download, execution_time = trigger_cron_job(
-        project_id=project_id,
-        dataset_id=dataset_id,
-        table_id=table_id,
-        cron_expression=desired_crontab,
-    )
-
     """
     Caso queiram subir todas as tabelas para o bucket novamente,
     1. Descomentem os parametros year e mode
@@ -73,23 +53,14 @@ with Flow(
     )
     """
 
-    with case(trigger_download, True):
-        download_task = download_data_to_gcs(  # pylint: disable=C0103
-            project_id=project_id,
-            dataset_id=dataset_id,
-            table_id=table_id,
-            query=query,
-            bd_project_mode=bd_project_mode,
-            billing_project_id=billing_project_id,
-        )
-
-    update_task = update_last_trigger(  # pylint: disable=C0103
-            project_id=project_id,
-            dataset_id=dataset_id,
-            table_id=table_id,
-            execution_time=execution_time,
-        )
-    update_task.set_upstream(download_task)
+    download_task = download_data_to_gcs(  # pylint: disable=C0103
+        project_id=project_id,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        query=query,
+        bd_project_mode=bd_project_mode,
+        billing_project_id=billing_project_id,
+    )
 
 dump_to_gcs_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 dump_to_gcs_flow.run_config = KubernetesRun(image=constants.DOCKER_IMAGE.value)
