2e655428e229b1ea8b812c088080f619f28581b1
Luiz Eduardo <luiz.sinx02@gmail.com>
2024-09-10 10:03:59 -0300

    Fix copa_brasil e add check_if_data_is_outdated

--- pipelines/datasets/mundo_transfermarkt_competicoes/flows.py
+++ pipelines/datasets/mundo_transfermarkt_competicoes/flows.py
@@ -23,6 +23,7 @@ from pipelines.datasets.mundo_transfermarkt_competicoes.tasks import (
     execucao_coleta_sync,
     make_partitions,
     get_data_source_transfermarkt_max_date,
+    get_data_source_max_date
 )
 from pipelines.utils.constants import constants as utils_constants
 from pipelines.utils.decorators import Flow
@@ -147,58 +148,70 @@ with Flow(
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump: ", dataset_id=dataset_id, table_id=table_id, wait=table_id
     )
-    df = execucao_coleta_sync(table_id)
-    output_filepath = make_partitions(df, upstream_tasks=[df])
 
-    wait_upload_table = create_table_and_upload_to_gcs(
-        data_path=output_filepath,
+    data_source_max_date = get_data_source_max_date()
+
+    outdated = check_if_data_is_outdated(
         dataset_id=dataset_id,
         table_id=table_id,
-        dump_mode="append",
-        wait=output_filepath,
+        data_source_max_date=data_source_max_date,
+        upstream_tasks=[data_source_max_date],
     )
 
-    with case(materialize_after_dump, True):
-        # Trigger DBT flow run
-        current_flow_labels = get_current_flow_labels()
-        materialization_flow = create_flow_run(
-            flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
-            project_name=constants.PREFECT_DEFAULT_PROJECT.value,
-            parameters={
-                "dataset_id": dataset_id,
-                "table_id": table_id,
-                "mode": materialization_mode,
-                "dbt_alias": dbt_alias,
-                "dbt_command": "run/test",
-            },
-            labels=current_flow_labels,
-            run_name=r"Materialize {dataset_id}.{table_id}",
-        )
+    with case(outdated, True):
 
-        wait_for_materialization = wait_for_flow_run(
-            materialization_flow,
-            stream_states=True,
-            stream_logs=True,
-            raise_final_state=True,
-        )
-        wait_for_materialization.max_retries = (
-            dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
-        )
-        wait_for_materialization.retry_delay = timedelta(
-            seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
-        )
+        df = execucao_coleta_sync(table_id, upstream_tasks=[outdated])
+        output_filepath = make_partitions(df, upstream_tasks=[df])
 
-        update_django_metadata(
+        wait_upload_table = create_table_and_upload_to_gcs(
+            data_path=output_filepath,
             dataset_id=dataset_id,
             table_id=table_id,
-            date_column_name={"date": "data"},
-            date_format="%Y-%m-%d",
-            coverage_type="part_bdpro",
-            time_delta={"months": 6},
-            prefect_mode=materialization_mode,
-            bq_project="basedosdados",
-            upstream_tasks=[wait_for_materialization],
+            dump_mode="append",
+            wait=output_filepath,
         )
+
+        with case(materialize_after_dump, True):
+            # Trigger DBT flow run
+            current_flow_labels = get_current_flow_labels()
+            materialization_flow = create_flow_run(
+                flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
+                project_name=constants.PREFECT_DEFAULT_PROJECT.value,
+                parameters={
+                    "dataset_id": dataset_id,
+                    "table_id": table_id,
+                    "mode": materialization_mode,
+                    "dbt_alias": dbt_alias,
+                    "dbt_command": "run/test",
+                },
+                labels=current_flow_labels,
+                run_name=r"Materialize {dataset_id}.{table_id}",
+            )
+
+            wait_for_materialization = wait_for_flow_run(
+                materialization_flow,
+                stream_states=True,
+                stream_logs=True,
+                raise_final_state=True,
+            )
+            wait_for_materialization.max_retries = (
+                dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
+            )
+            wait_for_materialization.retry_delay = timedelta(
+                seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+            )
+
+            update_django_metadata(
+                dataset_id=dataset_id,
+                table_id=table_id,
+                date_column_name={"date": "data"},
+                date_format="%Y-%m-%d",
+                coverage_type="part_bdpro",
+                time_delta={"months": 6},
+                prefect_mode=materialization_mode,
+                bq_project="basedosdados",
+                upstream_tasks=[wait_for_materialization],
+            )
 
 
 transfermarkt_copa_flow.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
