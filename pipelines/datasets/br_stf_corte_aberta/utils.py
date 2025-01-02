dfad87f8fc0219185ebbc35a0cf5a113cbcb2d4e
tricktx <patrick.tx@hotmail.com.br>
2025-01-02 20:23:58 -0300

    register flow part 2

--- pipelines/datasets/br_stf_corte_aberta/utils.py
+++ pipelines/datasets/br_stf_corte_aberta/utils.py
@@ -63,11 +63,12 @@ def read_csv():
 
 def fix_columns_data(df):
     lista = ["Data de autuação", "Data da decisão", "Data baixa"]
-        for x in lista:
-            df[x] = df[x].astype(str)
-            if len(df[x]) == 1:
-                df[x] = df[x].replace("-", '')
-            df[x] = df[x].replace("/", "-").astype(str)
+    for x in lista:
+        df[x] = df[x].astype(str)
+        if len(df[x]) == 1:
+            df[x] = df[x].replace("-", '')
+        df[x] = df[x].replace("/", "-").astype(str)
+        log(df[x].value_counts())
     return df
 
 
