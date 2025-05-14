c86216dcde4709000103862d2bc197c357db0f2d
aspeddro <aspeddro@gmail.com>
2025-05-14 15:45:20 -0300

    test_table: add macro

--- models/test_dataset/test_table.sql
+++ models/test_dataset/test_table.sql
@@ -12,4 +12,4 @@ select
     safe_cast(mes as int64) mes,
     safe_cast(sigla_uf as string) sigla_uf,
     safe_cast(dado as string) dado
-from `basedosdados-staging.test_dataset_staging.test_table` as t
+from {{ set_datalake_project("test_dataset_staging.test_table") }} as t
