561697f3ee274fd9f2de3762759c77038060fb4f
Pedro Castro <aspeddro@gmail.com>
2025-05-14 17:36:05 -0300

    migration-test-production: test table-approve and test-dbt-dev-model (#1007)
    * migration-test: register dbt flow
    
    * test

--- models/test_dataset/test_dataset__test_table.sql
+++ models/test_dataset/test_dataset__test_table.sql
@@ -6,10 +6,11 @@
     )
 }}
 
+
 select
     safe_cast(ano as int64) ano,
     safe_cast(mes as int64) mes,
     safe_cast(sigla_uf as string) sigla_uf,
     safe_cast(dado as string) dado,
-    1 as col_test
+    2 as col_test
 from {{ set_datalake_project("test_dataset_staging.test_table") }} as t
