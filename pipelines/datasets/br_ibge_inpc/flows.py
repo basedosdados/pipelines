bd12d1008a046cbf8f8227c4156f0f10dea6c441
Luiz Eduardo <luiz.sinx02@gmail.com>
2025-05-16 08:55:51 -0300

    Desativas pipelines que precisam refaturar

--- pipelines/datasets/br_ibge_inpc/flows.py
+++ pipelines/datasets/br_ibge_inpc/flows.py
@@ -14,12 +14,6 @@ from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
 from pipelines.constants import constants
-from pipelines.datasets.br_ibge_inpc.schedules import (
-    schedule_br_ibge_inpc_mes_brasil,
-    schedule_br_ibge_inpc_mes_categoria_brasil,
-    schedule_br_ibge_inpc_mes_categoria_municipio,
-    schedule_br_ibge_inpc_mes_categoria_rm,
-)
 from pipelines.utils.crawler_ibge_inflacao.flows import (
     flow_ibge_inflacao_mes_brasil_,
     flow_ibge_inflacao_mes_geral,
@@ -36,9 +30,9 @@ br_ibge_inpc_mes_categoria_brasil.storage = GCS(
 br_ibge_inpc_mes_categoria_brasil.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
-br_ibge_inpc_mes_categoria_brasil.schedule = (
-    schedule_br_ibge_inpc_mes_categoria_brasil
-)
+# br_ibge_inpc_mes_categoria_brasil.schedule = (
+#     schedule_br_ibge_inpc_mes_categoria_brasil
+# )
 
 
 br_ibge_inpc_mes_categoria_rm = deepcopy(flow_ibge_inflacao_mes_rm)
@@ -48,7 +42,7 @@ br_ibge_inpc_mes_categoria_rm.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_ibge_inpc_mes_categoria_rm.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
-br_ibge_inpc_mes_categoria_rm.schedule = schedule_br_ibge_inpc_mes_categoria_rm
+# br_ibge_inpc_mes_categoria_rm.schedule = schedule_br_ibge_inpc_mes_categoria_rm
 
 br_ibge_inpc_mes_categoria_municipio = deepcopy(
     flow_ibge_inflacao_mes_municipio_
@@ -63,9 +57,9 @@ br_ibge_inpc_mes_categoria_municipio.storage = GCS(
 br_ibge_inpc_mes_categoria_municipio.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
-br_ibge_inpc_mes_categoria_municipio.schedule = (
-    schedule_br_ibge_inpc_mes_categoria_municipio
-)
+# br_ibge_inpc_mes_categoria_municipio.schedule = (
+#     schedule_br_ibge_inpc_mes_categoria_municipio
+# )
 
 
 br_ibge_inpc_mes_brasil = deepcopy(flow_ibge_inflacao_mes_geral)
@@ -75,4 +69,4 @@ br_ibge_inpc_mes_brasil.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
 br_ibge_inpc_mes_brasil.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
-br_ibge_inpc_mes_brasil.schedule = schedule_br_ibge_inpc_mes_brasil
+# br_ibge_inpc_mes_brasil.schedule = schedule_br_ibge_inpc_mes_brasil
