9c0287c5688591c41fa1085056609d26cdb1beeb
tricktx <patrick.tx@hotmail.com.br>
2025-05-16 14:52:57 -0300

    update ibge pam

--- models/br_ibge_pam/br_ibge_pam__lavoura_permanente.sql
+++ models/br_ibge_pam/br_ibge_pam__lavoura_permanente.sql
@@ -5,7 +5,7 @@
         partition_by={
             "field": "ano",
             "data_type": "int64",
-            "range": {"start": 1974, "end": 2022, "interval": 1},
+            "range": {"start": 1974, "end": 2023, "interval": 1},
         },
         cluster_by=["sigla_uf", "id_municipio"],
     )
