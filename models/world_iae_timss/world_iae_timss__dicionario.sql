5c04364dc011da5cf8b4ce53a1b701d734f245d9
Larissa Brito <larissabribiologia@gmail.com>
2025-06-03 12:18:47 -0300

    colocando dicionario em ingles

--- models/world_iae_timss/world_iae_timss__dicionario.sql
+++ models/world_iae_timss/world_iae_timss__dicionario.sql
@@ -1,9 +1,9 @@
-{{ config(alias="dicionario", schema="world_iae_timss", materialized="table") }}
+{{ config(alias="dictionary", schema="world_iae_timss", materialized="table") }}
 
 select
-    safe_cast(id_tabela as string) id_tabela,
-    safe_cast(nome_coluna as string) nome_coluna,
-    safe_cast(chave as string) chave,
-    safe_cast(cobertura_temporal as string) cobertura_temporal,
-    safe_cast(valor as string) valor
+    safe_cast(id_tabela as string) as table_id,
+    safe_cast(nome_coluna as string) as column_name,
+    safe_cast(chave as string) as key,
+    safe_cast(cobertura_temporal as string) as temporal_coverage,
+    safe_cast(valor as string) as value
 from {{ set_datalake_project("world_iae_timss_staging.dicionario") }} as t
