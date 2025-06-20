bc289fb693efbbedd2dd16de6ad507f5b7900fad
tricktx <patrick.tx@hotmail.com.br>
2025-06-20 12:36:10 -0300

    add case when in coluns

--- models/mundo_cites_comercio/mundo_cites_comercio__especies_ameacadas.sql
+++ models/mundo_cites_comercio/mundo_cites_comercio__especies_ameacadas.sql
@@ -9,12 +9,32 @@ select
     safe_cast(ano as int64) ano,
     safe_cast(apendice_cities as string) apendice_cites,
     safe_cast(nome_cientifico as string) nome_cientifico,
-    safe_cast(classe_taxonomica as string) classe_taxonomica,
-    safe_cast(ordem_taxonomica as string) ordem_taxonomica,
-    safe_cast(familia_taxonomica as string) familia_taxonomica,
-    safe_cast(genero_taxonomico as string) genero_taxonomico,
-    safe_cast(pais_importador as string) pais_importador,
-    safe_cast(pais_exportador as string) pais_exportador,
+    case
+        when classe_taxonomica = 'nan'
+        then null
+        else safe_cast(classe_taxonomica as string)
+    end as classe_taxonomica,
+    case
+        when ordem_taxonomica = 'nan'
+        then null
+        else safe_cast(ordem_taxonomica as string)
+    end as ordem_taxonomica,
+    case
+        when familia_taxonomica = 'nan'
+        then null
+        else safe_cast(familia_taxonomica as string)
+    end as familia_taxonomica,
+    case
+        when genero_taxonomico = 'nan'
+        then null
+        else safe_cast(genero_taxonomico as string)
+    end as genero_taxonomico,
+    case
+        when pais_importador = 'nan' then null else safe_cast(pais_importador as string)
+    end as pais_importador,
+    case
+        when pais_exportador = 'nan' then null else safe_cast(pais_exportador as string)
+    end as pais_exportador,
     case
         when pais_origem = 'nan' then null else safe_cast(pais_origem as string)
     end as pais_origem,
