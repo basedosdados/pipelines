6d278f82bc1a309f6cd8b512da168e40635597bd
Larissa Brito <larissabribiologia@gmail.com>
2025-08-06 18:03:06 -0300

    removendo arquivo dbt.py e descomentando codigo tea_2022.py

--- models/br_ibge_censo_2022/code/tea_2022.py
+++ models/br_ibge_censo_2022/code/tea_2022.py
@@ -4,16 +4,16 @@ DATASET_ID = "br_ibge_censo_2022"
 
 # Dicionário com nome da tabela : caminho completo do arquivo CSV
 tables = {
-    # "estudantes_tea_raca_cor_grupo_idade": "/home/laribrito/BD/tea_censo_2022/estudantes_tea_raca_cor_grupo_idade.csv",
-    # "estudantes_tea_sexo_grupo_idade": "/home/laribrito/BD/tea_censo_2022/estudantes_tea_sexo_grupo_idade.csv",
-    # "estudantes_tea_grupo_idade_nivel_escolaridade": "/home/laribrito/BD/tea_censo_2022/populacao_tea_grupo_idade_nivel_escolaridade.csv",
-    # "populacao_tea_indigena": "/home/laribrito/BD/tea_censo_2022/populacao_tea_indigena.csv",
-    # "populacao_tea_raca_cor": "/home/laribrito/BD/tea_censo_2022/populacao_tea_raca_cor.csv",
-    # "populacao_tea_sexo_grupo_idade": "/home/laribrito/BD/tea_censo_2022/populacao_tea_sexo_grupo_idade.csv",
-    # "populacao_tea_sexo_nivel_escolaridade": "/home/laribrito/BD/tea_censo_2022/populacao_tea_sexo_nivel_escolaridade.csv",
-    # "estudantes_deficiencia_grupo_idade_nivel_escolaridade": "/home/laribrito/BD/tea_censo_2022/estudantes_deficiencia_grupo_idade_nivel_escolaridade.csv",
-    # "estudantes_deficiencia_grupo_idade_raca_cor": "/home/laribrito/BD/tea_censo_2022/estudantes_deficiencia_grupo_idade_raca_cor.csv",
-    # "estudantes_deficiencia_grupo_idade_sexo": "/home/laribrito/BD/tea_censo_2022/estudantes_deficiencia_grupo_idade_sexo.csv",
+    "estudantes_tea_raca_cor_grupo_idade": "/home/laribrito/BD/tea_censo_2022/estudantes_tea_raca_cor_grupo_idade.csv",
+    "estudantes_tea_sexo_grupo_idade": "/home/laribrito/BD/tea_censo_2022/estudantes_tea_sexo_grupo_idade.csv",
+    "estudantes_tea_grupo_idade_nivel_escolaridade": "/home/laribrito/BD/tea_censo_2022/populacao_tea_grupo_idade_nivel_escolaridade.csv",
+    "populacao_tea_indigena": "/home/laribrito/BD/tea_censo_2022/populacao_tea_indigena.csv",
+    "populacao_tea_raca_cor": "/home/laribrito/BD/tea_censo_2022/populacao_tea_raca_cor.csv",
+    "populacao_tea_sexo_grupo_idade": "/home/laribrito/BD/tea_censo_2022/populacao_tea_sexo_grupo_idade.csv",
+    "populacao_tea_sexo_nivel_escolaridade": "/home/laribrito/BD/tea_censo_2022/populacao_tea_sexo_nivel_escolaridade.csv",
+    "estudantes_deficiencia_grupo_idade_nivel_escolaridade": "/home/laribrito/BD/tea_censo_2022/estudantes_deficiencia_grupo_idade_nivel_escolaridade.csv",
+    "estudantes_deficiencia_grupo_idade_raca_cor": "/home/laribrito/BD/tea_censo_2022/estudantes_deficiencia_grupo_idade_raca_cor.csv",
+    "estudantes_deficiencia_grupo_idade_sexo": "/home/laribrito/BD/tea_censo_2022/estudantes_deficiencia_grupo_idade_sexo.csv",
     "estudantes_indigenas_deficiencia_grupo_idade": "/home/laribrito/BD/tea_censo_2022/estudantes_indigenas_deficiencia_grupo_idade.csv",
 }
 
