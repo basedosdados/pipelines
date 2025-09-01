2e1b69f45d3f3dcff4a2ebf2bae841073922db4f
luizavboas <luiza.vilas.oliveira@usp.br>
2025-09-01 15:45:15 -0300

    Feat: RAM tests

--- pipelines/datasets/br_rf_cno/constants.py
+++ pipelines/datasets/br_rf_cno/constants.py
@@ -4,6 +4,7 @@ Constants for br_rf_cno
 """
 
 from enum import Enum
+from pathlib import Path
 
 
 class constants(Enum):
@@ -69,3 +70,7 @@ class constants(Enum):
             "Metragem": "metragem",
         },
     }
+
+    CURRENT_DIR = Path(__file__).parent
+    TMP_DIR = CURRENT_DIR / "tmp"
+    OUTPUT_DIR = CURRENT_DIR / "output"
