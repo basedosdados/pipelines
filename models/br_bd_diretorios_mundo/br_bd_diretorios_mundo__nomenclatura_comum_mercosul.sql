a13c9c9a4447bd16b0670bd3a0a91b2ecf1a245f
Luiza Vilas Boas <113413332+luizavboas@users.noreply.github.com>
2025-09-12 16:54:28 -0300

    [data] br_bd_diretorios_mundo (#1197)
    …_harmonizado data
    
    
    # Template Pull Requests - Pipeline
    
    ## Nomeação do Pull Request
    
    A nomeação de cada Pull Request (PR) deve seguir o seguinte padrão:
    
    - O título de cada Pull Request (PR) deve começar com uma das seguintes
    palavras-chave, entre colchetes. Além disso, **marque a palavra-chave
    que melhor descreve o seu PR atual**:
      - [ ]  **[Feature]**: Para novas funcionalidades.
      - [x]  **[Data]**: Para subida de novos dados em produção.
      - [ ]  **[Bugfix]**: Para correções de bugs.
    - [ ] **[Refactor]**: Para mudanças no código que não alteram a
    funcionalidade.
      - [ ]  **[Docs]**: Para atualizações na documentação.
      - [ ]  **[Test]**: Para mudanças relacionadas a testes.
      - [ ]  **[Chore]**: Para tarefas menores e de manutenção.
      - [ ]  **[Deactivate]**: Para desativar o schedule da Pipeline
    ---
      - Exemplos de título:
        - **[docs] br_me_caged**
        - **[Feature] br_cgu_servidores_publicos**
    
    ## Draft:
    - Ao abrir o PR, deverá coloca-lo como draft
    
    ## Descrição do PR:
    
    - Explique de maneira clara e concisa o objetivo deste PR. O que foi
    alterado? Qual o problema que ele resolve?
        - **Motivação/Contexto:** <!-- Qual a necessidade dessa mudança? -->
    
    
    ## Detalhes Técnicos:
    
    - Detalhe as mudanças mais técnicas, como ajustes na pipeline, scripts
    ou modelo de dados utilizado.
    
    - **Principais alterações na pipeline/scripts:** <!-- Cite as principais
    mudanças na pipeline/script -->
    - **Mudanças nos dados e no schema:** <!-- Cite se há mudanças nos dados
    e no schema -->
    - **Impacto no desempenho:** <!-- Cite os impactos de desempenho dessas
    mudanças -->
    
    - Se alguma parte do código precisar de alguma atenção a mais, comente
    na linha sinalizando para os revisores.
    
    ## Teste e Validações:
    
    - Relate os testes e validações relacionado aos dados/script:
      - [ ]  Testado localmente
      - [ ]  Testado na Cloud
    
    **Caso haja algo relacionado aos testes que vale a pena informar:** <!--
    Cite informações sobre os testes? -->
    
    ## Riscos e Mitigações:
    - Identifique os riscos potenciais desta mudança e como mitigar esses
    Riscos
    
        - Riscos conhecidos: <!-- Cite problemas que podem surgir -->
        - Planos de rollback: <!-- Explique como reverter as mudanças -->
    
    ## Dependencias:
    - Liste quaisquer dependências externas, como bibliotecas, outros PRs ou
    mudanças que precisam ser feitas antes deste merge.
    - [ ] Dependências: <!-- Cite dependencias, bibliotecas e outros PRs que
    são relacionados a esse Pull Requests antes de mergear -->
        - [ ]  Nenhuma dependencias adicional
    
    
    ## Revisadores:
    - Quando o PR estiver pronto para ser revisado, retire o **Draft**
    através do **Ready for reviews**, marque os revisadores de repositório,
    envie o PR no nosso [discord](https://discord.gg/V3yTWRYWZZ) na aba
    **Correções de PRs, arquiteturas e afins** e marque a **@equipe_dados**:
        - Revisadores recomendados no github:
            - basedosdados/dados

--- models/br_bd_diretorios_mundo/br_bd_diretorios_mundo__nomenclatura_comum_mercosul.sql
+++ models/br_bd_diretorios_mundo/br_bd_diretorios_mundo__nomenclatura_comum_mercosul.sql
@@ -1,58 +1,80 @@
 {{ config(alias="nomenclatura_comum_mercosul", schema="br_bd_diretorios_mundo") }}
-
-
+with
+    safe_select as (
+        select
+            safe_cast(co_ncm as string) id_ncm,
+            safe_cast(co_unid as string) id_unidade,
+            safe_cast(co_sh6 as string) id_sh6,
+            safe_cast(co_ppe as string) id_ppe,
+            safe_cast(co_ppi as string) id_ppi,
+            safe_cast(co_fat_agreg as string) id_fator_agregado_ncm,
+            safe_cast(co_cgce_n3 as string) id_cgce_n3,
+            safe_cast(co_isic_classe as string) id_isic_classe,
+            safe_cast(co_siit as string) id_siit,
+            safe_cast(co_cuci_item as string) id_cuci_item,
+            safe_cast(
+                case
+                    when co_unid = '10'
+                    then 'quilograma líquido'
+                    when co_unid = '11'
+                    then 'número (unidade)'
+                    when co_unid = '12'
+                    then 'milheiro'
+                    when co_unid = '13'
+                    then 'pares'
+                    when co_unid = '14'
+                    then 'metro'
+                    when co_unid = '15'
+                    then 'metro quadrado'
+                    when co_unid = '16'
+                    then 'metro cúbico'
+                    when co_unid = '17'
+                    then 'litro'
+                    when co_unid = '18'
+                    then 'mil quilowatt hora'
+                    when co_unid = '19'
+                    then 'quilate'
+                    when co_unid = '20'
+                    then 'duzia'
+                    when co_unid = '21'
+                    then 'tonelada métrica líquida'
+                    when co_unid = '22'
+                    then 'grama líquido'
+                    when co_unid = '23'
+                    then 'bilhões de unidades internacionais'
+                    when co_unid = '24'
+                    then 'quilograma bruto'
+                    else co_unid
+                end as string
+            ) as nome_unidade,
+            safe_cast(no_ncm_por as string) nome_ncm_portugues,
+            safe_cast(no_ncm_esp as string) nome_ncm_espanhol,
+            safe_cast(no_ncm_ing as string) nome_ncm_ingles,
+        from
+            {{
+                set_datalake_project(
+                    "br_bd_diretorios_mundo_staging.nomenclatura_comum_mercosul"
+                )
+            }} as d
+    )
 select
-    safe_cast(co_ncm as string) id_ncm,
-    safe_cast(co_unid as string) id_unidade,
-    safe_cast(co_sh6 as string) id_sh6,
-    safe_cast(co_ppe as string) id_ppe,
-    safe_cast(co_ppi as string) id_ppi,
-    safe_cast(co_fat_agreg as string) id_fator_agregado_ncm,
-    safe_cast(co_cgce_n3 as string) id_cgce_n3,
-    safe_cast(co_isic_classe as string) id_isic_classe,
-    safe_cast(co_siit as string) id_siit,
-    safe_cast(co_cuci_item as string) id_cuci_item,
-    safe_cast(
-        case
-            when co_unid = '11'
-            then 'número (unidade)'
-            when co_unid = '10'
-            then 'quilograma líquido'
-            when co_unid = '12'
-            then 'milheiro'
-            when co_unid = '14'
-            then 'metro'
-            when co_unid = '13'
-            then 'pares'
-            when co_unid = '15'
-            then 'metro quadrado'
-            when co_unid = '16'
-            then 'metro cúbico'
-            when co_unid = '17'
-            then 'litro'
-            when co_unid = '18'
-            then 'mil quilowatt hora'
-            when co_unid = '19'
-            then 'quilate'
-            when co_unid = '20'
-            then 'duzia'
-            when co_unid = '21'
-            then 'tonelada métrica líquida'
-            when co_unid = '22'
-            then 'grama líquido'
-            when co_unid = '23'
-            then 'bilhões de unidades internacionais'
-            when co_unid = '24'
-            then 'quilograma bruto'
-            else co_unid
-        end as string
-    ) as nome_unidade,
-    safe_cast(no_ncm_por as string) nome_ncm_portugues,
-    safe_cast(no_ncm_esp as string) nome_ncm_espanhol,
-    safe_cast(no_ncm_ing as string) nome_ncm_ingles,
-from
-    {{
-        set_datalake_project(
-            "br_bd_diretorios_mundo_staging.nomenclatura_comum_mercosul"
-        )
-    }} as d
+    {% set cols = [
+        "id_ncm",
+        "id_unidade",
+        "id_sh6",
+        "id_ppe",
+        "id_ppi",
+        "id_fator_agregado_ncm",
+        "id_cgce_n3",
+        "id_isic_classe",
+        "id_siit",
+        "id_cuci_item",
+        "nome_unidade",
+        "nome_ncm_portugues",
+        "nome_ncm_espanhol",
+        "nome_ncm_ingles",
+    ] %}
+    {% for col in cols %}
+        {{ validate_null_cols(col) }} as {{ col }}{% if not loop.last %},{% endif %}
+    {% endfor %},
+from safe_select
