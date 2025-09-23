4d251a85baf1648a7ad929088ed23c19819caf9d
Pedro Castro <aspeddro@gmail.com>
2025-09-23 16:58:09 -0300

    [bugfix]: remove `create_flow_run` for dbt flow (#1220)
    ### Resumo
    
    - Remove chamadas para o flow dbt usando a função `create_flow_run` e
    usa `run_dbt` diretamente.
    - Existe duas adições, a função `run_dbt` e `download_data_to_gcs`.
    Essas duas estavam presente no flow do dbt.
    - Testar modelos em dev agora são executados diretamente no Github.
    - Os testes só são executados quando adicionado a label `test-dev-model`
    - O flow dbt ainda existe. Ele vai ser registrado quando um
    sql/schema.yml for alterado.
    - Se um arquivo sql/schema.yml for alterado e existir uma pipeline para
    esse dataset o flow vai ser registrado. Isso garante que ao usar
    `run_dbt` em um flow sempre vamos está usando os arquivos mais recentes.
    
    Close #1211

--- pipelines/utils/crawler_world_sofascore_competicoes_futebol/flows.py
+++ pipelines/utils/crawler_world_sofascore_competicoes_futebol/flows.py
@@ -4,36 +4,31 @@ Flows for world_sofascore_competicoes_futebol
 """
 
 # pylint: disable=invalid-name,line-too-long
-from datetime import timedelta
 
 from prefect import Parameter, case
 from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
-from prefect.tasks.prefect import create_flow_run, wait_for_flow_run
 
 from pipelines.constants import constants
-from pipelines.utils.constants import constants as utils_constants
 from pipelines.utils.crawler_world_sofascore_competicoes_futebol.tasks import (
     get_data_source_max_date_and_preparing_data,
 )
 from pipelines.utils.decorators import Flow
-from pipelines.utils.execute_dbt_model.constants import (
-    constants as dump_db_constants,
-)
 from pipelines.utils.metadata.tasks import (
     check_if_data_is_outdated,
     update_django_metadata,
 )
 from pipelines.utils.tasks import (
     create_table_and_upload_to_gcs,
-    get_current_flow_labels,
+    download_data_to_gcs,
     rename_current_flow_run_dataset_table,
+    run_dbt,
 )
 
 with Flow(
     name="BD template - world_sofascore_competicoes_futebol",
     code_owners=["luiz"],
-) as world_sofascore_competicoes_futebol:
+) as flow_world_sofascore_competicoes_futebol:
     # Parameters
 
     dataset_id = Parameter(
@@ -88,34 +83,19 @@ with Flow(
 
         # materialize
         with case(materialize_after_dump, True):
-            # Trigger DBT flow run
-            current_flow_labels = get_current_flow_labels()
-            materialization_flow = create_flow_run(
-                flow_name=utils_constants.FLOW_EXECUTE_DBT_MODEL_NAME.value,
-                project_name=constants.PREFECT_DEFAULT_PROJECT.value,
-                parameters={
-                    "dataset_id": dataset_id,
-                    "table_id": table_id,
-                    "target": target,
-                    "dbt_alias": dbt_alias,
-                    "dbt_command": "run/test",
-                    "disable_elementary": False,
-                },
-                labels=current_flow_labels,
-                run_name=f"Materialize {dataset_id}.{table_id}",
+            wait_for_materialization = run_dbt(
+                dataset_id=dataset_id,
+                table_id=table_id,
+                target=target,
+                dbt_alias=dbt_alias,
+                dbt_command="run/test",
+                disable_elementary=False,
+                upstream_tasks=[wait_upload_table],
             )
-
-            wait_for_materialization = wait_for_flow_run(
-                materialization_flow,
-                stream_states=True,
-                stream_logs=True,
-                raise_final_state=True,
-            )
-            wait_for_materialization.max_retries = (
-                dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_ATTEMPTS.value
-            )
-            wait_for_materialization.retry_delay = timedelta(
-                seconds=dump_db_constants.WAIT_FOR_MATERIALIZATION_RETRY_INTERVAL.value
+            wait_for_dowload_data_to_gcs = download_data_to_gcs(
+                dataset_id=dataset_id,
+                table_id=table_id,
+                upstream_tasks=[wait_for_materialization],
             )
             # coverage updater
 
@@ -128,12 +108,12 @@ with Flow(
                     prefect_mode=target,
                     coverage_type="all_free",
                     bq_project="basedosdados",
-                    upstream_tasks=[wait_for_materialization],
+                    upstream_tasks=[wait_for_dowload_data_to_gcs],
                 )
 
-world_sofascore_competicoes_futebol.storage = GCS(
+flow_world_sofascore_competicoes_futebol.storage = GCS(
     constants.GCS_FLOWS_BUCKET.value
 )
-world_sofascore_competicoes_futebol.run_config = KubernetesRun(
+flow_world_sofascore_competicoes_futebol.run_config = KubernetesRun(
     image=constants.DOCKER_IMAGE.value
 )
