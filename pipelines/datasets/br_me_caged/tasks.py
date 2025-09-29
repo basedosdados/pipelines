79e13ddea4ca2d5acf31435ab9300d4fe597044a
Luiza Vilas Boas <113413332+luizavboas@users.noreply.github.com>
2025-09-29 11:26:29 -0300

    [data] br_me_caged (#1212)
    # Template Pull Requests - Pipeline
    
    ## Nomeação do Pull Request
    
    A nomeação de cada Pull Request (PR) deve seguir o seguinte padrão:
    
    - O título de cada Pull Request (PR) deve começar com uma das seguintes
    palavras-chave, entre colchetes. Além disso, **marque a palavra-chave
    que melhor descreve o seu PR atual**:
      - [ ]  **[Feature]**: Para novas funcionalidades.
      - [x]  **[Data]**: Para subida de novos dados em produção.
      - [x]  **[Bugfix]**: Para correções de bugs.
    - [ ] **[Refactor]**: Para mudanças no código que não alteram a
    funcionalidade.
      - [ ]  **[Docs]**: Para atualizações na documentação.
      - [ ]  **[Test]**: Para mudanças relacionadas a testes.
      - [ ]  **[Chore]**: Para tarefas menores e de manutenção.
      - [ ]  **[Deactivate]**: Para desativar o schedule da Pipeline
    
    ## Descrição do PR:
     
    - Explique de maneira clara e concisa o objetivo deste PR. O que foi
    alterado? Qual o problema que ele resolve?
        - **Motivação/Contexto:** <!-- Qual a necessidade dessa mudança? -->
    
    
    ## Detalhes Técnicos:
    
    - Detalhe as mudanças mais técnicas, como ajustes na pipeline, scripts
    ou modelo de dados utilizado.
    
    - **Principais alterações na pipeline/scripts:** <!-- Cite as principais
    mudanças na pipeline/script -->
    - **Mudanças nos dados e no schema:** <!-- Cite se há mudanças nos dados
    e no schema -->
    - **Impacto no desempenho:** <!-- Cite os impactos de desempenho dessas
    mudanças -->
    
    - Se alguma parte do código precisar de alguma atenção a mais, comente
    na linha sinalizando para os revisores.
    
    ## Teste e Validações:
    
    - Relate os testes e validações relacionado aos dados/script:
      - [ ]  Testado localmente
      - [ ]  Testado na Cloud
    
    **Caso haja algo relacionado aos testes que vale a pena informar:** <!--
    Cite informações sobre os testes? -->
    
    ## Riscos e Mitigações:
    - Identifique os riscos potenciais desta mudança e como mitigar esses
    Riscos
    
        - Riscos conhecidos: <!-- Cite problemas que podem surgir -->
        - Planos de rollback: <!-- Explique como reverter as mudanças -->
    
    ## Dependencias:
    - Liste quaisquer dependências externas, como bibliotecas, outros PRs ou
    mudanças que precisam ser feitas antes deste merge.
    - [ ] Dependências: <!-- Cite dependencias, bibliotecas e outros PRs que
    são relacionados a esse Pull Requests antes de mergear -->
        - [ ]  Nenhuma dependencias adicional
    
    
    ## Revisadores:
    - Quando o PR estiver pronto para ser revisado, retire o **Draft**
    através do **Ready for reviews**, marque os revisadores de repositório,
    envie o PR no nosso [discord](https://discord.gg/V3yTWRYWZZ) na aba
    **Correções de PRs, arquiteturas e afins** e marque a **@equipe_dados**:
        - Revisadores recomendados no github:
            - basedosdados/dados
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pipelines/datasets/br_me_caged/tasks.py
+++ pipelines/datasets/br_me_caged/tasks.py
@@ -1,101 +1,390 @@
 # -*- coding: utf-8 -*-
 """
-Tasks for br_me_novo_caged
+Tasks for br_me_caged
 """
 
-import os
+import datetime
+import ftplib
 
 # pylint: disable=invalid-name
 import re
 from datetime import timedelta
-from glob import glob
+from pathlib import Path
+from typing import List, Tuple
 
+import basedosdados as bd
 import pandas as pd
+from dateutil.relativedelta import relativedelta
 from prefect import task
+from prefect.triggers import all_finished
 from tqdm import tqdm
 from unidecode import unidecode
 
 from pipelines.constants import constants
+from pipelines.datasets.br_me_caged.constants import (
+    constants as caged_constants,
+)
+from pipelines.datasets.br_me_caged.utils import (
+    download_file,
+    get_caged_schedule,
+    verify_yearmonth,
+)
+from pipelines.utils.metadata.utils import get_api_most_recent_date
+from pipelines.utils.utils import constants as utils_constants
+from pipelines.utils.utils import log
 
 
 @task
-def get_caged_data(table_id: str, year: int) -> None:
-    """
-    Get CAGED data
-    """
-    if year not in [2020, 2021, 2022]:
-        raise ValueError("Year must be 2020, 2021 or 2022")
-    groups = {
-        "microdados_movimentacao": "cagedmov",
-        "microdados_movimentacao_fora_prazo": "cagedfor",
-        "microdados_movimentacao_excluida": "cageddex",
-    }
+def build_table_paths(
+    table_id: str, parent_dir: str | Path = caged_constants.DATASET_DIR.value
+) -> Tuple[Path, Path]:
+    parent_dir = Path(parent_dir)
+    parent_dir.mkdir(parents=True, exist_ok=True)
 
-    group = groups[table_id]
-    command = f"bash pipelines/datasets/br_me_caged/bash_scripts/download.sh {group} {table_id} {year}"
+    table_dir = parent_dir / table_id
+    table_dir.mkdir(exist_ok=True)
+    table_input_dir = table_dir / "input"
+    table_output_dir = table_dir / "output"
 
-    os.system(command)
+    table_input_dir.mkdir(exist_ok=True)
+    table_output_dir.mkdir(exist_ok=True)
+
+    return table_input_dir, table_output_dir
+
+
+@task
+def get_source_last_date(
+    ftp_host: str = caged_constants.FTP_HOST.value,
+) -> datetime.date:
+    """
+    This task reaches 'ftp.mtps.gov.br' subfolders looking for most recent year and month
+
+    Parameters:
+        ftp_host (str): the FTP host to connect to (default: "ftp.mtps.gov.br")
+
+    Returns:
+        last_date (datetime): most recent date with updated content
+    """
+    ftp = ftplib.FTP(ftp_host)
+    ftp.login()
+    ftp.encoding = "latin-1"
+    ftp.cwd(caged_constants.REMOTE_DIR.value)
+    try:
+        folder_items = ftp.nlst()
+        year_folders = [
+            int(re.search(r"\d{4}", item).group(0))
+            for item in folder_items
+            if re.search(r"\d{4}", item)
+        ]
+
+        year_folders.sort(reverse=True)
+        ftp.cwd(str(year_folders[0]))
+        folder_items = ftp.nlst()
+        month_folders = [
+            int(re.search(r"^(?:\d{4})(\d{2})$", item).group(1))
+            for item in folder_items
+            if re.search(r"^(?:\d{4})(\d{2})$", item)
+        ]
+        month_folders.sort(reverse=True)
+        last_date = datetime.datetime(
+            year=year_folders[0], month=month_folders[0], day=1
+        )
+        return last_date.date()
+    except Exception as ErrorNlst:
+        log(f"Unable to access CAGED subfolders due to {ErrorNlst}")
+
+
+@task
+def get_table_last_date(
+    dataset_id: str,
+    table_id: str,
+) -> datetime.date:
+    """
+    This task gets most recent date content for a given table
+
+    Args:
+        dataset_id e table_id(string): table and dataset identification to fecth correspondent last date
+
+    Returns:
+        str: table most recent date
+    """
+
+    backend = bd.Backend(graphql_url=utils_constants.API_URL.value["prod"])
+
+    data_api = get_api_most_recent_date(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        backend=backend,
+        date_format="%Y-%m",
+    )
+    data_api = datetime.datetime(year=2020, month=1, day=1).date()
+    return data_api
+
+
+@task
+def generate_yearmonth_range(
+    start_date: str | datetime.date, end_date: str | datetime.date
+) -> list:
+    """
+    Generate a list of yearmonth strings between start_date and end_date (inclusive).
+
+    Parameters:
+    start_date (str): Start date in format 'YYYYMM'
+    end_date (str): End date in format 'YYYYMM'
+
+    Returns:
+    list: List of yearmonth strings in chronological order
+
+    Raises:
+    ValueError: If date format is incorrect or start_date is after end_date
+    """
+    if isinstance(start_date, datetime.date):
+        start_date = start_date.strftime("%Y%m")
+
+    if isinstance(end_date, datetime.date):
+        end_date = end_date.strftime("%Y%m")
+
+    # Validate input format
+    if not (
+        len(start_date) == 6
+        and len(end_date) == 6
+        and start_date.isdigit()
+        and end_date.isdigit()
+    ):
+        raise ValueError("Dates must be in 'YYYYMM' format")
+
+    # Convert to datetime objects
+    start = datetime.datetime.strptime(start_date, "%Y%m")
+    end = datetime.datetime.strptime(end_date, "%Y%m")
+
+    # Validate date order
+    if start > end:
+        raise ValueError("Start date must be before or equal to end date")
+
+    # Generate list of yearmonths
+    yearmonths = []
+    current = start
+    while current <= end:
+        yearmonths.append(current.strftime("%Y%m"))
+        current += relativedelta(months=1)
+
+    return yearmonths
+
+
+@task
+def crawl_novo_caged_ftp(
+    yearmonth: str,
+    table_id: str,
+    ftp_host: str = caged_constants.FTP_HOST.value,
+) -> List:
+    """
+    Downloads specified .7z files from a CAGED dataset FTP server.
+
+    Parameters:
+        yearmonth (str): the month to download data from (e.g., '202301' for January 2023)
+        ftp_host (str): the FTP host to connect to (default: "ftp.mtps.gov.br")
+
+    Returns:
+        List: Lists of unsuccessfully downloaded files
+    """
+    verify_yearmonth(yearmonth)
+    ftp = ftplib.FTP(ftp_host)
+    ftp.login()
+    ftp.cwd(f"{caged_constants.REMOTE_DIR.value}/{int(yearmonth[0:4])}/")
+
+    available_months = ftp.nlst()
+    if yearmonth not in available_months:
+        raise ValueError(
+            f"Month {yearmonth} is not available in the directory for the year {yearmonth[0:4]}"
+        )
+
+    ftp.cwd(yearmonth)
+    log(f"Baixando para o mês: {yearmonth}")
+
+    filenames = [f for f in ftp.nlst() if f.endswith(".7z")]
+
+    successful_downloads = []
+    failed_downloads = []
+
+    for file in filenames:
+        if "CAGEDMOV" in file and table_id == "microdados_movimentacao":
+            log(f"Baixando o arquivo: {file}")
+            success, corrupt_file = download_file(
+                ftp,
+                yearmonth,
+                file,
+                caged_constants.DATASET_DIR.value
+                / "microdados_movimentacao"
+                / "input",
+            )
+            if success:
+                successful_downloads.append(file)
+            else:
+                failed_downloads.append(corrupt_file)
+
+        elif (
+            "CAGEDFOR" in file
+            and table_id == "microdados_movimentacao_fora_prazo"
+        ):
+            log(f"Baixando o arquivo: {file}")
+            success, corrupt_file = download_file(
+                ftp,
+                yearmonth,
+                file,
+                caged_constants.DATASET_DIR.value
+                / "microdados_movimentacao_fora_prazo"
+                / "input",
+            )
+            if success:
+                successful_downloads.append(file)
+            else:
+                failed_downloads.append(corrupt_file)
+
+        elif (
+            "CAGEDEXC" in file
+            and table_id == "microdados_movimentacao_excluida"
+        ):
+            log(f"Baixando o arquivo: {file}")
+            success, corrupt_file = download_file(
+                ftp,
+                yearmonth,
+                file,
+                caged_constants.DATASET_DIR.value
+                / "microdados_movimentacao_excluida"
+                / "input",
+            )
+            if success:
+                successful_downloads.append(file)
+            else:
+                failed_downloads.append(corrupt_file)
+
+    ftp.quit()
+
+    log("\nDownload Summary:")
+    log(f"Successfully downloaded: {successful_downloads}")
+    log(f"Failed downloads: {failed_downloads}")
+
+    if len(successful_downloads) == 0:
+        log(failed_downloads)
+        raise Exception("No successful downloads!")
+    return failed_downloads
 
 
 @task(
+    trigger=all_finished,
     max_retries=constants.TASK_MAX_RETRIES.value,
     retry_delay=timedelta(seconds=constants.TASK_RETRY_DELAY.value),
 )
-def build_partitions(table_id: str) -> str:
+def build_partitions(table_id: str, table_output_dir: str | Path) -> str:
     """
     build partitions from gtup files
 
     table_id: microdados_movimentacao | microdados_movimentacao_fora_prazo | microdados_movimentacao_excluida
     """
-    input_files = glob(f"/tmp/caged/{table_id}/input/*txt")
-    for filename in tqdm(input_files):
-        df = pd.read_csv(filename, sep=";", dtype={"uf": str})
-        date = re.search(r"\d+", filename).group()
-        ano = date[:4]
-        mes = int(date[-2:])
+    input_files = Path(
+        caged_constants.DATASET_DIR.value / table_id / "input"
+    ).glob("*txt")
+    for filepath in tqdm(input_files):
+        log(f"Reading: {filepath}")
+        filename = filepath.name
+        try:
+            df = pd.read_csv(filepath, sep=";", dtype={"uf": str})
+            date = re.search(r"\d+", filename).group()
+            ano = date[:4]
+            mes = int(date[-2:])
+            df.columns = [unidecode(col) for col in df.columns]
+            df.rename(columns=caged_constants.RENAME_DICT.value, inplace=True)
 
-        df.columns = [unidecode(col) for col in df.columns]
+            log(f"Renaming dataframe columns to: {df.columns}")
+            df["sigla_uf"] = df["sigla_uf"].map(caged_constants.UF_DICT.value)
 
-        dict_uf = {
-            "11": "RO",
-            "12": "AC",
-            "13": "AM",
-            "14": "RR",
-            "15": "PA",
-            "16": "AP",
-            "17": "TO",
-            "21": "MA",
-            "22": "PI",
-            "23": "CE",
-            "24": "RN",
-            "25": "PB",
-            "26": "PE",
-            "27": "AL",
-            "28": "SE",
-            "29": "BA",
-            "31": "MG",
-            "32": "ES",
-            "33": "RJ",
-            "35": "SP",
-            "41": "PR",
-            "42": "SC",
-            "43": "RS",
-            "50": "MS",
-            "51": "MT",
-            "52": "GO",
-            "53": "DF",
-        }
+            for state in caged_constants.UF_DICT.value.values():
+                log(f"Partitioning for {state}")
+                data = df[df["sigla_uf"] == state]
+                data.drop(
+                    caged_constants.COLUMNS_TO_DROP.value[table_id],
+                    axis=1,
+                    inplace=True,
+                )
+                output_dir = (
+                    Path(table_output_dir)
+                    / f"ano={ano}"
+                    / f"mes={mes}"
+                    / f"sigla_uf={state}"
+                )
+                columns_to_select = [
+                    col
+                    for col in caged_constants.COLUMNS_TO_SELECT.value
+                    if col in data.columns
+                ]
+                output_dir.mkdir(exist_ok=True, parents=True)
+                output_path = str(output_dir / "data.csv")
+                data[columns_to_select].to_csv(
+                    output_path,
+                    index=False,
+                )
+                del data
+            del df
+        except Exception as e:
+            log(f"Failed to read: {filepath} due to: {e}", "error")
+    return table_output_dir
 
-        df["uf"] = df["uf"].map(dict_uf)
 
-        for state in dict_uf.values():
-            data = df[df["uf"] == state]
-            data.drop(["competenciamov", "uf"], axis=1, inplace=True)
-            data.to_csv(
-                f"/tmp/caged/{table_id}/ano={ano}/mes={mes}/sigla_uf={state}/data.csv",
-                index=False,
-            )
-            del data
-        del df
+@task
+def update_caged_schedule(
+    table_last_date: str | datetime.date,
+    table_id: str,
+    schedules_file: str = "./pipelines/datasets/br_me_caged/schedules.py",
+    schedule_url: str = caged_constants.URL_SCHEDULE.value,
+):
+    """
+    This task fetches a specific page to extract html data
+    containing CAGED monhtly releasing dates.
+    """
+    log(f"Looking for schedule info on CAGED releasing on {schedule_url}")
+    date_elements = get_caged_schedule(
+        url=schedule_url,
+        css_selector=caged_constants.CSS_SELECTOR_SCHEDULES.value,
+    )
+    if isinstance(table_last_date, str):
+        table_last_date = datetime.datetime.strptime(
+            table_last_date, "%d/%m/%Y"
+        ).date()
+    this_month = table_last_date.month
+    log(f"This date {table_last_date}")
+    next_start_date = date_elements[0]["data"]
+    index = 1
+    while index < len(date_elements) - 1:
+        log(f"Current next start date {next_start_date}")
+        if date_elements[index]["data_competencia"].month > this_month:
+            next_start_date = date_elements[index]["data"]
+        else:
+            break
+        index += 1
 
-    return f"/tmp/caged/{table_id}/"
+    # Read schedule file to match table_id schedule pattern
+    with open(schedules_file, "r", encoding="utf-8") as f:
+        code = f.read()
+    schedule_name = f"every_month{table_id.replace('microdados', '')}"
+    schedule_pattern = rf"({schedule_name}\s*=\s*Schedule\(\s*clocks\s*=\s*\[\s*IntervalClock\(\s*interval\s*=\s*timedelta\([\w=\d\,\s]+\)\,\s*start_date\s*=\s*datetime\([\w=\d\,\s]+\)\,\s*labels\s*=\s*\[\s*constants\.BASEDOSDADOS_DEV_AGENT_LABEL\.value[\,\s\)\]]+)"
+    start_date_pattern = r"start_date\s*=\s*datetime\([\w=\d\,\s]+\)"
+    match = re.search(schedule_pattern, code)
+
+    if not match:
+        raise ValueError(
+            f"Schedule {schedule_name} não encontrado no arquivo."
+        )
+
+    schedule_block = match.group(1)
+
+    if next_start_date is not None:
+        schedule_block = re.sub(
+            start_date_pattern,
+            f"start_date=datetime({next_start_date.year}, {next_start_date.month}, {next_start_date.day})",
+            schedule_block,
+        )
+    new_code = code[: match.start(1)] + schedule_block + code[match.end(1) :]
+    with open(schedules_file, "w", encoding="utf-8") as f:
+        f.write(new_code)
+
+    return next_start_date
