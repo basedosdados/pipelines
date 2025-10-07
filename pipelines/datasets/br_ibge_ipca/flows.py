badc405ba500d19e163c31ce84a46c5b1ee75f81
Patrick Teixeira <105399231+tricktx@users.noreply.github.com>
2025-10-07 10:21:04 -0300

    [Refactor] br_ibge_inflacao (#1255)
    # Template Pull Requests - Pipeline
    
    ## Nomeação do Pull Request
    
    A nomeação de cada Pull Request (PR) deve seguir o seguinte padrão:
    
    - O título de cada Pull Request (PR) deve começar com uma das seguintes
    palavras-chave, entre colchetes. Além disso, **marque a palavra-chave
    que melhor descreve o seu PR atual**:
      - [ ]  **[Feature]**: Para novas funcionalidades.
      - [ ]  **[Data]**: Para subida de novos dados em produção.
      - [ ]  **[Bugfix]**: Para correções de bugs.
    - [x] **[Refactor]**: Para mudanças no código que não alteram a
    funcionalidade.
      - [ ]  **[Docs]**: Para atualizações na documentação.
      - [ ]  **[Test]**: Para mudanças relacionadas a testes.
      - [ ]  **[Chore]**: Para tarefas menores e de manutenção.
      - [ ]  **[Deactivate]**: Para desativar o schedule da Pipeline
    ---
      - Exemplos de título:
        - **[docs] br_me_caged**
        - **[Feature] br_cgu_servidores_publicos**
    
    ## Draft:
    - Ao abrir o PR, deverá coloca-lo como draft
    
    ## Descrição do PR:
    
    1. Refatorando as pipelines de inflação do IBGE, IPCA, IPCA15 e INPC. 
    2. Além disso, adicionando testes nos modelos dbt.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pipelines/datasets/br_ibge_ipca/flows.py
+++ pipelines/datasets/br_ibge_ipca/flows.py
@@ -8,14 +8,9 @@ from prefect.run_configs import KubernetesRun
 from prefect.storage import GCS
 
 from pipelines.constants import constants
-from pipelines.crawler.ibge_inflacao.flows import (
-    flow_ibge_inflacao_mes_brasil_,
-    flow_ibge_inflacao_mes_geral,
-    flow_ibge_inflacao_mes_municipio_,
-    flow_ibge_inflacao_mes_rm,
-)
+from pipelines.crawler.ibge_inflacao.flows import flow_ibge
 
-br_ibge_ipca_mes_categoria_brasil = deepcopy(flow_ibge_inflacao_mes_brasil_)
+br_ibge_ipca_mes_categoria_brasil = deepcopy(flow_ibge)
 br_ibge_ipca_mes_categoria_brasil.name = "br_ibge_ipca.mes_categoria_brasil"
 br_ibge_ipca_mes_categoria_brasil.code_owners = ["Gabriel Pisa"]
 br_ibge_ipca_mes_categoria_brasil.storage = GCS(
@@ -29,7 +24,7 @@ br_ibge_ipca_mes_categoria_brasil.run_config = KubernetesRun(
 # )
 
 
-br_ibge_ipca_mes_categoria_rm = deepcopy(flow_ibge_inflacao_mes_rm)
+br_ibge_ipca_mes_categoria_rm = deepcopy(flow_ibge)
 br_ibge_ipca_mes_categoria_rm.name = "br_ibge_ipca.mes_categoria_rm"
 br_ibge_ipca_mes_categoria_rm.code_owners = ["Gabriel Pisa"]
 br_ibge_ipca_mes_categoria_rm.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
@@ -38,9 +33,7 @@ br_ibge_ipca_mes_categoria_rm.run_config = KubernetesRun(
 )
 # br_ibge_ipca_mes_categoria_rm.schedule = schedule_br_ibge_ipca_mes_categoria_rm
 
-br_ibge_ipca_mes_categoria_municipio = deepcopy(
-    flow_ibge_inflacao_mes_municipio_
-)
+br_ibge_ipca_mes_categoria_municipio = deepcopy(flow_ibge)
 br_ibge_ipca_mes_categoria_municipio.name = (
     "br_ibge_ipca.mes_categoria_municipio"
 )
@@ -56,7 +49,7 @@ br_ibge_ipca_mes_categoria_municipio.run_config = KubernetesRun(
 # )
 
 
-br_ibge_ipca_mes_brasil = deepcopy(flow_ibge_inflacao_mes_geral)
+br_ibge_ipca_mes_brasil = deepcopy(flow_ibge)
 br_ibge_ipca_mes_brasil.name = "br_ibge_ipca.mes_brasil"
 br_ibge_ipca_mes_brasil.code_owners = ["Gabriel Pisa"]
 br_ibge_ipca_mes_brasil.storage = GCS(constants.GCS_FLOWS_BUCKET.value)
