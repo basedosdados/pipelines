8e3c7798bc86a5aaec630166ce88cfd5bbe2ba96
folhesgabriel <pisagabriel09@gmail.com>
2025-12-08 11:15:24 -0300

    adjust flow import

--- pipelines/utils/__init__.py
+++ pipelines/utils/__init__.py
@@ -8,9 +8,9 @@ Prefect flows for basedosdados project
 ###############################################################################
 from pipelines.utils.dump_to_gcs.flows import *
 from pipelines.utils.execute_dbt_model.flows import *
+from pipelines.utils.materialize_prod.flows import *
 
 # from pipelines.utils.apply_architecture_to_dataframe.flows import *
 from pipelines.utils.metadata.flows import *
 from pipelines.utils.to_download.flows import *
 from pipelines.utils.traceroute.flows import *
-from pipelines.utils.transfer_files_to_prod.flows import *
