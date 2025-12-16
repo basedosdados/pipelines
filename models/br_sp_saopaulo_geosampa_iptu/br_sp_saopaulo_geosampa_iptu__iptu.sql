bc2536e203fb5b07d150c8feac2c12312171c5c5
tricktx <Patrick.tx@hotmail.com.br>
2025-12-16 18:05:33 -0300

    partition by 2025

--- models/br_sp_saopaulo_geosampa_iptu/br_sp_saopaulo_geosampa_iptu__iptu.sql
+++ models/br_sp_saopaulo_geosampa_iptu/br_sp_saopaulo_geosampa_iptu__iptu.sql
@@ -6,7 +6,7 @@
         partition_by={
             "field": "ano",
             "data_type": "int64",
-            "range": {"start": 1995, "end": 2024, "interval": 1},
+            "range": {"start": 1995, "end": 2025, "interval": 1},
         },
     )
 }}
