634db55bfe25ec05b14cc6decda619b6498fede3
tricktx <Patrick.tx@hotmail.com.br>
2026-02-25 14:09:23 -0300

    bugfix: subindo alteração bruta para subir todas as partições da ANS corretamente via pipeline

--- pipelines/datasets/br_ans_beneficiario/flows.py
+++ pipelines/datasets/br_ans_beneficiario/flows.py
@@ -9,17 +9,12 @@ from prefect.storage import GCS
 from pipelines.constants import constants
 from pipelines.datasets.br_ans_beneficiario.schedules import every_day_ans
 from pipelines.datasets.br_ans_beneficiario.tasks import (
-    check_condition,
-    check_if_update_date_is_today,
     crawler_ans,
     extract_links_and_dates,
     files_to_download,
-    get_file_max_date,
-    is_empty,
 )
 from pipelines.utils.decorators import Flow
 from pipelines.utils.metadata.flows import update_django_metadata
-from pipelines.utils.metadata.tasks import check_if_data_is_outdated
 from pipelines.utils.tasks import (  # update_django_metadata,
     create_table_dev_and_upload_to_gcs,
     create_table_prod_gcs_and_run_dbt,
@@ -53,7 +48,7 @@ with Flow(
     )
     dbt_alias = Parameter("dbt_alias", default=False, required=False)
 
-    force_update = Parameter("force_update", default=False, required=False)
+    # force_update = Parameter("force_update", default=False, required=False)
 
     rename_flow_run = rename_current_flow_run_dataset_table(
         prefix="Dump: ",
@@ -66,76 +61,76 @@ with Flow(
     links_and_dates = extract_links_and_dates(url=url)
 
     # Se update == True, força o update dos dados.
-    with case(force_update, True):
-        files = files_to_download(
-            links_and_dates, upstream_tasks=[links_and_dates]
-        )
+    # with case(force_update, True):
+    files = files_to_download(
+        links_and_dates, upstream_tasks=[links_and_dates]
+    )
 
-    with case(force_update, False):
-        # Se update == False, verifica se a data de hoje == data de atualização | data da cobertura temporal < data do último arquivo (ex: arquivo 202401 -> (2023-12-01 < 2024-01-01) )
-        # Essa condição é verificada na task `check_condition()` abaixo.
+    # with case(force_update, False):
+    #     # Se update == False, verifica se a data de hoje == data de atualização | data da cobertura temporal < data do último arquivo (ex: arquivo 202401 -> (2023-12-01 < 2024-01-01) )
+    #     # Essa condição é verificada na task `check_condition()` abaixo.
 
-        file_last_date = get_file_max_date(
-            links_and_dates, upstream_tasks=[links_and_dates]
-        )
+    #     file_last_date = get_file_max_date(
+    #         links_and_dates, upstream_tasks=[links_and_dates]
+    #     )
 
-        update_date_check = check_if_update_date_is_today(
-            links_and_dates, upstream_tasks=[links_and_dates]
-        )
+    #     update_date_check = check_if_update_date_is_today(
+    #         links_and_dates, upstream_tasks=[links_and_dates]
+    #     )
 
-        coverage_check = check_if_data_is_outdated(
-            dataset_id,
-            table_id,
-            data_source_max_date=file_last_date,
-            date_format="%Y-%m-%d",
-            upstream_tasks=[links_and_dates, file_last_date],
-        )
+    #     coverage_check = check_if_data_is_outdated(
+    #         dataset_id,
+    #         table_id,
+    #         data_source_max_date=file_last_date,
+    #         date_format="%Y-%m-%d",
+    #         upstream_tasks=[links_and_dates, file_last_date],
+    #     )
 
-        with case(check_condition(update_date_check, coverage_check), True):
-            # Se check_condition = True, cria a lista com os arquivos para o download.
-            files = files_to_download(
-                links_and_dates, upstream_tasks=[links_and_dates]
-            )
+    #     with case(check_condition(update_date_check), True):
+    #         # Se check_condition = True, cria a lista com os arquivos para o download.
+    #         files = files_to_download(
+    #             links_and_dates, upstream_tasks=[links_and_dates]
+    #         )
 
-    with case(is_empty(files), False):
-        output_filepath = crawler_ans(files, upstream_tasks=[files])
-        wait_upload_table = create_table_dev_and_upload_to_gcs(
+    # with case(is_empty(files), False):
+    output_filepath = crawler_ans(files, upstream_tasks=[files])
+    wait_upload_table = create_table_dev_and_upload_to_gcs(
+        data_path=output_filepath,
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dump_mode="append",
+        source_format="parquet",
+        upstream_tasks=[output_filepath],
+    )
+    wait_for_materialization = run_dbt(
+        dataset_id=dataset_id,
+        table_id=table_id,
+        dbt_command="run/test",
+        dbt_alias=dbt_alias,
+        upstream_tasks=[wait_upload_table],
+    )
+
+    with case(materialize_after_dump, True):
+        wait_upload_prod = create_table_prod_gcs_and_run_dbt(
             data_path=output_filepath,
             dataset_id=dataset_id,
             table_id=table_id,
             dump_mode="append",
             source_format="parquet",
-            upstream_tasks=[output_filepath],
-        )
-        wait_for_materialization = run_dbt(
-            dataset_id=dataset_id,
-            table_id=table_id,
-            dbt_command="run/test",
-            dbt_alias=dbt_alias,
-            upstream_tasks=[wait_upload_table],
+            upstream_tasks=[wait_for_materialization],
         )
 
-        with case(materialize_after_dump, True):
-            wait_upload_prod = create_table_prod_gcs_and_run_dbt(
-                data_path=output_filepath,
+        with case(update_metadata, True):
+            update_django_metadata(
                 dataset_id=dataset_id,
                 table_id=table_id,
-                dump_mode="append",
-                source_format="parquet",
-                upstream_tasks=[wait_for_materialization],
+                date_column_name={"year": "ano", "month": "mes"},
+                date_format="%Y-%m-%d",
+                coverage_type="part_bdpro",
+                time_delta={"months": 6},
+                bq_project="basedosdados",
+                upstream_tasks=[wait_upload_prod],
             )
-
-            with case(update_metadata, True):
-                update_django_metadata(
-                    dataset_id=dataset_id,
-                    table_id=table_id,
-                    date_column_name={"year": "ano", "month": "mes"},
-                    date_format="%Y-%m-%d",
-                    coverage_type="part_bdpro",
-                    time_delta={"months": 6},
-                    bq_project="basedosdados",
-                    upstream_tasks=[wait_upload_prod],
-                )
 
 datasets_br_ans_beneficiario_flow.storage = GCS(
     constants.GCS_FLOWS_BUCKET.value
--- pipelines/datasets/br_ans_beneficiario/tasks.py
+++ pipelines/datasets/br_ans_beneficiario/tasks.py
@@ -2,7 +2,7 @@
 Tasks for br_ans_beneficiario
 """
 
-import asyncio
+import gc
 import os
 import re
 from datetime import datetime
@@ -14,10 +14,8 @@ from prefect import task
 from tqdm import tqdm
 
 from pipelines.datasets.br_ans_beneficiario.utils import (
-    get_url_from_template,
     parquet_partition,
 )
-from pipelines.utils.to_download.utils import download_files_async
 from pipelines.utils.utils import log
 
 
@@ -65,6 +63,7 @@ def extract_links_and_dates(url) -> pd.DataFrame:
 
     df["desatualizado"] = df["data_hoje"] == df["ultima_atualizacao"]
     # df['desatualizado'] = df['arquivo'].apply(lambda x: True if x in ['inf_diario_fi_202201.zip','inf_diario_fi_202305.zip'] else False)
+
     return df
 
 
@@ -83,8 +82,8 @@ def get_file_max_date(df):
 
 
 @task
-def check_condition(con1: bool, con2: bool):
-    return con1 is True or con2 is True
+def check_condition(con1: bool):
+    return con1 is True
 
 
 @task
@@ -98,27 +97,22 @@ def check_for_updates(df):
 @task
 def files_to_download(df):
     log("Arquivos na fila para o download -->")
-    log(
-        df[
-            df["ultima_atualizacao"] == max(df["ultima_atualizacao"])
-        ].arquivo.to_list()
-    )
-    return df[
-        df["ultima_atualizacao"] == max(df["ultima_atualizacao"])
-    ].arquivo.to_list()
+    log(df["arquivo"].unique().tolist())
+
+    return df["arquivo"].unique().tolist()
 
 
 @task
 def crawler_ans(files):
     for file in tqdm(files):
-        urls, _ = get_url_from_template(file)
+        # urls, _ = get_url_from_template(file)
 
-        save_path = "/tmp/data/br_ans_beneficiario/beneficiario/input/"
-        os.makedirs(save_path, exist_ok=True)
-        log(f"`mkdir = True` >>> {save_path} directory was created.")
-        asyncio.run(download_files_async(urls, save_path, "zip"))
+        # save_path = "/tmp/data/br_ans_beneficiario/beneficiario/input/"
+        # os.makedirs(save_path, exist_ok=True)
+        # log(f"`mkdir = True` >>> {save_path} directory was created.")
+        # asyncio.run(download_files_async(urls, save_path, "zip"))
 
-        log(f"DOWNLOADED FILE ->>> {file}")
+        # log(f"DOWNLOADED FILE ->>> {file}")
 
         parquet_partition(
             path="/tmp/data/br_ans_beneficiario/beneficiario/input/"
@@ -129,9 +123,12 @@ def crawler_ans(files):
         )
         log(f"CSV`s files have been deleted for ->>> {file}")
 
+        gc.collect()
+
     return "/tmp/data/br_ans_beneficiario/output/"
 
 
 @task
 def is_empty(lista):
+    log(f"Lista de arquivos para download: {lista}")
     return len(lista) == 0
