efec013d9dcdd3b49033646a0b83edd2c9333b5b
folhesgabriel <pisagabriel09@gmail.com>
2026-05-05 08:54:50 -0300

    refactor: remove deprecated post hooks

--- models/br_bcb_sicor/br_bcb_sicor__operacoes_desclassificadas.sql
+++ models/br_bcb_sicor/br_bcb_sicor__operacoes_desclassificadas.sql
@@ -2,8 +2,8 @@
     config(
         alias="operacoes_desclassificadas",
         schema="br_bcb_sicor",
-        pre_hook="             BEGIN                 DROP ALL ROW ACCESS POLICIES ON {{ this }};             EXCEPTION WHEN ERROR THEN                 SELECT 1;              END;         ",
         materialized="table",
+        pre_hook="             BEGIN                 DROP ALL ROW ACCESS POLICIES ON {{ this }};             EXCEPTION WHEN ERROR THEN                 SELECT 1;              END;         ",
     )
 }}
 select
