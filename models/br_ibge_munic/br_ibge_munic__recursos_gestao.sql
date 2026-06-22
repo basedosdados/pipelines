fdc2a9462888b8f04314803c4b8e92432ac5eb0b
folhesgabriel <pisagabriel09@gmail.com>
2026-06-22 10:18:01 -0300

    test: trigger table-approve for all 5 br_ibge_munic tables

--- models/br_ibge_munic/br_ibge_munic__recursos_gestao.sql
+++ models/br_ibge_munic/br_ibge_munic__recursos_gestao.sql
@@ -586,3 +586,4 @@ select
         articulacao_saneamento_apoio_setor_privado_comunidades as string
     ) articulacao_saneamento_apoio_setor_privado_comunidades
 from {{ set_datalake_project("br_ibge_munic_staging.recursos_gestao") }} as t
+-- trigger table-approve test (prefect 3 migration)
