17c420c449e3e1dcbb6498774e3baca8d0b26c96
Winzen <luiz.sinx02@gmail.com>
2026-06-23 15:50:58 -0300

    test: add pull_request trigger to table-approve and touch etnia_indigena model

--- models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__etnia_indigena.sql
+++ models/br_bd_diretorios_brasil/br_bd_diretorios_brasil__etnia_indigena.sql
@@ -7,6 +7,6 @@
 }}
 
 select
-    safe_cast(id_etnia_indigena as string) id_etnia_indigena,
-    safe_cast(nome as string) nome
+    safe_cast(id_etnia_indigena as string) as id_etnia_indigena,
+    safe_cast(nome as string) as nome
 from {{ set_datalake_project("br_bd_diretorios_brasil_staging.etnia_indigena") }} as t
