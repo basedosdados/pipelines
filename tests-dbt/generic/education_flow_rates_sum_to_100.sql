0f464a1899afe5b42f968fc85182288f7c2932c2
Leonardo de Vargas Michelotti <leonardo.michelotti@baristo.local>
2026-07-12 11:35:39 -0300

    test: valida consistencia das taxas de rendimento

---
+++ tests-dbt/generic/education_flow_rates_sum_to_100.sql
@@ -0,0 +1,48 @@
+{% test education_flow_rates_sum_to_100(model, tolerance=0.2) %}
+
+    with
+        rates as (
+            select
+                ano,
+                localizacao,
+                rede,
+                'ef_anos_iniciais' as etapa,
+                taxa_aprovacao_ef_anos_iniciais as aprovacao,
+                taxa_reprovacao_ef_anos_iniciais as reprovacao,
+                taxa_abandono_ef_anos_iniciais as abandono
+            from {{ model }}
+
+            union all
+
+            select
+                ano,
+                localizacao,
+                rede,
+                'ef_anos_finais' as etapa,
+                taxa_aprovacao_ef_anos_finais as aprovacao,
+                taxa_reprovacao_ef_anos_finais as reprovacao,
+                taxa_abandono_ef_anos_finais as abandono
+            from {{ model }}
+
+            union all
+
+            select
+                ano,
+                localizacao,
+                rede,
+                'em' as etapa,
+                taxa_aprovacao_em as aprovacao,
+                taxa_reprovacao_em as reprovacao,
+                taxa_abandono_em as abandono
+            from {{ model }}
+        )
+
+    select *, aprovacao + reprovacao + abandono as total
+    from rates
+    where
+        aprovacao is not null
+        and reprovacao is not null
+        and abandono is not null
+        and abs(aprovacao + reprovacao + abandono - 100) > {{ tolerance }}
+
+{% endtest %}
