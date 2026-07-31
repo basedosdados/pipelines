8baba529bd4a5baf0042d53801a944231fbb4301
Ricardo Dahis <rdahis@gmail.com>
2026-07-31 17:10:08 +1000

    feat(world_cricsheet): add dbt models and tests

---
+++ models/world_cricsheet/world_cricsheet__match_players.sql
@@ -0,0 +1,21 @@
+{{
+    config(
+        schema="world_cricsheet",
+        alias="match_players",
+        materialized="table",
+        partition_by={
+            "field": "year",
+            "data_type": "int64",
+            "range": {"start": 1770, "end": 2035, "interval": 1},
+        },
+    )
+}}
+
+
+select
+    safe_cast(year as int64) year,
+    safe_cast(match_id as string) match_id,
+    safe_cast(team as string) team,
+    safe_cast(player as string) player,
+    safe_cast(player_identifier as string) player_identifier
+from {{ set_datalake_project("world_cricsheet_staging.match_players") }} as t
---
+++ models/world_cricsheet/world_cricsheet__people.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        schema="world_cricsheet",
+        alias="people",
+        materialized="table",
+    )
+}}
+
+
+select
+    safe_cast(identifier as string) identifier,
+    safe_cast(name as string) name,
+    safe_cast(unique_name as string) unique_name,
+    safe_cast(key_bcci as string) key_bcci,
+    safe_cast(key_bcci_2 as string) key_bcci_2,
+    safe_cast(key_bigbash as string) key_bigbash,
+    safe_cast(key_cricbuzz as string) key_cricbuzz,
+    safe_cast(key_cricheroes as string) key_cricheroes,
+    safe_cast(key_crichq as string) key_crichq,
+    safe_cast(key_cricinfo as string) key_cricinfo,
+    safe_cast(key_cricinfo_2 as string) key_cricinfo_2,
+    safe_cast(key_cricinfo_3 as string) key_cricinfo_3,
+    safe_cast(key_cricingif as string) key_cricingif,
+    safe_cast(key_cricketarchive as string) key_cricketarchive,
+    safe_cast(key_cricketarchive_2 as string) key_cricketarchive_2,
+    safe_cast(key_cricketworld as string) key_cricketworld,
+    safe_cast(key_nvplay as string) key_nvplay,
+    safe_cast(key_nvplay_2 as string) key_nvplay_2,
+    safe_cast(key_opta as string) key_opta,
+    safe_cast(key_opta_2 as string) key_opta_2,
+    safe_cast(key_pulse as string) key_pulse,
+    safe_cast(key_pulse_2 as string) key_pulse_2
+from {{ set_datalake_project("world_cricsheet_staging.people") }} as t
