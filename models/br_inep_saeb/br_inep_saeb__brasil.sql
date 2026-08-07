d9190aa7bbf8f2511ab97ba77144ee00ea5a78aa
Pedro Castro <aspeddro@gmail.com>
2026-08-07 17:33:12 -0300

    [update]: Saeb 2025
    Atualizando três tabelas de br_inep_saeb:
    
    - brasil
    - uf
    - municipio

--- models/br_inep_saeb/br_inep_saeb__brasil.sql
+++ models/br_inep_saeb/br_inep_saeb__brasil.sql
@@ -1,6 +1,7 @@
 -- - Atualização 10/12/2025
 {{ config(alias="brasil", schema="br_inep_saeb", materialized="table") }}
 
+
 select
     safe_cast(ano as int64) ano,
     safe_cast(rede as string) rede,
