04e1750402c60aa613727a334f2f4529544b148c
luizavboas <luiza.vilas.oliveira@usp.br>
2026-08-10 19:14:06 -0300

    Fix: particionamento simples estava errado e usava data_referencia

--- models/br_rf_cnpj/br_rf_cnpj__simples.sql
+++ models/br_rf_cnpj/br_rf_cnpj__simples.sql
@@ -7,7 +7,7 @@
 }}
 
 select
-    safe.parse_date('%Y-%m', data_referencia) data_referencia,
+    -- safe.parse_date('%Y-%m', data_referencia) data_referencia,  
     lpad(safe_cast(cnpj_basico as string), 8, '0') cnpj_basico,
     safe_cast(opcao_simples as int64) opcao_simples,
     safe_cast(data_opcao_simples as date) data_opcao_simples,
@@ -15,6 +15,6 @@ select
     safe_cast(opcao_mei as int64) opcao_mei,
     safe_cast(data_opcao_mei as date) data_opcao_mei,
     safe_cast(data_exclusao_mei as date) data_exclusao_mei,
-    safe_cast(data_modificacao as date) data_modificacao
+-- safe_cast(data_modificacao as date) data_modificacao
 from {{ set_datalake_project("br_rf_cnpj_staging.simples") }} as t
 where safe_cast(opcao_mei as string) != "opcao_mei"
