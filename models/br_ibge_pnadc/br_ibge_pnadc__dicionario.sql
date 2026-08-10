1e662c751128a431b1785658896cab153f4e2732
Larissa Brito <larissabribiologia@gmail.com>
2026-08-10 16:56:35 -0300

    modificando p/ materializar em prod

--- models/br_ibge_pnadc/br_ibge_pnadc__dicionario.sql
+++ models/br_ibge_pnadc/br_ibge_pnadc__dicionario.sql
@@ -1,5 +1,6 @@
 {{ config(alias="dicionario", schema="br_ibge_pnadc", materialized="table") }}
 
+
 select
     safe_cast(id_tabela as string) id_tabela,
     safe_cast(nome_coluna as string) nome_coluna,
